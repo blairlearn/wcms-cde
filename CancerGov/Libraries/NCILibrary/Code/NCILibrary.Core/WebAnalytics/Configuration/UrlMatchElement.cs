7a98c5333bd0373644d28ca8e17261145abddaf9
prasadbk <webcommauto@users.noreply.github.com>
2010-09-14 13:47:27 +0000

    Code change to implement Web Analytics story.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@456 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CancerGov/Libraries/NCILibrary/Code/NCILibrary.Core/WebAnalytics/Configuration/UrlMatchElement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Configuration;
+
+
+namespace NCI.Web.CDE.WebAnalytics.Configuration
+{
+    public class UrlMatchElement : ConfigurationElement
+    {
+        [ConfigurationProperty("pathMatch", IsRequired = true)]
+        public string PathMatch
+        {
+            get
+            {
+                return (string)base["pathMatch"];
+            }
+        }
+
+        [ConfigurationProperty("channelName")]
+        public string ChannelName
+        {
+            get
+            {
+                return (string)base["channelName"];
+            }
+        }
+
+        [ConfigurationProperty("urlMatches")]
+        public UrlMatchElementCollection UrlMatches
+        {
+            get { return (UrlMatchElementCollection)base["urlMatches"]; }
+        }
+    }    
+}
