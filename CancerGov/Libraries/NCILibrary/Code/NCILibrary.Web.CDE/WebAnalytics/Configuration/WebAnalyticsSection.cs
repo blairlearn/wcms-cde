de4ce6bcdcd559b88880d52a2841edd32c0e8771
prasadbk <webcommauto@users.noreply.github.com>
2010-09-14 16:43:52 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@462 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CancerGov/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/WebAnalytics/Configuration/WebAnalyticsSection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+
+
+namespace NCI.Web.CDE.WebAnalytics.Configuration
+{
+    public class WebAnalyticsSection : ConfigurationSection
+    {
+        [ConfigurationProperty("enabled", IsRequired = false, DefaultValue = true)]
+        public bool Enabled
+        {
+            get { return (bool)base["enabled"]; }
+        }
+
+        [ConfigurationProperty("reportingSuites")]
+        public ReportingSuitesElementCollection ReportingSuites
+        {
+            get { return (ReportingSuitesElementCollection)base["reportingSuites"]; }
+        }
+
+
+        [ConfigurationProperty("enableNonJavaScriptTagging", IsRequired = false, DefaultValue = false)]
+        public bool EnableNonJavaScriptTagging
+        {
+            get { return (bool)base["enableNonJavaScriptTagging"]; }
+        }
+
+    }
+}
