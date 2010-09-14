82bed47204a1bc10ab9591588b73a8d582f41cd7
wallsjt <webcommauto@users.noreply.github.com>
2010-09-14 22:40:29 +0000

    Adding the Topic Search Category Snippet and Module to the solution.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@466 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/TopicSearchCategorySnippet.ascx
+++ CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/TopicSearchCategorySnippet.ascx
@@ -1,2 +1,56 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" Inherits="CancerGov.Modules.TopicSearch.UI.TopicSearchCategorySnippet" %>
-hello world
+<form id="Form1" runat="server">
+<table cellspacing="0" cellpadding="1" border="0" width="571" class="gray-border">
+	<tbody><tr>
+			<td valign="top">
+<table cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff" width="569">
+<tbody><tr>
+<td valign="top" class="box-title"><img height="25" border="0" width="7" alt="" src="$rxs_imageBase/spacer.gif"></td>
+<td valign="middle" class="box-title" colspan="2">INSERT TABLE TITLE HERE</td>
+</tr>
+<tr>
+<td valign="top" class="gray-border" colspan="3"><img height="1" border="0" width="1" alt="" src="$rxs_imageBase/spacer.gif"></td>
+</tr>
+<tr>
+<td valign="top"><img height="7" border="0" width="8" alt="" src="$rxs_imageBase/spacer.gif"></td>
+<td valign="top"><img height="1" border="0" width="553" alt="" src="$rxs_imageBase/spacer.gif"></td>
+<td valign="top"><img height="1" border="0" width="8" alt="" src="$rxs_imageBase/spacer.gif"></td>
+</tr>
+<tr>
+<td valign="top"><img height="1" border="0" width="7" alt="" src="$rxs_imageBase/spacer.gif"></td>
+<td valign="top" class="leftnav">
+<table cellspacing="0" cellpadding="0" border="0" width="553">
+<%-- --%>
+<tbody><tr><td height="10" colspan="4"></td></tr><tr><td width="20"></td><td valign="top"><b>Select Subcategory:</b>
+<%--Display topic search radio buttons.--%>
+<p>
+      <asp:radiobuttonlist id="rblTopicSearchList" runat="server" RepeatLayout="Flow">
+      
+      </asp:radiobuttonlist>
+<br></p></td>
+<%--Display timeframe buttons.--%>
+<td width="150" valign="top"><b>Select Timeframe:</b><p><b></b>
+      <asp:radiobuttonlist id="rblTimeframeList" runat="server" RepeatLayout="Flow">
+        <asp:listitem id="all" runat="server" value="All (No Date Limit)" />
+        <asp:listitem id="thirty" runat="server" value="Last 30 Days" />
+        <asp:listitem id="sixty" runat="server" value="Last 60 Days" />
+        <asp:listitem id="ninety" runat="server" value="Last 90 Days" />
+      </asp:radiobuttonlist>
+<br></p></td>
+<%--Display search button.--%>
+<td width="100"></td></tr><tr><td height="3" colspan="4"></td></tr><tr><td width="20"></td><td colspan="3"><br><br>
+	<asp:Button ID="btnSearch" runat="server" OnClick="btnSearch_Click"/>
+</td></tr><tr><td height="10" colspan="4"></td></tr>
+<%----%>
+</tbody></table>
+</td>
+<td valign="top"><img height="1" border="0" width="7" alt="" src="$rxs_imageBase/spacer.gif"></td>
+</tr>
+<tr>
+<td valign="top" colspan="3"><img height="7" border="0" width="1" alt="" src="$rxs_imageBase/spacer.gif"></td>
+</tr>
+</tbody></table>
+</td>
+</tr>
+</tbody></table>
+</form>
--- CancerGov/SiteSpecific/Modules/CancerGov.TopicSearch/UI/TopicSearchCategorySnippet.cs
+++ CancerGov/SiteSpecific/Modules/CancerGov.TopicSearch/UI/TopicSearchCategorySnippet.cs
@@ -7,11 +7,72 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using NCI.Web.CDE.UI;
+using NCI.Web.CDE;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using NCI.Web.CDE.PageAssembly;
 
 namespace CancerGov.Modules.TopicSearch.UI
 {
+    /// <summary>
+    /// This Snippet Template is for displaying page options based on Alternate content versions.
+    /// </summary>
+    [DefaultProperty("Text")]
+    [ToolboxData("<{0}:TopicSearchCategorySnippet runat=server></{0}:TopicSearchCategorySnippet>")]
     public class TopicSearchCategorySnippet : SnippetControl
     {
+        protected RadioButtonList rblTopicSearchList;
+        protected RadioButtonList rblTimeframeList;
+        //protected string strTableTitle;
+        public void Page_Load(object sender, EventArgs e)
+        {
+            processTopicSearch(SnippetInfo.Data);
 
+        }
+        protected override void Render(HtmlTextWriter writer)
+        {
+            base.Render(writer);
+        }
+
+        /// <summary>
+        /// Process the topic search information in the xml and the radio button list.
+        /// </summary>
+        /// <param name="snippetXmlData">The xml fragment which contains topic search information.</param>
+        private void processTopicSearch(string snippetXmlData)
+        {
+            // The snippet CDATA may contain CDATA as part of the data but percussion replaces the CDATA 
+            // close tag with Replace ']]>' with ']]ENDCDATA' this ']]ENDCDATA' should be replaced with 
+            // valid CDATA close tag ']]>' before it can be deserialized
+            snippetXmlData = snippetXmlData.Replace("]]ENDCDATA", "]]>");
+            
+            TopicSearchCategory mTSC = null;
+            // TODO: use the factory to create the Module_PageOptionsBox objects
+            using(XmlTextReader reader = new XmlTextReader(snippetXmlData.Trim(), XmlNodeType.Element, null))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(TopicSearchCategory), "cde");
+                mTSC = (TopicSearchCategory)serializer.Deserialize(reader);
+            }
+
+            if (mTSC == null)
+            {
+                //log.error
+                return;
+            }
+
+            if (!Page.IsPostBack)
+            {
+                rblTopicSearchList.DataSource = (object)mTSC.TopicSearches;
+                rblTopicSearchList.DataValueField = "TopicSearchID";
+                rblTopicSearchList.DataTextField = "TopicSearchName";
+                rblTopicSearchList.DataBind();
+            }
+            //strTableTitle += ;
+            
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+        }
     }
 }
