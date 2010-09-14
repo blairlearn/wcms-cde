e3392f88c7a1b79f9d07c8f1b38a165401154b61
wallsjt <webcommauto@users.noreply.github.com>
2010-09-14 18:52:54 +0000

    Adding the Topic Search Category Snippet and Module to the solution.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@464 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/TopicSearchCategorySnippet.ascx
@@ -0,0 +1,2 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" Inherits="CancerGov.Modules.TopicSearch.UI.TopicSearchCategorySnippet" %>
+hello world
---
+++ CancerGov/SiteSpecific/Modules/CancerGov.TopicSearch/UI/TopicSearchCategorySnippet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NCI.Web.CDE.UI;
+
+namespace CancerGov.Modules.TopicSearch.UI
+{
+    public class TopicSearchCategorySnippet : SnippetControl
+    {
+
+    }
+}
