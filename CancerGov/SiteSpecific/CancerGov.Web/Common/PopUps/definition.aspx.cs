b85850204a7c9817ee10397cef7c380c3ce49629
mungarark <webcommauto@users.noreply.github.com>
2010-10-25 17:48:43 +0000

    Saving changes
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@849 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CancerGov/SiteSpecific/CancerGov.Web/Common/PopUps/definition.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NCI.Util;
+using System.Collections;
+
+namespace CancerGov.Web.Common.PopUps
+{
+    public partial class Definition :PopUpPage
+    {
+
+    }
+}
