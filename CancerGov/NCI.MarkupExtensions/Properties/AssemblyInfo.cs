3e5ee372df8ede1ff35dd07bef43dbe91cf5e870
mungarark <webcommauto@users.noreply.github.com>
2010-11-03 15:28:06 +0000

    Saving changes to markup extension code
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@935 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CancerGov/NCI.MarkupExtensions/Properties/AssemblyInfo.cs
+++ CancerGov/NCI.MarkupExtensions/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NCI.MarkupExtensions")]
+[assembly: AssemblyTitle("MarkupExtensions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("NCI.MarkupExtensions")]
+[assembly: AssemblyProduct("MarkupExtensions")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
