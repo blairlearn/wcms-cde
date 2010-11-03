5e9bbbf19aab09234e468381e4fb62dd15120f99
mungarark <webcommauto@users.noreply.github.com>
2010-11-03 14:57:54 +0000

    Saving changes for Markup extensions
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@931 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CancerGov/PresentationClasses/MarkupExtensions/CancerGovLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NCI.Text;
+
+namespace CancerGov.MarkupExtensions
+{
+    /// <summary>
+    /// Provides access to a keyed collection of delegates that create markup extension handlers 
+    /// related to Cancer.gov.
+    /// </summary>
+    public class CancerGovLoader : MarkupExtensionLoader
+    {
+        protected override void LoadHandlers()
+        {
+            Add(() => new ViewDateHandler());
+            Add(() => new ViewLinkHandler());
+            Add(() => new ViewPropertyHandler());
+            Add(() => new ViewTitleHandler());
+        }
+    }
+}
---
+++ CancerGov/PresentationClasses/MarkupExtensions/CancerGovViewPageHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web;
+using NCI.Text;
+using CancerGov.UI.Pages;
+
+namespace CancerGov.MarkupExtensions
+{
+    /// <summary>
+    /// Derives from CancerGovBasePageHandler and adds an overloaded Process method that includes the ViewPage 
+    /// for the current request.
+    /// </summary>
+    public abstract class CancerGovViewPageHandler : CancerGovBasePageHandler
+    {
+        /// <summary>
+        /// Pulls the current ViewPage from the http context so cancer gov specific 
+        /// Process methods in derived classes will have access to the base page.
+        /// </summary>
+        protected ViewPage ViewPage
+        {
+            get
+            {
+                if ((HttpContext.Current.Handler is ViewPage) == false)
+                {
+                    throw new MarkupExtensionException(this.GetType() + " requires that HttpContext.Current.Handler be compatible with " + typeof(ViewPage) + " but was of type " + HttpContext.Current.Handler.GetType());
+                }
+
+                return (ViewPage)HttpContext.Current.Handler;
+            }
+        }
+    }
+}
