e4072dc465447e3df572ad40f835c41ca75ba8e1
ghimirebn <webcommauto@users.noreply.github.com>
2010-11-05 15:20:53 +0000

    Implementing ClinicalTrialsView page.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@969 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/ClinicalTrialsView.ascx.cs
+++ CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/ClinicalTrialsView.ascx.cs
@@ -1,31 +1,34 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Specialized;   // In order to reference Prototype.
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
+using System.Web.UI.MobileControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
 using CancerGov.CDR.ClinicalTrials.Helpers;
 using CancerGov.CDR.ClinicalTrials.Search;
+using CancerGov.CDR.DataManager;
 using CancerGov.Common.HashMaster;
+using CancerGov.UI.CDR;
+using CancerGov.UI.HTML;
+using CancerGov.UI.PageObjects;
 using NCI.Logging;
 using NCI.Util;
+using NCI.Web.CDE;
 using NCI.Web.CDE.Modules;
+using NCI.Web.CDE.UI;
 using NCI.Web.UI.WebControls;
 using NCI.Web.UI.WebControls.FormControls;
 using NCI.Web.UI.WebControls.JSLibraries;
-using CancerGov.CDR.DataManager;
-using System.Web.UI.MobileControls;
-using System.Text;
-using NCI.Web.CDE;
-using NCI.Web.CDE.UI;
-using CancerGov.UI.CDR;
-using System.Collections.Specialized;   // In order to reference Prototype.
+using System.Text.RegularExpressions;
 
 //using CancerGov.UI.WebAnalytics;
 
@@ -69,14 +72,14 @@ namespace CancerGov.Web.SnippetTemplates
         {
             base.OnLoad(e);
 
-			int iProtocolID = -1;
-			int iProtocolSearchID = 0;
-			string strVersion = "";
+            int iProtocolID = -1;
+            int iProtocolSearchID = 0;
+            string strVersion = "";
             bool hasProtocolSearchid = false;
 
- 			//this.PageHtmlHead.Title = "National Cancer Institute - Clinical Trials (PDQ&#174;)";
+            //this.PageHtmlHead.Title = "National Cancer Institute - Clinical Trials (PDQ&#174;)";
             //this.PageHtmlHead.Title = "Clinical Trials (PDQ&#174;) - National Cancer Institute";
-			//for protocal title as page title (SCR: 30153), see Line: ~156
+            //for protocal title as page title (SCR: 30153), see Line: ~156
             // -------------------------------------------
             // 6-22-09 This code was used to overcome the issue related to CTSearchPage loading 
             // generic navigation because it is also used by SearchCancerTopics.  Ultimately the solution 
@@ -95,56 +98,60 @@ namespace CancerGov.Web.SnippetTemplates
             {
                 //CancerGovError.LogError("ViewClinicalTrials", 1, "No ProtocolID Specified");
                 //this.RaiseErrorPage();
-                NCI.Logging.Logger.LogError("ViewClinicalTrials", NCIErrorLevel.Error, "No ProtocalID Specified");
+                NCI.Logging.Logger.LogError("ViewClinicalTrials.OnLoad", "No ProtocalID Specified", NCIErrorLevel.Error);
                 this.RaiseErrorPage("No ProtocalID Specified");
             }
 
-			iProtocolSearchID = Strings.ToInt(Strings.IfNull(Strings.Clean(Request.Params["protocolsearchid"]),"0"));
-			strVersion = Strings.IfNull(Strings.Clean(Request.Params["version"]),"healthprofessional");
-			
-			if (strVersion.ToUpper() == "PATIENT")
-			{
-				pvVersion = ProtocolVersions.Patient;
-			}
+            iProtocolSearchID = Strings.ToInt(Strings.IfNull(Strings.Clean(Request.Params["protocolsearchid"]), "0"));
+            strVersion = Strings.IfNull(Strings.Clean(Request.Params["version"]), "healthprofessional");
+
+            if (strVersion.ToUpper() == "PATIENT")
+            {
+                pvVersion = ProtocolVersions.Patient;
+            }
+
 
             //Get Related Links Info Box
-            if (pvVersion == ProtocolVersions.Patient)
-            {
-                Guid gLinksList = Strings.ToGuid(ConfigurationSettings.AppSettings["ClinicalTrialsPatientRelatedLinksList"]);
+            #region TODO RELATED LINKS INFO BOX
+            //if (pvVersion == ProtocolVersions.Patient)
+            //{
+            //    Guid gLinksList = Strings.ToGuid(ConfigurationSettings.AppSettings["ClinicalTrialsPatientRelatedLinksList"]);
 
-                if (gLinksList != Guid.Empty)
-                {
-                    List lLinksList = new List(gLinksList);
+            //    if (gLinksList != Guid.Empty)
+            //    {
+            //        List lLinksList = new List(gLinksList);
 
-                    if ((lLinksList != null) && (lLinksList.Count > 0))
-                    {
-                        InfoBoxListStyle iblsStyle = new InfoBoxListStyle();
-                        BaseList blLinksList = new BaseList(lLinksList, (BasePage)this, iblsStyle);
+            //        if ((lLinksList != null) && (lLinksList.Count > 0))
+            //        {
+            //            InfoBoxListStyle iblsStyle = new InfoBoxListStyle();
+            //            BaseList blLinksList = new BaseList(lLinksList, iblsStyle);
 
-                        if (blLinksList != null)
-                        {
-                            InfoBoxStyle ibs = new InfoBoxStyle(159);
-                            ibs.ShowHeader = true;
-                            InfoBox ibxListbox = new InfoBox(blLinksList, "Related Links", ibs, (BasePage)this);
-                            StringBuilder sbContent = new StringBuilder();
-                            sbContent.Append("<table border=\"0\" width=\"165\" cellpadding=\"0\" cellspacing=\"0\" align=\"right\">\n");
-                            sbContent.Append("<tr>\n");
-                            sbContent.Append("<td valign=\"top\" width=\"6\"><img src=\"/images/spacer.gif\" width=\"3\" height=\"1\" alt=\"\"></td>\n");
-                            sbContent.Append("<td valign=\"top\" width=\"159\">\n");
-                            sbContent.Append(ibxListbox.Render());
-                            sbContent.Append("</td>\n");
-                            sbContent.Append("</tr>\n");
-                            sbContent.Append("<tr>\n");
-                            sbContent.Append("<td valign=\"top\" colspan=\"2\"><img src=\"/images/spacer.gif\" width=\"1\" height=\"3\" alt=\"\"></td>\n");
-                            sbContent.Append("</tr>\n");
-                            sbContent.Append("</table>\n");
-                            strContent = sbContent.ToString();
-                        }
-                    }
-                }
-            }
-			
-			Protocol pProtocol = null;
+            //            if (blLinksList != null)
+            //            {
+            //                InfoBoxStyle ibs = new InfoBoxStyle(159);
+            //                ibs.ShowHeader = true;
+            //                InfoBox ibxListbox = new InfoBox(blLinksList, "Related Links", ibs, (BasePage)this);
+            //                StringBuilder sbContent = new StringBuilder();
+            //                sbContent.Append("<table border=\"0\" width=\"165\" cellpadding=\"0\" cellspacing=\"0\" align=\"right\">\n");
+            //                sbContent.Append("<tr>\n");
+            //                sbContent.Append("<td valign=\"top\" width=\"6\"><img src=\"/images/spacer.gif\" width=\"3\" height=\"1\" alt=\"\"></td>\n");
+            //                sbContent.Append("<td valign=\"top\" width=\"159\">\n");
+            //                sbContent.Append(ibxListbox.Render());
+            //                sbContent.Append("</td>\n");
+            //                sbContent.Append("</tr>\n");
+            //                sbContent.Append("<tr>\n");
+            //                sbContent.Append("<td valign=\"top\" colspan=\"2\"><img src=\"/images/spacer.gif\" width=\"1\" height=\"3\" alt=\"\"></td>\n");
+            //                sbContent.Append("</tr>\n");
+            //                sbContent.Append("</table>\n");
+            //                strContent = sbContent.ToString();
+            //            }
+            //        }
+            //    }
+            //}
+            #endregion
+
+
+            Protocol pProtocol = null;
             try
             {
                 pProtocol = new Protocol(iProtocolID, ProtocolFunctions.GetSectionList(pvVersion, ProtocolDisplayFormats.SingleProtocol), iProtocolSearchID, pvVersion);
@@ -153,93 +160,89 @@ namespace CancerGov.Web.SnippetTemplates
             {
                 //CancerGovError.LogError("ViewClinicalTrials", 1, "ProtocolID = " + iProtocolID + " Error: " + fetchError.Message);
                 //this.RaiseErrorPage();
-                NCI.Logging.Logger.LogError("ViewClinicalTrials", NCIErrorLevel.Error, "ProtocolID = " + iProtocolID + " Error: " + fetchError.Message);
+                NCI.Logging.Logger.LogError("ViewClinicalTrials", "ProtocolID = " + iProtocolID + " Error: " + fetchError.Message, NCIErrorLevel.Error, fetchError);
                 this.RaiseErrorPage("Error:" + fetchError.Message);
             }
             catch (ProtocolTableEmptyException fetchError)
             {
                 //CancerGovError.LogError("ViewClinicalTrials", 1, "ProtocolID = " + iProtocolID + " Error: " + fetchError.Message);
                 //this.RaiseErrorPage();
-                NCI.Logging.Logger.LogError("ViewClinicalTrials", 1, "ProtocolID = " + iProtocolID + " Error: " + fetchError.Message);
-                this.RaiseErrorPage();
+                NCI.Logging.Logger.LogError("ViewClinicalTrials", "ProtocolID = " + iProtocolID + " Error: " + fetchError.Message, NCIErrorLevel.Error, fetchError);
+                this.RaiseErrorPage("Error:" + fetchError.Message);
             }
             catch (ProtocolTableMiscountException fetchError)
             {
                 //CancerGovError.LogError("ViewClinicalTrials", 1, "ProtocolID = " + iProtocolID + " Error: " + fetchError.Message);
                 //this.RaiseErrorPage();
-                NCI.Logging.Logger.LogError("ViewClinicalTrials", 1, "ProtocolID = " + iProtocolID + " Error: " + fetchError.Message);
-                this.RaiseErrorPage();
+                NCI.Logging.Logger.LogError("ViewClinicalTrials", "ProtocolID = " + iProtocolID + " Error: " + fetchError.Message, NCIErrorLevel.Error, fetchError);
+                this.RaiseErrorPage("Error:" + fetchError.Message);
             }
 
             //Fix related to SCR: 30153 - client decided to impliment 
             //this.PageHtmlHead.Title = pProtocol.ProtocolTitle + " - National Cancer Institute";
 
-			StringBuilder sbDate = new StringBuilder();
-		
-			if (pProtocol.ProtocolType == ProtocolTypes.Protocol) 
-			{
-				if (pProtocol.DateLastModified != new DateTime(0)) 
-				{
-					sbDate.Append("<span class=\"protocol-date-label\">Last Modified: </span>");
-					sbDate.Append("<span class=\"protocol-dates\">");
-					sbDate.Append(pProtocol.DateLastModified.ToString("d"));
-					sbDate.Append("</span>");
-			
-					if (pProtocol.DateFirstPublished != new DateTime(0)) 
-					{
-						sbDate.Append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp");
-					} 
-					else 
-					{
-						sbDate.Append("&nbsp;&nbsp;");
-					}
-				}
+            StringBuilder sbDate = new StringBuilder();
 
-				if (pProtocol.DateFirstPublished != new DateTime(0)) 
-				{
-					sbDate.Append("<span class=\"protocol-date-label\">First Published: </span>");
-					sbDate.Append("<span class=\"protocol-dates\">");
-					sbDate.Append(pProtocol.DateFirstPublished.ToString("d"));
-					sbDate.Append("</span>");
-					sbDate.Append("&nbsp;&nbsp;");
-				}
-			}
+            if (pProtocol.ProtocolType == ProtocolTypes.Protocol)
+            {
+                if (pProtocol.DateLastModified != new DateTime(0))
+                {
+                    sbDate.Append("<span class=\"protocol-date-label\">Last Modified: </span>");
+                    sbDate.Append("<span class=\"protocol-dates\">");
+                    sbDate.Append(pProtocol.DateLastModified.ToString("d"));
+                    sbDate.Append("</span>");
+
+                    if (pProtocol.DateFirstPublished != new DateTime(0))
+                    {
+                        sbDate.Append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp");
+                    }
+                    else
+                    {
+                        sbDate.Append("&nbsp;&nbsp;");
+                    }
+                }
+
+                if (pProtocol.DateFirstPublished != new DateTime(0))
+                {
+                    sbDate.Append("<span class=\"protocol-date-label\">First Published: </span>");
+                    sbDate.Append("<span class=\"protocol-dates\">");
+                    sbDate.Append(pProtocol.DateFirstPublished.ToString("d"));
+                    sbDate.Append("</span>");
+                    sbDate.Append("&nbsp;&nbsp;");
+                }
+            }
             //SCR30153
-    		//this.PageHtmlHead.Title = pProtocol.ProtocolTitle;
+            //this.PageHtmlHead.Title = pProtocol.ProtocolTitle;
 
-			StringBuilder sbPageUrl = new StringBuilder();
-			sbPageUrl.Append("/search/ViewClinicalTrials.aspx");
-			sbPageUrl.Append("?cdrid=");
-			sbPageUrl.Append(iProtocolID.ToString());
+            StringBuilder sbPageUrl = new StringBuilder();
+            sbPageUrl.Append("/search/ViewClinicalTrials.aspx");
+            sbPageUrl.Append("?cdrid=");
+            sbPageUrl.Append(iProtocolID.ToString());
 
 
 
-			if (iProtocolSearchID > 0) 
-			{
-				sbPageUrl.Append("&protocolsearchid=");
-				sbPageUrl.Append(iProtocolSearchID.ToString());
-			}
+            if (iProtocolSearchID > 0)
+            {
+                sbPageUrl.Append("&protocolsearchid=");
+                sbPageUrl.Append(iProtocolSearchID.ToString());
+            }
 
-			if(this.PageDisplayInformation.Version != DisplayVersions.Print)
-			{
-                CancerGov.UI.HTML.HtmlImage headerImage = new CancerGov.UI.HTML.HtmlImage("/images/title_clinical_trials.jpg", "", "160", "60");
-                headerImage.DisplayInfo = this.PageDisplayInformation;
-                TitleBlock tbTitle = new TitleBlock("Clinical Trials (PDQ<sup class=\"header\">&#174;</sup>)", headerImage, this.PageDisplayInformation);
-                
-				tbTitle.Gutter.Add(new CDRVersionBar((BasePage)this, pvVersion, sbDate.ToString(), sbPageUrl.ToString()));
+            if (this.PageDisplayInformation.Version != DisplayVersions.Print)
+            {
+                //CancerGov.UI.HTML.HtmlImage headerImage = new CancerGov.UI.HTML.HtmlImage("/images/title_clinical_trials.jpg", "", "160", "60");
+                //headerImage.DisplayInfo = this.PageDisplayInformation;
+                //TitleBlock tbTitle = new TitleBlock("Clinical Trials (PDQ<sup class=\"header\">&#174;</sup>)", headerImage, this.PageDisplayInformation);
+
+                //tbTitle.Gutter.Add(new CDRVersionBar((BasePage)this, pvVersion, sbDate.ToString(), sbPageUrl.ToString()));
                 //this.PageHeaders.Add(tbTitle);
                 //this.PageLeftColumn = new LeftNavColumn(this, Strings.ToGuid(ConfigurationSettings.AppSettings["ClinicalTrialSearchLeftViewID"]));
                 //this.PageLeftColumn.Insert(0, new ProtocolContentVersionBox((BasePage)this, pProtocol));
             }
-//          else
-//          {
-//              pProtocol.displayVersion = this.PageDisplayInformation.Version;
-//           }
 
 
             // Add "See all Trial Sites" link if protocolsearchid is in the QueryString
             ProtocolRendererOptions renderOptions;
-                        string protocolSearchID;
+            string protocolSearchID;
             if ((protocolSearchID = Request.QueryString.Get("protocolsearchid")) != null)
             {
                 hasProtocolSearchid = true;
@@ -258,39 +261,39 @@ namespace CancerGov.Web.SnippetTemplates
 
             }
             else
-                renderOptions = null; 
-             
-            ProtocolRenderer prProtocol = new ProtocolRenderer((BasePage)this, pProtocol, renderOptions);
-			this.strContent += prProtocol.Render();	
-		
-			if(this.PageDisplayInformation.Version == DisplayVersions.Print)
-			{
-				string printContent = "";
-				string glossaryTableTitle = "Glossary Terms";
-				string linksTableTitle = "Table of Links";
+                renderOptions = null;
 
-				if(this.PageDisplayInformation.Language == NCI.Web.CDE.DisplayLanguage.Spanish)
-				{
-					glossaryTableTitle = "Glosario";
-					linksTableTitle = "Lista de Enlaces";
-				}
+            ProtocolRenderer prProtocol = new ProtocolRenderer( base.pageDisplayInformation, pProtocol, renderOptions, "");
+            this.strContent += prProtocol.Render();
 
-				CancerGov.Common.Extraction.GlossaryTermExtractor gte = new CancerGov.Common.Extraction.GlossaryTermExtractor();
-				CancerGov.Common.Extraction.FootnoteExtractor fe = new CancerGov.Common.Extraction.FootnoteExtractor();
-				fe.ExcludeList = new string[] {"http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?"};
-				fe.RemoveReturnToTopBar = false;
-				
-				printContent = gte.ExtractGlossaryTerms(strContent);
-				printContent = fe.Extract(new Regex("<a\\s+?(?:class=\".*?\"\\s+?)*?href=\"(?<extractValue>.*?)\"(?:\\s+?\\w+?=\"(?:.*?)\")*?\\s*?>(?<linkText>.*?)</a>", RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.Singleline), "extractValue", CancerGov.Common.Extraction.ExtractionTypes.URL, printContent);
-				printContent += gte.BuildGlossaryTable(glossaryTableTitle);
-				printContent += fe.GetFootnotes(linksTableTitle, 80);
+            if (this.PageDisplayInformation.Version == DisplayVersions.Print)
+            {
+                string printContent = "";
+                string glossaryTableTitle = "Glossary Terms";
+                string linksTableTitle = "Table of Links";
 
-				this.strContent = "<table cellspacing=\"0\" width=\"650\" cellpadding=\"0\" border=\"0\"><tr><td width=\"650\">" + printContent + "</td></tr></table>\n";
-			}
-			else
-			{
-				this.strContent += new ReturnToTopAnchor(this.PageDisplayInformation).Render();
-			}
+                if (this.PageDisplayInformation.Language == NCI.Web.CDE.DisplayLanguage.Spanish)
+                {
+                    glossaryTableTitle = "Glosario";
+                    linksTableTitle = "Lista de Enlaces";
+                }
+
+                CancerGov.Common.Extraction.GlossaryTermExtractor gte = new CancerGov.Common.Extraction.GlossaryTermExtractor();
+                CancerGov.Common.Extraction.FootnoteExtractor fe = new CancerGov.Common.Extraction.FootnoteExtractor();
+                fe.ExcludeList = new string[] { "http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?" };
+                fe.RemoveReturnToTopBar = false;
+
+                printContent = gte.ExtractGlossaryTerms(strContent);
+                printContent = fe.Extract(new Regex("<a\\s+?(?:class=\".*?\"\\s+?)*?href=\"(?<extractValue>.*?)\"(?:\\s+?\\w+?=\"(?:.*?)\")*?\\s*?>(?<linkText>.*?)</a>", RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.Singleline), "extractValue", CancerGov.Common.Extraction.ExtractionTypes.URL, printContent);
+                printContent += gte.BuildGlossaryTable(glossaryTableTitle);
+                printContent += fe.GetFootnotes(linksTableTitle, 80);
+
+                this.strContent = "<table cellspacing=\"0\" width=\"650\" cellpadding=\"0\" border=\"0\"><tr><td width=\"650\">" + printContent + "</td></tr></table>\n";
+            }
+            else
+            {
+                this.strContent += new ReturnToTopAnchor(this.PageDisplayInformation).Render();
+            }
 
             //// Web Analytics *************************************************
             //this.WebAnalyticsPageLoad.SetChannelFromSectionNameAndUrl("Clinicaltrials", this.Request.Url.OriginalString.ToString());
@@ -306,7 +309,7 @@ namespace CancerGov.Web.SnippetTemplates
             //litOmniturePageLoad.Text = this.WebAnalyticsPageLoad.Tag();
             //// End Web Analytics *********************************************
 
-		}
+        }
 
     }
 }
