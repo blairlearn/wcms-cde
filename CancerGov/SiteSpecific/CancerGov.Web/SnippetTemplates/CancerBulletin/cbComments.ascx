3a910c59b5fd68e5e9d912a4dcc111be3b8b8010
mafatrm <webcommauto@users.noreply.github.com>
2010-11-12 18:38:35 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@1053 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/CancerBulletin/cbComments.ascx
+++ CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/CancerBulletin/cbComments.ascx
@@ -1,1 +1,63 @@
-﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="cbComments.ascx.cs" Inherits="CancerGov.Web.SnippetTemplates.CancerBulletin.cbComments" %>
+<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="cbComments.ascx.cs"
+    Inherits="CancerGov.Web.SnippetTemplates.CancerBulletin.cbComments" %>
+    
+    
+<meta http-equiv="content-type" content="text/html;charset=ISO-8859-1">
+<meta name="vs_defaultClientScript" content="JavaScript (ECMAScript)">
+<meta name="vs_targetSchema" content="http://schemas.microsoft.com/intellisense/ie5">
+<!-- 
+<%=this.PageHtmlHead.Render()%>  -->
+
+
+<!-- Styles needed for this page -->
+<style>
+    .GoodText
+    {
+        font-weight: bold;
+        color: #484bd0;
+        font-family: Trebuchet MS, Tahoma, Verdana, Arial, sans-serif;
+    }
+    .BadText
+    {
+        font-weight: bold;
+        color: #a8364d;
+        font-family: Trebuchet MS, Tahoma, Verdana, Arial, sans-serif;
+    }
+    .InfoText
+    {
+        font-weight: bold;
+        font-size: 19px;
+        color: #333367;
+        font-family: Trebuchet MS, Tahoma, Verdana, Arial, sans-serif;
+    }
+    .HeaderText
+    {
+        font-family: Times New Roman, Serif;
+        font-size: 19px;
+        color: #C5080B;
+    }
+</style>
+<div align="center">
+    <table border="0" cellpadding="10" cellspacing="0">
+        <tr>
+            <td valign="top">
+                <div style="font-family: Arial; margin-bottom: 3px; margin-top: 0px;">
+                    Share your comments about the <i>NCI Cancer Bulletin</i>.</div>
+                <p>
+                    <!-- We welcome your ideas and suggestions for the Director's Updates. Please use the form below to submit your comments, including topics you would like addressed in future Director's Updates.-->
+                </p>
+                <div style="font-family: Arial;">
+                    <%=this.strError%></div>
+                <br />
+                <form runat="server" id="frmComments" method="post">
+                <label for="txtComment" class="hidden">
+                    comments</label>
+                <textarea name="txtComment" id="txtComment" rows="15" wrap="soft" cols="65" style="font-size: 12px;
+                    width: 550px;"></textarea>
+                <br />
+                <input type="submit" value="Submit" style="font-size: 12px;">
+                </form>
+            </td>
+        </tr>
+    </table>
+</div>
--- CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/CancerBulletin/cbComments.ascx.cs
+++ CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/CancerBulletin/cbComments.ascx.cs
@@ -10,14 +10,138 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
+using NCI.Web.CancerGov.Apps;
 
 namespace CancerGov.Web.SnippetTemplates.CancerBulletin
 {
-    public partial class cbComments : System.Web.UI.UserControl
+    public partial class cbComments : AppsBaseUserControl
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+        //protected void Page_Load(object sender, EventArgs e)
+        //{
 
+        //}
+        protected string strPostResponse = "";
+        protected string strError = "";
+
+
+        protected void Page_Load(object Source, EventArgs e)
+        {
+            
+            string strComment = "";
+
+            //include page title
+            //this.pageHtmlHead.Title = "NCI Cancer Bulletin Comments - National Cancer Institute";
+            //trThanks.Visible = false;
+            //tblTextHeader.Visible = false;
+
+            //this.pageDisplayInformation.Language = DisplayLanguage.English;
+            //this.PageBanner.NavigationBar.ClickLog = false;  
+            //this.NCISectionId = Strings.ToGuid(ConfigurationSettings.AppSettings["DCSectionID"]);
+
+            //if (this.PageDisplayInformation.Version == DisplayVersion.Text)
+            //{
+            //    tblImgHeader.Visible = false;
+            //    tblTextHeader.Visible = true;
+            //}
+
+            if (Page.Request.HttpMethod == "POST")
+            {
+                //Insert Stuff
+                strComment = Request.Params["txtComment"];
+
+                if ((strComment == null) || ((strComment = strComment.Trim()) == ""))
+                {
+                    //Note this also trims the comment...
+                    strError = "<font color=red>Please enter a message.<br></font>";
+                }
+                else
+                {
+                    //Insert it
+
+                    string strConnString = ConfigurationSettings.AppSettings["DbConnectionString"];
+
+                    System.Data.SqlClient.SqlConnection scnComment = new System.Data.SqlClient.SqlConnection(strConnString);
+                    System.Data.SqlClient.SqlCommand scComment = new System.Data.SqlClient.SqlCommand();
+                    scComment.Connection = scnComment;
+
+
+                    scComment.Connection.Open();
+                    scComment.CommandText = "insert into DCComments (CommentID,Comment,CommentType) Values (newid(),'" + strComment.Replace("'", "''") + "','CancerBulletin')";
+
+                    try
+                    {
+                        scComment.ExecuteNonQuery();
+
+                        //THANK YOU
+                        strPostResponse = "<div  style=\"font-family:Arial; color:#4d4d4d; font-size:20px;\">Thank You</div>" +
+                            "			<div>Your feedback was sent to the <i>NCI Cancer Bulletin</i> team. " +
+                            "                   We thank you. <br /><a href=\"/ncicancerbulletin\">View the <i>NCI Cancer Bulletin</i> home page</a><br>" +
+                            "			</div>";
+
+
+                    }
+                    catch (System.Data.SqlClient.SqlException sqlE)
+                    {
+
+                        strPostResponse = "	<p> " +
+                            "Unexpected errors occurred. Our technicians have been " +
+                            "notified and are working to correct the situation." +
+                            "</p>";
+
+                    }
+
+                    //Also send email
+
+                    //System.Web.Mail.MailMessage mailMsg = new System.Web.Mail.MailMessage();
+
+                    //mailMsg.From = "misc@mail.nih.gov";
+                    //mailMsg.Subject = "Cancer Bulletin";
+
+                    //mailMsg.Body += strComment;
+
+                    //mailMsg.To = ConfigurationSettings.AppSettings["DCIdeasEmailRecipient"];
+
+                    //System.Web.Mail.SmtpMail.Send(mailMsg);
+
+                    //trThanks.Visible = true;
+                    //trForm.Visible = false;
+                }
+            }
         }
+
+        public cbComments()
+        {
+            Page.Init += new System.EventHandler(Page_Init);
+        }
+
+        /*		private	void Page_Load(object sender, System.EventArgs e)
+                {	
+                    //content =	new	ContentTemplateProvider(this);			
+                }
+        */
+        #region	Page_Init method
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            //
+            // CODEGEN:	This call is required by the ASP.NET Web Form Designer.
+            //
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region	InitializeComponent	method
+
+        ///	<summary>
+        ///	Required method	for	Designer support - do not modify
+        ///	the	contents of	this method	with the code editor.
+        ///	</summary>
+        private void InitializeComponent()
+        {
+        }
+
+        #endregion
+
     }
 }
