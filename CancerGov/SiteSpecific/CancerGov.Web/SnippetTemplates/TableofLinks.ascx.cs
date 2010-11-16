c70c411b569a32b891e8de08b50e263dd3436265
mungarark <webcommauto@users.noreply.github.com>
2010-11-16 20:24:09 +0000

    Including new control to handle table of links
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@1112 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/TableofLinks.ascx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NCI.Web.CDE;
+using NCI.Web.CDE.UI;
+namespace CancerGov.Web.SnippetTemplates
+{
+    public partial class TableofLinks : SnippetControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ltTableofLinks.Text = TableofLinks;
+
+        }
+
+        protected string TableofLinks
+        {
+            get
+            {
+                string language = string.Empty;
+                string snippetXmlData = string.Empty;
+                if (PageAssemblyContext.Current.PageAssemblyInstruction.Language == "en")
+                {
+                    language = "English";
+                }
+                else
+                {
+                    language = "Spanish";
+                }
+
+                string tableofLinks = string.Empty;
+
+                return tableofLinks;
+            }
+        }
+    }
+}
