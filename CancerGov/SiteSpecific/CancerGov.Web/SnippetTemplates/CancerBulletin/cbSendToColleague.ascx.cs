db98d554ae410c87992a48cadf31eb8a0ec362f6
mafatrm <webcommauto@users.noreply.github.com>
2010-11-16 17:32:42 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@1098 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/CancerBulletin/cbSendToColleague.ascx.cs
+++ CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/CancerBulletin/cbSendToColleague.ascx.cs
@@ -10,14 +10,25 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
+using NCI.Web.CancerGov.Apps;
 
 namespace CancerGov.Web.SnippetTemplates.CancerBulletin
 {
-    public partial class cbSendToFriend : System.Web.UI.UserControl
+    public partial class cbSendToColleague : AppsBaseUserControl
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+        //protected void Page_Load(object sender, EventArgs e)
+        //{
 
-        }
+        //}
+
+        protected string strInfo = "";
+        protected string strHeader = "";
+        protected string strMessageBody = "";
+        protected string strTextClass = "";
+
+
+
+
+
     }
 }
--- CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/CancerBulletin/cbSendToFriend.ascx
+++
@@ -1,2 +0,0 @@
-﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="cbSendToFriend.ascx.cs"
-    Inherits="CancerGov.Web.SnippetTemplates.CancerBulletin.cbSendToFriend" %>
