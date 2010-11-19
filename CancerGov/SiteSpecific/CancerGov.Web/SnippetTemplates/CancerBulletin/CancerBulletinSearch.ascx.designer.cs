8015273550343e7a3b7f08c7fcbc3052efb78853
prasadbk <webcommauto@users.noreply.github.com>
2010-11-19 21:18:22 +0000

    Bug Fixes
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@1177 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/CancerBulletin/CancerBulletinSearch.ascx.designer.cs
+++ CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/CancerBulletin/CancerBulletinSearch.ascx.designer.cs
@@ -21,14 +21,5 @@ namespace CancerGov.Web.SnippetTemplates.CancerBulletin {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlInputImage searchAllButton;
-        
-        /// <summary>
-        /// searchRangeButton control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputImage searchRangeButton;
     }
 }
