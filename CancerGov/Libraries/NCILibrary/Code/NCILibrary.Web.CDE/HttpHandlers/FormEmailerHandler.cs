7ddc2730f63345795dd80c8bd332cb9e0f446f77
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2010-11-19 20:12:10 +0000

    Adding form handlers
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@1172 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CancerGov/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/HttpHandlers/FormEmailerHandler.cs
+++ CancerGov/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/HttpHandlers/FormEmailerHandler.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Web;
 
 namespace NCI.Web.CDE.HttpHandlers
 {
-    class FormEmailerHandler
+    /// <summary>
+    /// HttpHandler to handle generic request forms which email the results to program staff.
+    /// </summary>
+    public class FormEmailerHandler : IHttpHandler
     {
+        #region IHttpHandler Members
+
+        /// <summary>
+        /// Gets a value indicating whether another request can use the <see cref="T:System.Web.IHttpHandler"/> instance.
+        /// </summary>
+        /// <value></value>
+        /// <returns>true if the <see cref="T:System.Web.IHttpHandler"/> instance is reusable; otherwise, false.
+        /// </returns>
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Enables processing of HTTP Web requests by a custom HttpHandler that implements the <see cref="T:System.Web.IHttpHandler"/> interface.
+        /// </summary>
+        /// <param name="context">An <see cref="T:System.Web.HttpContext"/> object that provides references to the intrinsic server objects (for example, Request, Response, Session, and Server) used to service HTTP requests.</param>
+        public void ProcessRequest(HttpContext context)
+        {
+        }
+
+        #endregion
     }
 }
--- CancerGov/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/HttpHandlers/ListservSubscriptionHandler.cs
+++ CancerGov/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/HttpHandlers/ListservSubscriptionHandler.cs
@@ -2,10 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Web;
 
 namespace NCI.Web.CDE.HttpHandlers
 {
-    class ListservSubscriptionHandler
+    /// <summary>
+    /// HttpHandler to handle forms for subscribing users to the NIH list serv.
+    /// </summary>
+    public class ListservSubscriptionHandler : IHttpHandler
     {
+        #region IHttpHandler Members
+
+        /// <summary>
+        /// Gets a value indicating whether another request can use the <see cref="T:System.Web.IHttpHandler"/> instance.
+        /// </summary>
+        /// <value></value>
+        /// <returns>true if the <see cref="T:System.Web.IHttpHandler"/> instance is reusable; otherwise, false.
+        /// </returns>
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Enables processing of HTTP Web requests by a custom HttpHandler that implements the <see cref="T:System.Web.IHttpHandler"/> interface.
+        /// </summary>
+        /// <param name="context">An <see cref="T:System.Web.HttpContext"/> object that provides references to the intrinsic server objects (for example, Request, Response, Session, and Server) used to service HTTP requests.</param>
+        public void ProcessRequest(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
