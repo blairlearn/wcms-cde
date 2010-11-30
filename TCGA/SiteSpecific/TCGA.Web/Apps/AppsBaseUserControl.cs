d96fcab0692145274db420757b35245790ae2ae3
prasadbk <webcommauto@users.noreply.github.com>
2010-11-30 18:20:17 +0000

    Implementing the user control template Publications
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@1251 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ TCGA/SiteSpecific/TCGA.Web/Apps/AppsBaseUserControl.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using System.Web.UI.HtmlControls;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.Configuration;
+using System.Xml;
+using System.Globalization;
+using NCI.Util;
+using NCI.Web.CDE.WebAnalytics;
+using NCI.Web.CDE;
+using NCI.Web.CDE.UI;
+
+namespace NCI.Web.CancerGov.Apps
+{
+    public class AppsBaseUserControl : SnippetControl
+    {
+        private WebAnalyticsPageLoad webAnalyticsPageLoad = new WebAnalyticsPageLoad();
+        private DisplayInformation pageDisplayInformation;
+        private string strHelpPageLink = "#";
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+
+        virtual protected string GetResource(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+                return "";
+            object localizedObject = this.GetGlobalResourceObject("SiteWideSearch", key);
+            if (localizedObject == null)
+                return "key: " + key + " not localized";
+            string val = localizedObject as string;
+            if (string.IsNullOrEmpty(val))
+                return key;
+            return localizedObject as string;
+        }
+
+        public WebAnalyticsPageLoad WebAnalyticsPageLoad
+        {
+            get { return webAnalyticsPageLoad; }
+            set { webAnalyticsPageLoad = value; }
+        }
+
+        public DisplayInformation PageDisplayInformation
+        {
+            get
+            {
+                pageDisplayInformation = new DisplayInformation();
+                switch (PageInstruction.Language)
+                {
+                    case "en":
+                        pageDisplayInformation.Language = DisplayLanguage.English;
+                        break;
+                    case "es":
+                        pageDisplayInformation.Language = DisplayLanguage.Spanish;
+                        break;
+                    default:
+                        pageDisplayInformation.Language = DisplayLanguage.English;
+                        break;
+                }
+                pageDisplayInformation.Version = PageAssemblyContext.Current.DisplayVersion;
+
+                return pageDisplayInformation;
+            }
+
+        }
+
+        virtual public void RaiseErrorPage()
+        {
+            RaiseErrorPage("");
+        }
+
+        virtual public void RaiseErrorPage(string messageKey)
+        {
+            string systemMessagePageUrl = ConfigurationSettings.AppSettings["SystemMessagePage"].Trim();
+
+            if (systemMessagePageUrl.Substring(systemMessagePageUrl.Length - 1, 1) != "?")
+                systemMessagePageUrl += "?";
+
+            systemMessagePageUrl += "msg=" + messageKey.Trim();
+            Response.Redirect(systemMessagePageUrl, true);
+        }
+
+        public string PrettyUrl
+        {
+            get
+            {
+                return this.PageInstruction.GetUrl("PrettyUrl").UriStem;
+            }
+        }
+        public string PrettyUrlWithQS
+        {
+            get
+            {
+                return this.PageInstruction.GetUrl("PrettyUrl").ToString();
+            }
+        }
+
+        public string CurrentPageUrl
+        {
+            get { return this.Request.RawUrl; }
+        }
+
+        public string HelpPageLink
+        {
+            get { return strHelpPageLink; }
+            set { strHelpPageLink = value; }
+        }
+    }
+}
---
+++ TCGA/SiteSpecific/TCGA.Web/SnippetTemplates/ViewPublicationsByCancerType.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ViewPublicationsByCancerType.ascx.cs" Inherits="TCGA.Web.SnippetTemplates.ViewPublicationsByCancerType" %>
