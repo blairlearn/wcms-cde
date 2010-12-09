dcf53fa7d99183176ade00d319c21ec3fe332b46
ghimirebn <webcommauto@users.noreply.github.com>
2010-12-09 20:31:27 +0000

    Updated the arch_drugdictionary.aspx page removing http://wwwnewint.cancer.gov
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@1361 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CancerGov/SiteSpecific/CancerGov.Web/Archive/arch_drugdictionary.aspx.designer.cs
+++ CancerGov/SiteSpecific/CancerGov.Web/Archive/arch_drugdictionary.aspx.designer.cs
@@ -20,7 +20,7 @@ namespace www.Archive {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.UserControl alphaListBox;
+        protected global::Www.Archive.UserControls.AlphaListBox alphaListBox;
         
         /// <summary>
         /// drugDictionaryList control.
