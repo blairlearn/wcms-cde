7c88764694f169d81cbe58db718cdd5c108037ce
mungarark <webcommauto@users.noreply.github.com>
2011-01-28 20:21:00 +0000

    including code to make popups work for definitions
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@1729 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ TCGA/SiteSpecific/TCGA.Web/Common/PopUps/popDefinition.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="popDefinition.aspx.cs" Inherits="TCGA.Web.Common.PopUps.popDefinition" %>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Frameset//EN">
+<HTML>
+  <HEAD id="header" runat="server">
+	<TITLE>Dictionary of Cancer Terms</TITLE>
+	
+  </HEAD>
+  <FRAMESET border=0 frameSpacing=0 rows=55,*,26 frameBorder=no>
+	<FRAME name="Header" title="Header" src="/Common/PopUps/<%=Header%>" noResize scrolling=no>
+	<FRAME name="Definition" title="Definition" src="/Common/PopUps/definition.aspx?<%=UrlArgs%>" noResize>
+	<FRAME name="Footer" title="Footer" src="/Common/PopUps/<%=Footer%>" noResize scrolling=no>
+  </FRAMESET>
+</HTML>
---
+++ TCGA/SiteSpecific/TCGA.Web/Common/PopUps/popDefinition.aspx.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TCGA.Web;
+using System.Web.UI.HtmlControls;
+using TCGA.UI;
+using NCI.Web.CDE;
+
+namespace TCGA.Web.Common.PopUps
+{
+    public partial class PopDefinition : PopUpPage
+    {
+        private IRenderer content;
+        private string urlArgs = "";
+        private string header = "popHeader.htm";
+        private string footer = "popFooter.htm";
+
+        #region Page properties
+
+        /// <summary>
+        /// Sets definition frame source document arguments
+        /// </summary>
+        public string UrlArgs
+        {
+            get { return urlArgs; }
+            set { urlArgs = value; }
+        }
+
+        /// <summary>
+        /// Sets header frame document source
+        /// </summary>
+        public string Header
+        {
+            get { return header; }
+            set { header = value; }
+        }
+
+        /// <summary>
+        /// Sets footer frame document source
+        /// </summary>
+        public string Footer
+        {
+            get { return footer; }
+            set { footer = value; }
+        }
+
+        #endregion
+
+        #region Page properties
+
+        public IRenderer Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Default web form class constructor
+        /// </summary>
+        public PopDefinition()
+        {
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+                        base.OnLoad(e);
+            if (PageAssemblyContext.Current.DisplayVersion == DisplayVersions.Web)
+            {
+                header = "popTextHeader.htm";
+                footer = "popTextFooter.htm";
+                if (Request.QueryString["language"] != "English")
+                {
+                    footer = "popTextFooterSpanish.htm";
+                    header = "popTextHeaderSpanish.htm";
+                }
+            }
+            else
+            {
+                if (Request.QueryString["language"] != "English")
+                {
+                    footer = "popFooterSpanish.htm";
+                    header = "popHeaderSpanish.htm";
+                }
+            }
+
+            urlArgs = Request.Url.Query.Substring(1);
+        }
+
+                protected void Page_Init(object sender, EventArgs e)
+        {
+            //
+            // CODEGEN: This call is required by the ASP.NET Web Form Designer.
+            //
+            InitializeComponent();
+        }
+
+                #region Web Form Designer generated code
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+
+        }
+        #endregion
+    }
+}
