7ad39b3c531c18992526d6241a7e62eb9cd51ab0
Min Gao <zhuomingao@users.noreply.github.com>
2011-01-29 23:53:15 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@1767 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CancerGov/Database/Common/CreateView.sql
+++ CancerGov/Database/Common/CreateView.sql
@@ -7,15 +7,18 @@ as
 select 
 contentid, Short_Description , short_title, long_title, meta_keywords, legacy_search_filter, Date_display_mode
 ,Long_Description
+, date_last_reviewed
 ,date_last_modified
 , Date_first_published
 , prettyurl, language, videourl, audiourl, imageurl, news_qanda_url
+,(select top 1 prettyurl from dbo.cgvpagemetadata t where t.contentid = p. otherlanguagecontentid) as otherlanguageURL
 , case Date_Display_Mode   
  when '1' then Date_first_published
  when '5' then Date_first_published
  else date_last_modified
 end as 'Date'
-from dbo.cgvPageMetaData 
+
+from dbo.cgvPageMetaData p
 
 GO
 if object_id('cgvStagingPageSearch') is not NULL
@@ -26,15 +29,18 @@ as
 select 
 contentid, Short_Description , short_title, long_title, meta_keywords, legacy_search_filter, Date_display_mode
 ,Long_Description
+, date_last_reviewed
 ,date_last_modified
 , Date_first_published
 , prettyurl, language, videourl, audiourl, imageurl, news_qanda_url
+,(select top 1 prettyurl from dbo.cgvpagemetadata t where t.contentid = p. otherlanguagecontentid) as otherlanguageURL
 , case Date_Display_Mode   
  when '1' then Date_first_published
  when '5' then Date_first_published
  else date_last_modified
 end as 'Date'
-from dbo.cgvStagingPageMetaData
+
+from dbo.cgvStagingPageMetaData p
 
 GO
 
