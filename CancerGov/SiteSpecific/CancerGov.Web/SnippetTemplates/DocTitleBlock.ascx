4a231486a5e5d0a7711afcda33fdc86fd6ec2d14
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2011-02-03 13:05:39 +0000

    Adding a snippet control to render DocTitleBlocks because they have the stupid print version.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@1825 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/DocTitleBlock.ascx
@@ -0,0 +1,29 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="DocTitleBlock.ascx.cs" Inherits="CancerGov.Web.SnippetTemplates.DocTitleBlock" %>
+<asp:PlaceHolder ID="phWeb" runat="server" Visible="false">
+<table width="751" cellspacing="0" cellpadding="0" border="0" bgcolor="#d4d9d9">
+	<tbody>
+		<tr>
+			<td width="100%" valign="top" align="left"><table cellspacing="0" cellpadding="0" border="0">
+					<tbody>
+						<tr>
+							<td colspan="2"><img width="1" height="4" border="0" alt="" src="/images/spacer.gif" /></td>
+						</tr>
+						<tr>
+							<td><img width="8" height="1" border="0" alt="" src="/images/spacer.gif" /></td>
+							<td><span class="document-title"><asp:Literal ID="litTitle" runat="server" /></span></td>
+						</tr>
+						<tr>
+							<td colspan="2"><img width="1" height="4" border="0" alt="" src="/images/spacer.gif" /></td>
+						</tr>
+					</tbody>
+			</table></td>
+			<td valign="top" align="right"><img width="10" height="1" border="0" alt="" src="/images/spacer.gif" /></td>
+			<td valign="top" bgcolor="#ffffff"><img width="1" height="1" border="0" alt="" src="/images/spacer.gif" /></td>
+			<td valign="top" bgcolor="#d4d9d9" align="right"><asp:Image ID="imgImage" runat="server" AlternateText="" GenerateEmptyAlternateText="true" /></td>			
+		</tr>
+	</tbody>
+</table>
+</asp:PlaceHolder>
+<asp:PlaceHolder ID="phPrint" runat="server" Visible="false">
+
+</asp:PlaceHolder>
---
+++ CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/DocTitleBlock.ascx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using NCI.Web.CDE;
+using NCI.Web.CDE.UI;
+
+namespace CancerGov.Web.SnippetTemplates
+{
+    public partial class DocTitleBlock : SnippetControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //Parse Data To Get Information
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(this.SnippetInfo.Data);
+
+                XmlNode xnTitle = doc.SelectSingleNode("//Title");
+                XmlNode titleDisplay = doc.SelectSingleNode("//TitleDisplay");
+                XmlNode imageUrl = doc.SelectSingleNode("//ImageUrl");
+
+                string title = PageAssemblyContext.Current.PageAssemblyInstruction.GetField("");
+
+                if (titleDisplay != null)
+                {
+                    switch (titleDisplay.Value)
+                    {
+                        case "DocTitleBlockTitle":
+                            {
+                            }
+                            break;
+                        default:
+                            {
+
+                            }
+                            break;
+                    }
+                }
+                
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
