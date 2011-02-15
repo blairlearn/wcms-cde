cb32fc440277c0a219915d67e6432335f09b50f1
mungarark <webcommauto@users.noreply.github.com>
2011-02-15 16:07:43 +0000

    blocking .ico url's from processing
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/trunk/CDESites@1931 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CancerGov/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/HttpModules/PageAssemblyInstructionLoader.cs
+++ CancerGov/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/HttpModules/PageAssemblyInstructionLoader.cs
@@ -46,7 +46,7 @@ namespace NCI.Web.CDE
             // Get absolute path of the request URL as pretty URL
             String url = context.Server.UrlDecode(context.Request.Url.AbsolutePath.ToLower(CultureInfo.InvariantCulture));
 
-            if (url.IndexOf(".css") != -1 || url.IndexOf(".gif") != -1 || url.IndexOf(".jpg") != -1 || url.IndexOf(".js") != -1 || url.IndexOf(".axd") != -1)
+            if (url.ToLower().IndexOf(".ico") != -1 || url.IndexOf(".css") != -1 || url.IndexOf(".gif") != -1 || url.IndexOf(".jpg") != -1 || url.IndexOf(".js") != -1 || url.IndexOf(".axd") != -1)
                 return;
 
             //Check if the url has been rewritten yet.
