7ce09e51f3ae5957ac5dbbcb44453bc785cb4320
doylejd <webcommauto@users.noreply.github.com>
2011-06-28 18:18:33 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/6.2@2995 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDETemplates/CancerGov/Scripts/sw-autocomplete.js
@@ -0,0 +1,43 @@
+// JavaScript Document
+
+jQuery(document).ready(function($) {
+	
+		//function log( message ) {
+		//	$( "<div/>" ).text( message ).prependTo( "#log" );
+		//	$( "#log" ).attr( "scrollTop", 0 );
+		//}
+		
+		var language = "English";
+		
+		if ($( 'meta[name="content-language"]' ).attr("content") == "es") {
+			language = "Spanish"
+		}
+		
+		$( "#swKeyword" ).autocomplete({
+			source: "/AutoSuggestSearch.svc/SearchJSON/" + language,
+			minLength: 3,
+			focus: function( event, ui ) {
+				$( "#swKeyword" ).val( ui.item.item );
+				return false;
+			},
+			select: function( event, ui ) {
+				$( "#swKeyword" ).val( ui.item.item );
+				return false;
+			}
+		}).data( "autocomplete" )._renderItem = function( ul, item ) {
+			//Escape bad characters
+			var lterm = this.term.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+			
+			//This should find any words which begin with the text from item.			
+			var regex = new RegExp("(^" + lterm + "|\\s+" + lterm + ")");
+			
+			var word = item.item.replace(regex, "<strong>$&</strong>");
+			
+			return $( "<li></li>" )
+				.data( "item.autocomplete", item )
+				.append( "<a>" + word + "</a>" )
+				.appendTo( ul );
+		};
+	}
+	);
+
