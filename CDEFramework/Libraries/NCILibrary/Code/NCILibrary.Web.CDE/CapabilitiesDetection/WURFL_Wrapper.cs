60f94d8453fa1459cf8094000d81ebd5f86f01b1
hodgel2 <webcommauto@users.noreply.github.com>
2011-11-14 23:05:31 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/6.4@3776 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/CapabilitiesDetection/WURFL_Wrapper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NCI.Web.CDE.CapabilitiesDetection
+{
+    public class WURFL_Wrapper
+    {
+       private WurflLoader _WurflLoader = new WurflLoader();
+        private WURFL.IDevice _device = null;
+        private Dictionary<string, string> _capabilityDictionary = null;
+        private bool _loaded = false;
+        private HttpRequest _httpRequest = null;
+
+        public HttpRequest HttpRequest
+        {
+            get { return _httpRequest; }
+        }
+
+        public bool DetectCapabilities(HttpRequest httpRequest)
+        {
+            if ((_device = WurflLoader.GetManager().GetDeviceForRequest(httpRequest)) != null)
+            {
+                _capabilityDictionary = new Dictionary<string, string>(_device.GetCapabilities());
+
+                _httpRequest = httpRequest;
+                _loaded = true;
+                return true;
+            }
+            else
+                return false;
+        }
+               
+        public string GetCapability(string property)
+        {
+            
+            if (_loaded)
+            {
+                if (_capabilityDictionary.ContainsKey(property))
+                    return _capabilityDictionary[property];
+                else
+                    return null;
+            }
+            else
+                return null;
+        }
+
+        public Dictionary<string, string> GetCapabilitiesDictionary()
+        {
+            if (_loaded)
+                return _capabilityDictionary;
+            else
+                return null;
+        }
+        public WURFL.IDevice GetDevice()
+        {
+            if (_loaded)
+                return _device;
+            else
+                return null;
+        }
+    }
+}
