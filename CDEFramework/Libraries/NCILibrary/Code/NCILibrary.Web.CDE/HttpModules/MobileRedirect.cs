03029fd368b2c39901e623e4c2c0117b24718813
hodgel2 <webcommauto@users.noreply.github.com>
2011-11-17 17:35:33 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/6.4@3790 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/HttpModules/MobileRedirect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+using System.Globalization;
+using System.Configuration;
+using System.IO;
+using NCI.Web.CDE.Configuration;
+using NCI.Logging;
+using System.Reflection;
+
+namespace NCI.Web.CDE
+{
+    public class MobileRedirect : IHttpModule
+    {
+    }
+}
