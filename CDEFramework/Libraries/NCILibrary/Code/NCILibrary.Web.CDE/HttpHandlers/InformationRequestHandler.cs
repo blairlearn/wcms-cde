7531b5db2615b3da9ccc0e9b4c51cdd9346a4421
hodgel2 <webcommauto@users.noreply.github.com>
2011-12-05 21:48:32 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/6.4@3865 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/HttpHandlers/InformationRequestHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using NCI.Web.CDE.InformationRequest;
+using NCI.Web.CDE.Configuration;
+
+namespace NCI.Web.CDE
+{
+    public class InformationRequestHandler : IHttpHandler
+    {
+        #region IHttpHandler Members
+
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            //After processing the Information Request and sending the result - shut down the HTTP context
+//            context.Response.Flush();
+//            context.Response.End();
+        }
+
+        #endregion
+    }
+}
