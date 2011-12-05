68a55f9329fb5db74a87db6962b907e8db9d18b7
hodgel2 <webcommauto@users.noreply.github.com>
2011-12-05 21:48:47 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/6.4@3866 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/InformationRequest/InformationRequestErrors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NCI.Web.CDE.InformationRequest
+{
+    /// <summary>
+    /// Information Request Messages 
+    /// </summary>    
+    public sealed class InformationRequestMessages
+    {
+        private InformationRequestMessages() {}
+
+        public static readonly string CanonicalUrlNotFound ="CanonicalUrl not found";
+        public static readonly string MobileUrlNotFound = "MobileUrl not found";
+        
+    }
+}
