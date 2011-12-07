8dba1745bba335375773e3ca0a14a7757df15ee1
schultznr <webcommauto@users.noreply.github.com>
2011-12-07 20:20:28 +0000

    Adding code that puts two meta tags into the html of pages, telling the Exit notification method where the exit notification info pages are.  This modified the web.configs associated code as well as the WebPageAssembler.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/6.4@3886 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/enums.cs
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/enums.cs
@@ -14,7 +14,9 @@ namespace NCI.Web.CDE.UI
         KeyWords,
         ContentType,
         ContentLanguage,
-        Title
+        Title,
+        EnglishLinkingPolicy,
+        EspanolLinkingPolicy
     }
 
     /// <summary>
