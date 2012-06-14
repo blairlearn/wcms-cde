3bdf1ac07d707438be55b15a40e5f9d0368f4ac1
hodgel2 <webcommauto@users.noreply.github.com>
2012-06-14 16:44:27 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/6.5@5177 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/HttpModules/MobileRedirect.cs
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/HttpModules/MobileRedirect.cs
@@ -60,9 +60,8 @@ namespace NCI.Web.CDE
             {
 
                 // if mobile device and mobile url exist, redirect to it
-                //if ((DisplayDeviceDetector.DisplayDevice == DisplayDevices.BasicMobile) ||
-                //    (DisplayDeviceDetector.DisplayDevice == DisplayDevices.AdvancedMobile))
-                if (true)
+                if ((DisplayDeviceDetector.DisplayDevice == DisplayDevices.BasicMobile) ||
+                    (DisplayDeviceDetector.DisplayDevice == DisplayDevices.AdvancedMobile))
                 {
                     string[] alternateContentVersionsKeys;
 
