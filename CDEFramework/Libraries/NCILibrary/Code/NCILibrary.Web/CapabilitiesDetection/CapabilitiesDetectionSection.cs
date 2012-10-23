24d56fdbf11dd20ca0b53ef01c20102c5a8a7cc4
Blair Learn <blairlearn@users.noreply.github.com>
2012-10-23 22:35:33 +0000

    Modify capabilities detection code to allow WURFL data files to be loaded from a location outside the App_Data folder.
    Update default WURFL data and patch files in App_Data folder.
    
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/6.5.1.1@5990 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web/CapabilitiesDetection/CapabilitiesDetectionSection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+using Wurfl.AspNet.DeviceBrowser.Helpers;
+
+namespace NCILibrary.Web.CapabilitiesDetection
+{
+    public class CapabilitiesDetectionSection : ConfigurationSection
+    {
+        [ConfigurationProperty("deviceData", IsRequired = false)]
+        public WurflDataElement DeviceData
+        {
+            get { return (WurflDataElement)base["deviceData"]; }
+        }
+    }
+}
