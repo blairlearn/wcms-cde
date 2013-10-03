d2b224c0046177cbd8dc38392fb880240f83e181
ruckerad <webcommauto@users.noreply.github.com>
2013-10-03 16:09:48 +0000

    Added new code behinds for the CancerGov main nav and section nav.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/6.5.3@6551 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.Web.UI/SnippetControls/CGovSectionNavControl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+using NCI.Text;
+
+namespace NCI.Web.CDE.UI.SnippetControls
+{
+    /// <summary>
+    /// This Snippet Template is for displaying blobs of HTML that Percussion has generated.
+    /// </summary>
+    [DefaultProperty("Text")]
+    [ToolboxData("<{0}:CGovSectionNavControl runat=server></{0}:CGovSectionNavControl>")]
+    public class CGovSectionNavControl : SnippetControl
+    {
+        //My Nav Here
+        string _htmlData = String.Empty;
+
+        //Property for My Nav
+        public string HtmlData
+        {
+            get { return _htmlData; }
+            set { _htmlData = value; }
+        }
+
+        public void Page_Load(object sender, EventArgs e)
+        {
+            HtmlData = SnippetInfo.Data;
+        }
+
+        public override void RenderControl(HtmlTextWriter writer)
+        {
+            base.RenderControl(writer);
+            LiteralControl lit = new LiteralControl(HtmlData);
+            lit.RenderControl(writer);
+        }
+    }
+}
