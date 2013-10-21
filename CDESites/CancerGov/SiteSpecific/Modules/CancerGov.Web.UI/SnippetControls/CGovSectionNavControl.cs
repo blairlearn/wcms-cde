a07942b8ce23589b00f926cfbc9205a1aaa1fab7
ruckerad <webcommauto@users.noreply.github.com>
2013-10-21 13:04:00 +0000

    Removed some comments and finished up CGovSectionNavControl, with getting the html generation to match the website
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/6.5.3@6555 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDESites/CancerGov/SiteSpecific/Modules/CancerGov.Web.UI/SnippetControls/CGovSectionNavControl.cs
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.Web.UI/SnippetControls/CGovSectionNavControl.cs
@@ -33,20 +33,17 @@ namespace CancerGov.Web.UI.SnippetControls
 
         public void Page_Load(object sender, EventArgs e)
         {
-           // _navItem = NavigationItem.ParseTree(SnippetInfo.Data);
+           _navItem = NavigationItem.ParseTree(SnippetInfo.Data);
         }
 
         public override void RenderControl(HtmlTextWriter writer)
         {
             base.RenderControl(writer);
-            //Let's render out a basic div tag with hello world in it.
+            if (_navItem == null)
+            {
+                throw new Exception("Item is null");
+            }
 
-            //Ok let's add a class attribute.  Now, the way the HTMLWriter works is that it is a big stack.
-            //So a stack is first in last out, and in ordr to add a class to the div, we need to do it
-            //before we render the tag.
-
-            //We can do this by adding an attribute.  Note this takes a HtmlTextWriter*Attribute*
-            //not a HtmlTextWriter*Tag*.
 
             //The section navigation has a beginning div tag which is why it is added here.
        
@@ -61,7 +58,7 @@ namespace CancerGov.Web.UI.SnippetControls
             writer.RenderEndTag();
             writer.RenderEndTag();
 
-            RenderNavTree(_navItem.ChildItems[0], writer);
+            RenderNavTree(_navItem, writer);
             //This ends the </div>.  The number of RenderEndTags must match the number of RenderBeginTags
             writer.RenderEndTag();
 
@@ -76,8 +73,7 @@ namespace CancerGov.Web.UI.SnippetControls
             // base.RenderControl(writer);
             //also remember that the RenderEndTag must be called when you want to "output" the closing tag.
             //I will put a placeholder div in for right now to help illustrate what I am getting at...
-            int size = root.ChildItems.Length;
-            int count = 1;
+           
             //writer.Write("Test"+size); //testing to see if it gets here
 
             // writer.RenderBeginTag(HtmlTextWriterTag.Div);
@@ -87,34 +83,23 @@ namespace CancerGov.Web.UI.SnippetControls
                 writer.RenderBeginTag(HtmlTextWriterTag.Ul);
                 foreach (NavigationItem item in root.ChildItems)
                 {
-                    RenderNavItem(item, writer, count, size);
-                    count++;
+                    RenderNavItem(item, writer);
+            
                 }
                 writer.RenderEndTag();
             }
            // writer.RenderEndTag();
         }
 
-        private void RenderNavItem(NavigationItem item, HtmlTextWriter writer, int itemNum, int numItems)
+        private void RenderNavItem(NavigationItem item, HtmlTextWriter writer)
         {
             //base.RenderControl(writer);
             //Add in appropriate classes
             //detect if we the element is open/on/etc.
             String  path = Request.RawUrl;
             
-            if (itemNum == 1)
-            {
-                writer.AddAttribute(HtmlTextWriterAttribute.Class, "first nav-item-"+itemNum);
-            }
-            else if (itemNum == numItems)
-            {
-                writer.AddAttribute(HtmlTextWriterAttribute.Class, "last nav-item-" + numItems);
-            }
-            else
-            {
-                writer.AddAttribute(HtmlTextWriterAttribute.Class, "nav-item-" + itemNum);
-            }
-            writer.RenderBeginTag(HtmlTextWriterTag.Li);
+           
+            
             //render the element information, matching the current HTML - so you may need more than writer.write() statements for that
             //actually, you will because you need the link!!
             
@@ -122,75 +107,28 @@ namespace CancerGov.Web.UI.SnippetControls
 
             //This block of code checks the URL path against the Item but since the home page has a path of '/'
             //First check to make sure it is the url path is not equal to the item url path
-            if (!path.Equals(item.URL))
+            if (path.Equals(item.URL))
             {
-                if (itemNum == 1)
-                {
-                    if (path.Contains(item.SectionPath))
-                    {
-                        writer.AddAttribute(HtmlTextWriterAttribute.Class, "first current");
-                    }
-                    writer.AddAttribute(HtmlTextWriterAttribute.Class, "first");
-                }
-                else if (itemNum == numItems)
-                {
-                    if (path.Contains(item.URL))
-                    {
-                        writer.AddAttribute(HtmlTextWriterAttribute.Class, "last current");
-                    }
-                    else
-                    {
-                        writer.AddAttribute(HtmlTextWriterAttribute.Class, "last");
-                    }
-                }
-                else
-                {
-                    if (path.Contains(item.URL))
-                    {
-                        writer.AddAttribute(HtmlTextWriterAttribute.Class, "current");
-                    }
-                }
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, "leftnav-on");
+            }
+            else if (path.Contains(item.URL))
+            {
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, "leftnav-open");
             }
 
-            else
-            {
-                if (itemNum == 1)
-                {
-                    if (path.Contains(item.SectionPath))
-                    {
-                        writer.AddAttribute(HtmlTextWriterAttribute.Class, "first current");
-                    }
-                    writer.AddAttribute(HtmlTextWriterAttribute.Class, "first");
-                }
-                else if (itemNum == numItems)
-                {
-                    if (path.Contains(item.URL))
-                    {
-                        writer.AddAttribute(HtmlTextWriterAttribute.Class, "last current");
-                    }
-                    else
-                    {
-                        writer.AddAttribute(HtmlTextWriterAttribute.Class, "last");
-                    }
-                }
-                else
-                {
-                    if (path.Contains(item.URL))
-                    {
-                        writer.AddAttribute(HtmlTextWriterAttribute.Class, "current");
-                    }
-                }
-            }
-
+            
+            writer.RenderBeginTag(HtmlTextWriterTag.Li);
+            
+            
             writer.AddAttribute(HtmlTextWriterAttribute.Href, item.URL);
             
             writer.RenderBeginTag(HtmlTextWriterTag.A);
 
-            writer.RenderBeginTag(HtmlTextWriterTag.Span);
+          
             
             writer.Write(item.Title);
             
-            writer.RenderEndTag();
+         
             
             writer.RenderEndTag();
             
@@ -200,7 +138,7 @@ namespace CancerGov.Web.UI.SnippetControls
                 writer.RenderBeginTag(HtmlTextWriterTag.Ul);
                 foreach (NavigationItem subitem in item.ChildItems)
                 {
-                    RenderNavItem(subitem, writer, itemNum++, numItems);
+                    RenderNavItem(subitem, writer);
                 }
                 writer.RenderEndTag();
             }
