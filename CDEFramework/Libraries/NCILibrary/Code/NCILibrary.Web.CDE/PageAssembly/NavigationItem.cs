b210821360eab71a75669702fed4f554832ed620
ruckerad <webcommauto@users.noreply.github.com>
2013-11-18 20:04:44 +0000

    Finished up commenting NavigationItem.cs
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/6.5.3@6588 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/PageAssembly/NavigationItem.cs
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/PageAssembly/NavigationItem.cs
@@ -27,14 +27,23 @@ namespace NCI.Web.CDE
             get { return _items.ToArray(); }
         }
 
-
+        /// <summary>
+        /// This  is the general constructor for the Navigation Item
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="url"></param>
+        /// <param name="sectionPath"></param>
         private NavigationItem(string title, string url, string sectionPath)
         {
             Title = title;
             URL = url;
             SectionPath = sectionPath;
         }
-
+        /// <summary>
+        /// This parses the xml that is passed in into the structure needed to produce the html for the site
+        /// </summary>
+        /// <param name="xml">xml passed in from snippet info</param>
+        /// <returns>the parsed tree</returns>
         public static NavigationItem ParseTree(string xml)
         {
 
@@ -44,10 +53,14 @@ namespace NCI.Web.CDE
 
             return ParseTree(doc.DocumentElement); ;
         }
-
+        /// <summary>
+        /// parses the elements from the xml doc
+        /// </summary>
+        /// <param name="node">the element passed in</param>
+        /// <returns>the Navigation Item</returns>
         private static NavigationItem ParseTree(XmlNode node)
         {
-
+            //gets sets the nodes to what they are in the xml document
             XmlNode titleNode = node.SelectSingleNode("./Title");
             XmlNode urlNode = node.SelectSingleNode("./URL");
             XmlNode sectionPathNode = node.SelectSingleNode("./SectionPath");
@@ -57,16 +70,20 @@ namespace NCI.Web.CDE
                 throw new Exception("Error Parsing Nodes: Title, URL and/or SectionPath are null");
             }
 
+            //creates the navigation item based on the nodes above
             NavigationItem result = new NavigationItem(titleNode.InnerText, urlNode.InnerText, sectionPathNode.InnerText);
 
+            //if there are children of a node it gets set here
             XmlNodeList Children = node.SelectNodes("./NavItems/NavItem");
 
+            //check if null
             if (Children == null)
             {
                 throw new Exception("Error: There are no child nodes");
             }
             else
             {
+                //if children isn't null then recursively generates the nav items
                 foreach (XmlNode noder in Children)
                 {
                     result._items.Add(ParseTree(noder));
@@ -76,12 +93,19 @@ namespace NCI.Web.CDE
                 return result;
         }   
         
-
+        /// <summary>
+        /// no longer used
+        /// </summary>
+        /// <returns></returns>
         public override String ToString()
         {
             return ToStringInternal(0);
         }
-
+        /// <summary>
+        /// no longer used
+        /// </summary>
+        /// <param name="depth"></param>
+        /// <returns></returns>
         private String ToStringInternal(int depth)
         {
             String s = "";
