e8d25e173e2b76fdfaba1bdc079eefbe22eeb69d
Dan Young <youngdr@users.noreply.github.com>
2014-02-24 23:35:36 +0000

    https://tracker.nci.nih.gov/browse/OCEPROJECT-437 - SocialMetadata settings and actualy Disqus comments module.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/6.6i2@6919 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/SnippetControls/DisqusComments.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using NCI.Web.CDE.UI;
+using NCI.Web.CDE;
+using NCI.Web.CDE.Modules;
+using System.Configuration;
+using CancerGov.Web.UI.SnippetControls;
+using NCI.Logging;
+
+namespace NCI.Web.CDE.UI.Modules
+{
+    /// <summary>
+    /// This Snippet Template is for inserting a Disqus comment thread onto the page.
+    /// </summary>
+    [DefaultProperty("Text")]
+    [ToolboxData("<{0}:DisqusComments runat=server></{0}:DisqusComments>")]
+    public class DisqusComments : SnippetControl
+    {
+        protected DisqusWebControl theControl = null;
+
+        public void Page_Load(object sender, EventArgs e)
+        {
+            // check if comments are enabled
+            BasePageAssemblyInstruction basePage = ((BasePageAssemblyInstruction)PageAssemblyContext.Current.PageAssemblyInstruction);
+            
+            bool isCommentingAvailable = false;
+
+            if (basePage is SinglePageAssemblyInstruction)
+            {                
+                // convert string to boolean for IsCommentingEnabled
+                isCommentingAvailable = ((SinglePageAssemblyInstruction)basePage).SocialMetadata.IsCommentingAvailable;
+            }
+            else if (basePage is MultiPageAssemblyInstruction)
+            {
+                // convert string to boolean for IsCommentingEnabled
+                isCommentingAvailable = ((MultiPageAssemblyInstruction)basePage).SocialMetadata.IsCommentingAvailable;
+            }
+
+            Logger.LogError("CDE:DisqusComments.cs:Page_Load", "SocialMetadata isCommentingAvailable value is " + isCommentingAvailable, NCIErrorLevel.Debug);
+
+            // pseudocode
+
+            // skip everything above if not showing comments
+
+            // end pseudocode
+
+            // initialize the control
+            theControl = new DisqusWebControl();
+            this.Controls.Add(theControl);
+
+            // load the shortname from settings
+            DisqusCommentsSettings disqusCommentsSettings = ModuleObjectFactory<DisqusCommentsSettings>.GetModuleObject(SnippetInfo.Data);
+            if (disqusCommentsSettings != null)
+            {
+                theControl.Shortname = disqusCommentsSettings.Shortname;
+            }
+
+            // begin setting the control's properties
+
+            // identifer
+            string contentType = basePage.ContentItemInfo.ContentItemType;
+            string contentId = basePage.ContentItemInfo.ContentItemID;
+            theControl.Identifier = contentType + "-" + contentId;
+
+            // split based on multipage or singlepage
+            if (basePage is SinglePageAssemblyInstruction)
+            {
+                theControl.Title = ((SinglePageAssemblyInstruction)basePage).PageMetadata.ShortTitle;
+                theControl.URL = HttpContext.Current.Request.Url.Scheme
+                + "://"
+                + HttpContext.Current.Request.Url.Authority 
+                + ((SinglePageAssemblyInstruction)basePage).PrettyUrl;
+            }
+            else if (basePage is MultiPageAssemblyInstruction)
+            {
+                theControl.Title = ((MultiPageAssemblyInstruction)basePage).PageMetadata.ShortTitle;
+                theControl.URL = HttpContext.Current.Request.Url.Scheme
+                + "://"
+                + HttpContext.Current.Request.Url.Authority 
+                + ((MultiPageAssemblyInstruction)basePage).PrettyUrl;
+            }
+        }
+
+    }
+}
---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/PageAssembly/SocialMetadata.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Xml.Schema;
+using System.Configuration;
+namespace NCI.Web.CDE
+{
+    public class SocialMetadata
+    {
+        /// <summary>
+        /// Gets the enabled state of comments on the page.
+        /// </summary>
+        [XmlElement(Form = XmlSchemaForm.Unqualified)]
+        public bool IsCommentingAvailable { get; set; }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>.
+        /// </summary>
+        /// <param name="obj">The <see cref="T:System.Object"/> to compare with the current <see cref="T:System.Object"/>.</param>
+        /// <returns>
+        /// true if the specified <see cref="T:System.Object"/> is equal to the current <see cref="T:System.Object"/>; otherwise, false.
+        /// </returns>
+        /// <exception cref="T:System.NullReferenceException">
+        /// The <paramref name="obj"/> parameter is null.
+        /// </exception>
+        public override bool Equals(object obj)
+        {
+            SocialMetadata target = obj as SocialMetadata;
+
+            if (IsCommentingAvailable != target.IsCommentingAvailable)
+                return false;
+            
+            return true;
+        }
+    }
+}
