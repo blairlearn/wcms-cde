853ab8b3111994d56dc038e35db7efb25ab36f31
ruckerad <webcommauto@users.noreply.github.com>
2014-02-26 16:52:44 +0000

    Placeholder templates for previous/next blog posts created
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/6.6i2@6946 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/SnippetControls/NextPrevious.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+using NCI.Text;
+using NCI.Web.CDE.UI;
+using NCI.Web.CDE;
+
+
+namespace NCI.Web.CDE.UI.SnippetControls
+{
+    /// <summary>
+    /// This Snippet Template is for creating buttons for previous/next posts for Blog Post Content Type.
+    /// </summary>
+    [DefaultProperty("Text")]
+    [ToolboxData("<{0}:NextPrevious runat=server></{0}:NextPrevious>")]
+    public class NextPrevious : SnippetControl
+    {
+
+
+    }
+}
---
+++ CDESites/CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/NextPrevious.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="NextPrevious.ascx.cs" Inherits="NCI.Web.CDE.UI.SnippetControls.NextPrevious" %>
