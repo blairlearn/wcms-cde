9ac89a446103ae600ec5f73210d613de7ba871c8
Dion D'aquino <daquinohd@users.noreply.github.com>
2014-09-08 02:23:27 +0000

    Creating CDE widget for new language toggle https://tracker.nci.nih.gov/browse/OCEPROJECT-1250
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/cheetoh-it2@7844 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/Configuration/LanguageToggle.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+
+namespace NCI.Web.CDE.UI.Configuration
+{
+    public class LanguageToggle : IStateManager
+    {
+
+        private bool _trackViewState = false;
+
+
+        private string _template;
+
+
+
+        [PersistenceMode(PersistenceMode.InnerProperty)]
+        public string Template
+        {
+            get
+            {
+                return (string)this.ViewState["Template"] ?? string.Empty;
+            }
+            set
+            {
+                //If the text is the same, just ignore and don't set dirtyness
+                //or fire any events.
+                if (!object.Equals(value, this.ViewState["Template"]))
+                {
+                    this.ViewState["Template"] = value;
+                    this.OnLangToggleChanged();
+                }
+            }
+        }
+
+
+        private StateBag _statebag = new StateBag();
+        protected StateBag ViewState
+        {
+            get
+            {
+                return this._statebag;
+            }
+        }
+
+        internal event EventHandler LangToggleChanged;
+
+        protected internal void OnLangToggleChanged()
+        {
+            if (this.LangToggleChanged != null)
+            {
+                this.LangToggleChanged(this, EventArgs.Empty);
+            }
+        }
+
+        protected internal LanguageToggle CloneLanguageToggle()
+        {
+            LanguageToggle newLangToggle = this.CreateLanguageToggle();
+            this.CopyProperties(newLangToggle);
+            return newLangToggle;
+        }
+
+        protected void CopyProperties(LanguageToggle newLangToggle)
+        {
+            ((LanguageToggle)newLangToggle).Template = this.Template;
+        }
+
+        protected LanguageToggle CreateLanguageToggle()
+        {
+            return new LanguageToggle();
+        }
+
+        protected void LoadViewState(object savedState)
+        {
+            if (savedState != null)
+            {
+                object[] objArray = (object[])savedState;
+
+                if (objArray[0] != null)
+                    ((IStateManager)this.ViewState).LoadViewState(objArray[0]);
+            }
+        }
+
+        protected object SaveViewState()
+        {
+            object obj1 = ((IStateManager)this.ViewState).SaveViewState();
+
+            if (obj1 == null)
+                return null;
+
+            return new object[] { obj1 };
+
+        }
+
+        protected void TrackViewState()
+        {
+            this._trackViewState = true;
+            ((IStateManager)this.ViewState).TrackViewState();
+            //If there are any sub IStateManager items, start tracking view state for them as well.
+        }
+
+        public virtual string OnClick
+        {
+            get
+            {
+                return (string)this.ViewState["OnClick"] ?? string.Empty;
+            }
+            set
+            {
+                //If the text is the same, just ignore and don't set dirtyness
+                //or fire any events.
+                if (!object.Equals(value, this.ViewState["OnClick"]))
+                {
+                    this.ViewState["OnClick"] = value;
+                    this.OnLangToggleChanged();
+                }
+            }
+        }
+
+        internal void SetDirty()
+        {
+            this._statebag.SetDirty(true);
+
+            //If there are any sub IStateManager items, set them dirty as well.
+        }
+        #region IStateManager Members
+
+        /// <summary>
+        /// When implemented by a class, gets a value indicating whether a server control is tracking its view state changes.
+        /// </summary>
+        /// <value></value>
+        /// <returns>true if a server control is tracking its view state changes; otherwise, false.
+        /// </returns>
+        public bool IsTrackingViewState
+        {
+            get { return this._trackViewState; }
+        }
+
+        /// <summary>
+        /// When implemented by a class, loads the server control's previously saved view state to the control.
+        /// </summary>
+        /// <param name="state">An <see cref="T:System.Object"/> that contains the saved view state values for the control.</param>
+        void IStateManager.LoadViewState(object state)
+        {
+            this.LoadViewState(state);
+        }
+
+        /// <summary>
+        /// When implemented by a class, saves the changes to a server control's view state to an <see cref="T:System.Object"/>.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="T:System.Object"/> that contains the view state changes.
+        /// </returns>
+        object IStateManager.SaveViewState()
+        {
+            return this.SaveViewState();
+        }
+
+        /// <summary>
+        /// When implemented by a class, instructs the server control to track changes to its view state.
+        /// </summary>
+        void IStateManager.TrackViewState()
+        {
+            this.TrackViewState();
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/Configuration/LanguageToggleCollection.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Collections;
+using System.Globalization;
+using System.ComponentModel;
+
+namespace NCI.Web.CDE.UI.Configuration
+{
+
+    public class LanguageToggleCollection : StateManagedCollection
+    {
+        //These are the types of AddThis items that can be in this collection.
+        //If there needs to be a new type, then add a class and then
+        //add it to this list.
+        private static readonly Type[] knownTypes = new Type[] { 
+            typeof(LanguageToggle)
+        };
+
+        public event EventHandler LanguageToggleCollectionChanged;
+
+        [Browsable(false)]
+        public LanguageToggle this[int index]
+        {
+            get
+            {
+                return (this[index] as LanguageToggle);
+            }
+        }
+
+        [Browsable(false)]
+        public LanguageToggle this[String index]
+        {
+            get
+            {
+                foreach (LanguageToggle langToggle in this)
+                {
+                    return langToggle;
+                }
+                return null;
+            }
+        }
+
+        [Browsable(false)]
+        public LanguageToggle this[CultureInfo culture]
+        {
+            get
+            {
+                foreach (LanguageToggle langToggle in this)
+                {
+                    return langToggle;
+                }
+                return null;
+            }
+        }
+
+        public void Add(LanguageToggle langToggle)
+        {
+            ((IList)this).Add(langToggle);
+        }
+
+        public LanguageToggleCollection CloneFields()
+        {
+            LanguageToggleCollection langTogglesItems = new LanguageToggleCollection();
+            foreach (LanguageToggle langToggle in this)
+            {
+                langTogglesItems.Add(langToggle.CloneLanguageToggle());
+            }
+            return langTogglesItems;
+        }
+
+        public bool Contains(LanguageToggle langToggle)
+        {
+            return ((IList)this).Contains(langToggle);
+        }
+
+        public void CopyTo(LanguageToggle[] array, int index)
+        {
+            this.CopyTo(array, index);
+        }
+
+        public int IndexOf(LanguageToggle langToggle)
+        {
+            return ((IList)this).IndexOf(langToggle);
+        }
+
+        public void Insert(int index, LanguageToggle langToggle)
+        {
+            ((IList)this).Insert(index, langToggle);
+        }
+
+        public void Removed(LanguageToggle langToggle)
+        {
+            ((IList)this).Remove(langToggle);
+        }
+
+        public void RemoveAt(int index)
+        {
+            ((IList)this).RemoveAt(index);
+        }
+
+        protected override void OnClearComplete()
+        {
+            this.OnLanguageToggleCollectionChanged();
+        }
+
+        protected override object CreateKnownType(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    return new LanguageToggle();
+            }
+
+            throw new ArgumentOutOfRangeException("Incorrect type");
+        }
+
+        protected override void OnRemoveComplete(int index, object value)
+        {
+            LanguageToggle langToggle = value as LanguageToggle;
+            if (langToggle != null)
+            {
+                langToggle.LangToggleChanged += new EventHandler(this.LanguageToggleCollectionChanged);
+            }
+
+            this.OnLanguageToggleCollectionChanged();
+        }
+
+        protected override void OnValidate(object value)
+        {
+            base.OnValidate(value);
+            if (!(value is LanguageToggle))
+            {
+                throw new ArgumentException("Invalid type");
+            }
+        }
+
+        protected override Type[] GetKnownTypes()
+        {
+            return knownTypes;
+        }
+
+        protected override void SetDirtyObject(object b)
+        {
+            ((LanguageToggleCollection)b).SetDirty();
+        }
+
+        private void OnLanguageToggleCollectionChanged(object sender, EventArgs e)
+        {
+            this.OnLanguageToggleCollectionChanged();
+        }
+
+        private void OnLanguageToggleCollectionChanged()
+        {
+            if (this.LanguageToggleCollectionChanged != null)
+            {
+                this.LanguageToggleCollectionChanged(this, EventArgs.Empty);
+            }
+        }
+
+    }
+}
