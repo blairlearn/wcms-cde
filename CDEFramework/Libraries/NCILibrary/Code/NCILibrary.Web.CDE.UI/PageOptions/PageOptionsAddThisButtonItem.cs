f0730dbe498bb77a1b56becdf521839554710f0a
Dion D'aquino <daquinohd@users.noreply.github.com>
2014-09-19 01:49:35 +0000

    Created page options control from addThis control
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/cheetoh-it2-pageoptions@7964 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/PageOptions/PageOptionsAddThisButtonItem.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+
+namespace NCI.Web.CDE.UI.WebControls
+{
+    public class PageOptionsAddThisButtonItem : PageOptionsButtonItem
+    {
+
+        public string Service
+        {
+            get
+            {
+                return (string)this.ViewState["Service"] ?? string.Empty;
+            }
+            set
+            {
+                //If the text is the same, just ignore and don't set dirtyness
+                //or fire any events.
+                if (!object.Equals(value, this.ViewState["Service"]))
+                {
+                    this.ViewState["Service"] = value;
+                    this.OnButtonItemChanged();
+                }
+            }
+        }
+
+        /*
+        private StateBag _statebag = new StateBag();
+        protected StateBag ViewState
+        {
+            get
+            {
+                return this._statebag;
+            }
+        }
+        */
+
+        internal event EventHandler ButtonItemChanged;
+
+        protected internal void OnButtonItemChanged()
+        {
+            if (this.ButtonItemChanged != null)
+            {
+                this.ButtonItemChanged(this, EventArgs.Empty);
+            }
+        }
+
+        protected internal PageOptionsAddThisButtonItem ClonePageOptionsAddThisButtonItem()
+        {
+            PageOptionsAddThisButtonItem newPageOptionsAddThisButtonItem = this.CreatePageOptionsAddThisButtonItem();
+            this.CopyProperties(newPageOptionsAddThisButtonItem);
+            return newPageOptionsAddThisButtonItem;
+        }
+
+        protected void CopyProperties(PageOptionsAddThisButtonItem newPageOptionsAddThisButtonItem)
+        {
+            ((PageOptionsAddThisButtonItem)newPageOptionsAddThisButtonItem).Service = this.Service;
+            ((PageOptionsAddThisButtonItem)newPageOptionsAddThisButtonItem).Title = this.Title;
+        }
+
+        protected PageOptionsAddThisButtonItem CreatePageOptionsAddThisButtonItem()
+        {
+            return new PageOptionsAddThisButtonItem();
+        }
+        
+        /*
+        protected void LoadViewState(object savedState)
+        {
+            if (savedState != null)
+            {
+                object[] objArray = (object[])savedState;
+
+                if (objArray[0] != null)
+                    ((IStateManager)this.ViewState).LoadViewState(objArray[0]);
+            }
+        }
+
+        protected object SaveViewState()
+        {
+            object obj1 = ((IStateManager)this.ViewState).SaveViewState();            
+
+            if (obj1 == null)
+                return null;
+
+            return new object[] { obj1 };
+
+        }
+
+        protected void TrackViewState()
+        {
+            this._trackViewState = true;
+            ((IStateManager)this.ViewState).TrackViewState();
+            //If there are any sub IStateManager items, start tracking view state for them as well.
+        }
+
+        public virtual string OnClick
+        {
+            get
+            {
+                return (string)this.ViewState["OnClick"] ?? string.Empty;
+            }
+            set
+            {
+                //If the text is the same, just ignore and don't set dirtyness
+                //or fire any events.
+                if (!object.Equals(value, this.ViewState["OnClick"]))
+                {
+                    this.ViewState["OnClick"] = value;
+                    this.OnButtonItemChanged();
+                }
+            }
+        }
+
+        internal void SetDirty()
+        {
+            this._statebag.SetDirty(true);
+
+            //If there are any sub IStateManager items, set them dirty as well.
+        }
+
+        #region IStateManager Members
+
+        /// <summary>
+        /// When implemented by a class, gets a value indicating whether a server control is tracking its view state changes.
+        /// </summary>
+        /// <value></value>
+        /// <returns>true if a server control is tracking its view state changes; otherwise, false.
+        /// </returns>
+        public bool IsTrackingViewState
+        {
+            get { return this._trackViewState; }
+        }
+
+        /// <summary>
+        /// When implemented by a class, loads the server control's previously saved view state to the control.
+        /// </summary>
+        /// <param name="state">An <see cref="T:System.Object"/> that contains the saved view state values for the control.</param>
+        void IStateManager.LoadViewState(object state)
+        {
+            this.LoadViewState(state);
+        }
+
+        /// <summary>
+        /// When implemented by a class, saves the changes to a server control's view state to an <see cref="T:System.Object"/>.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="T:System.Object"/> that contains the view state changes.
+        /// </returns>
+        object IStateManager.SaveViewState()
+        {
+            return this.SaveViewState();
+        }
+
+        /// <summary>
+        /// When implemented by a class, instructs the server control to track changes to its view state.
+        /// </summary>
+        void IStateManager.TrackViewState()
+        {
+            this.TrackViewState();
+        }
+
+        #endregion
+        */
+
+    }
+}
---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/PageOptions/PageOptionsButtonCollection.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Collections;
+using System.ComponentModel;
+
+namespace NCI.Web.CDE.UI.WebControls
+{
+    public class PageOptionsButtonCollection : StateManagedCollection
+    {
+        //These are the types of AddThis items that can be in this collection.
+        //If there needs to be a new type, then add a class and then
+        //add it to this list.
+        private static readonly Type[] knownTypes = new Type[] { 
+            typeof(PageOptionsButtonItem),
+            typeof(CustomAddThisButtonItem),
+            typeof(GoogleAddThisButtonItem)
+        };
+
+        public event EventHandler ButtonItemCollectionChanged;
+
+        [Browsable(false)]
+        public PageOptionsButtonItem this[int index]
+        {
+            get
+            {
+                return (this[index] as PageOptionsButtonItem);
+            }
+        }
+
+        [Browsable(false)]
+        public PageOptionsButtonItem this[String index]
+        {
+            get
+            {
+                foreach(PageOptionsButtonItem buttonItem in this){
+                    if (buttonItem.Service.Equals(index))
+                    {
+                        return buttonItem;
+                    }
+                }
+                return null;
+            }
+        }
+
+        public void Add(PageOptionsButtonItem buttonItem)
+        {
+            ((IList)this).Add(buttonItem);
+        }
+
+        public PageOptionsButtonCollection CloneFields()
+        {
+            PageOptionsButtonCollection buttonItems = new PageOptionsButtonCollection();
+            foreach (PageOptionsButtonItem buttonItem in this)
+            {
+                buttonItems.Add(buttonItem.CloneAddThisButtonItem());
+            }
+            return buttonItems;
+        }
+
+        public bool Contains(PageOptionsButtonItem buttonItem)
+        {
+            return ((IList)this).Contains(buttonItem);
+        }
+
+        public void CopyTo(PageOptionsButtonItem[] array, int index)
+        {
+            this.CopyTo(array, index);
+        }
+
+        public int IndexOf(PageOptionsButtonItem buttonItem)
+        {
+            return ((IList)this).IndexOf(buttonItem);
+        }
+
+        public void Insert(int index, PageOptionsButtonItem buttonItem)
+        {
+            ((IList)this).Insert(index, buttonItem);
+        }
+
+        public void Removed(PageOptionsButtonItem buttonItem)
+        {
+            ((IList)this).Remove(buttonItem);
+        }
+
+        public void RemoveAt(int index)
+        {
+            ((IList)this).RemoveAt(index);
+        }
+
+        protected override void OnClearComplete()
+        {
+            this.OnButtonItemCollectionChanged();
+        }
+
+        protected override object CreateKnownType(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    return new PageOptionsButtonItem();
+                case 1:
+                    return new CustomAddThisButtonItem();
+                case 2:
+                    return new GoogleAddThisButtonItem();
+            }
+
+            throw new ArgumentOutOfRangeException("Incorrect type");
+        }
+
+        protected override void OnRemoveComplete(int index, object value)
+        {
+            PageOptionsButtonItem buttonItem = value as PageOptionsButtonItem;
+            if (buttonItem != null)
+            {
+                buttonItem.ButtonItemChanged += new EventHandler(this.ButtonItemCollectionChanged);
+            }
+
+            this.OnButtonItemCollectionChanged();
+        }
+
+        protected override void OnValidate(object value)
+        {
+            base.OnValidate(value);
+            if (!(value is PageOptionsButtonItem))
+            {
+                throw new ArgumentException("Invalid type");
+            }
+        }
+
+        protected override Type[] GetKnownTypes()
+        {
+            return knownTypes;
+        }
+
+        protected override void SetDirtyObject(object b)
+        {
+            ((PageOptionsButtonCollection)b).SetDirty();
+        }
+
+        private void OnButtonItemCollectionChanged(object sender, EventArgs e)
+        {
+            this.OnButtonItemCollectionChanged();
+        }
+
+        private void OnButtonItemCollectionChanged()
+        {
+            if (this.ButtonItemCollectionChanged != null)
+            {
+                this.ButtonItemCollectionChanged(this, EventArgs.Empty);
+            }
+        }
+
+    }
+}
