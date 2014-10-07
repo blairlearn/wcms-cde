4541a07357a37d3028fc0f1c7debc2ee8c18f188
Dion D'aquino <daquinohd@users.noreply.github.com>
2014-10-07 13:09:02 +0000

    Updated class BreadCrumbSnippet.cs to draw breadcrumb trail https://tracker.nci.nih.gov/browse/OCEPROJECT-1380
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/cheetoh-it3@8122 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/SnippetControls/BreadCrumbSnippet.cs
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/SnippetControls/BreadCrumbSnippet.cs
@@ -10,14 +10,24 @@ using System.Text.RegularExpressions;
 
 using NCI.Text;
 
+
+/*
+ * TODO:
+ * fix root path - get code to pull root path from xml instead of using canonical url 
+ *  - check for double forward slash on path & remove as necessary
+ * fix publishing issues 
+ *  - fix snippet DisplayVersion (web / viewall only)
+ * update CDE templates
+ * create test cases 
+ * update CSS
+ */
 namespace NCI.Web.CDE.UI.SnippetControls
 {
     public class BreadCrumbSnippet : SnippetControl
     {
-        protected string RootPath { get; set; }
 
+        protected string RootPath { get { return PageAssemblyContext.Current.PageAssemblyInstruction.GetUrl("CanonicalUrl").ToString(); } }
         protected string CurrUrl { get { return PageAssemblyContext.Current.PageAssemblyInstruction.GetUrl("PrettyUrl").ToString(); } }
-
 
         public override void RenderControl(HtmlTextWriter writer)
         {
@@ -36,7 +46,6 @@ namespace NCI.Web.CDE.UI.SnippetControls
             {
                 return;
             }
-
 
             //Opening UL tag 
             writer.AddAttribute(HtmlTextWriterAttribute.Class, "breadcrumbs");
@@ -59,41 +68,48 @@ namespace NCI.Web.CDE.UI.SnippetControls
 
         private void RenderBreadcrumbSections(SectionDetail section, HtmlTextWriter writer)
         {
-            /*    
-                   //Need to add FullPath to section details... this should combine ParentPath with SectionName (including a path separator between them)
-                   //Basically we need to check if we should draw this section in the bread crumbs, and the reason we would is because this
-                   //section is within the section that is the root of the navon.  Luckily we can do this with string comparisons...
-                   if (We the current "section" is with the folder structure of the RootPath of the breadcrumb) {
+            /*
+             * Need to add FullPath to section details... this should combine ParentPath with SectionName (including a path separator between them)
+             * Basically we need to check if we should draw this section in the bread crumbs, and the reason we would is because this
+             * section is within the section that is the root of the navon.  Luckily we can do this with string comparisons...
             */
-            //If the section has a parent, attempt to draw it first.        
-            if (section.ParentPath != null)
+            // if (The current "section" is within the folder structure of the RootPath of the breadcrumb)
+            if (RootPath != null &&
+                System.Text.RegularExpressions.Regex.IsMatch(RootPath, section.FullPath, System.Text.RegularExpressions.RegexOptions.IgnoreCase))
             {
-                writer.Write(section.Parent);
-                //RenderBreadcrumbSections(section, writer);
-                // RenderBreadcrumbLink(section.parent, writer);
+                //If the section has a parent, attempt to draw it first.        
+                if (section.ParentPath != null)
+                {
+                    RenderBreadcrumbSections(section.Parent, writer);
+                }
+
+
+                //If the LandingPageURL is not set, DO NOT DRAW A BAD LINK!!!
+                if (section.LandingPageURL != null && section.NavTitle != null)
+                {
+                    //Draw this item
+                    if (section.LandingPageURL == CurrUrl)
+                    {
+                        writer.RenderBeginTag(HtmlTextWriterTag.Li);
+                        writer.Write(" > ");
+                        writer.Write(section.NavTitle);
+                        writer.RenderEndTag();
+                    }
+                    else
+                    {
+                        writer.RenderBeginTag(HtmlTextWriterTag.Li);
+                        writer.AddAttribute(HtmlTextWriterAttribute.Href, section.LandingPageURL);
+                        writer.RenderBeginTag(HtmlTextWriterTag.A);
+                        writer.Write(section.NavTitle);
+                        writer.RenderEndTag();
+                        writer.Write(" > ");
+                        writer.RenderEndTag();
+                    }
+                }
             }
-
-
-
-            //If the LandingPageURL is not set, DO NOT DRAW A BAD LINK!!!
-            if (section.LandingPageURL != null && section.NavTitle != null)
+            else
             {
-                //Draw this item
-                if (section.LandingPageURL == CurrUrl)
-                {
-                    writer.RenderBeginTag(HtmlTextWriterTag.Li);
-                    writer.Write(section.NavTitle);
-                    writer.RenderEndTag();
-                }
-                else
-                {
-                    writer.RenderBeginTag(HtmlTextWriterTag.Li);
-                    writer.AddAttribute(HtmlTextWriterAttribute.Href, section.LandingPageURL);
-                    writer.RenderBeginTag(HtmlTextWriterTag.A);
-                    writer.Write(section.NavTitle);
-                    writer.RenderEndTag();
-                    writer.RenderEndTag();
-                }
+                return;
             }
 
         } // RenderBreadCrumbSections()
