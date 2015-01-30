9dc74971d90d6bcf06d9654dc3b2ef73c518833c
Dion D'aquino <daquinohd@users.noreply.github.com>
2015-01-30 16:56:24 +0000

    Added snippet controls for syndication https://tracker.nci.nih.gov/browse/OCEPROJECT-2168
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/devonrex-it2@9076 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/SnippetControls/SyndicationNotice.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+using NCI.Text;
+
+namespace NCI.Web.CDE.UI.SnippetControls
+{
+    /// <summary>
+    /// This Snippet Template is for displaying blobs of HTML that Percussion has generated.
+    /// </summary>
+    [DefaultProperty("Text")]
+    [ToolboxData("<{0}:SyndicationNotice runat=server></{0}:SyndicationNotice>")]
+    public class SyndicationNotice : SnippetControl
+    {
+        string _htmlData = String.Empty;
+
+        public string HtmlData
+        {
+            get { return _htmlData; }
+            set { _htmlData = value; }
+        }
+
+        public void Page_Load(object sender, EventArgs e)
+        {
+            if (displaySyndicationNotice() == "true")
+                HtmlData = SnippetInfo.Data;
+            else
+                HtmlData = String.Empty;
+        }
+
+        public override void RenderControl(HtmlTextWriter writer)
+        {
+            base.RenderControl(writer);
+            HtmlData = MarkupExtensionProcessor.Instance.Process(HtmlData);
+            LiteralControl lit = new LiteralControl(HtmlData);
+            lit.RenderControl(writer);
+        }
+        /// <summary>
+        /// Process the syndication XML field - determines if the syndication notice
+        /// should display or not
+        /// </summary>
+        /// <param name="snippetXmlData">The xml fragment which contains pageoptions information.</param>
+        private string displaySyndicationNotice()
+        {
+
+            IPageAssemblyInstruction pgInstruction = PageAssemblyContext.Current.PageAssemblyInstruction;
+            // If AlternateContentVersions information is not in the instructions then do not create 
+            // the PageOptions box.
+            string[] acvKeys = pgInstruction.AlternateContentVersionsKeys;
+            string isSyndicated = "false";
+            if (acvKeys != null)
+            {
+                if (pgInstruction.AlternateContentVersionsKeys.Contains("syndicated"))
+                {
+                    isSyndicated = "true";
+                }
+            }
+            return isSyndicated;
+        }
+    }
+}
