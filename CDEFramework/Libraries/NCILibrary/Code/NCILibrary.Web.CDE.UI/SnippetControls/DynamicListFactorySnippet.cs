8c5911e29dc1413708d25cd046ff4e108e7aedc7
Dion D'aquino <daquinohd@users.noreply.github.com>
2015-02-10 16:37:21 +0000

    Added initial instance of DynamicListFactorySnippet.cs
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/devonrex-it2@9368 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/SnippetControls/DynamicListFactorySnippet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NCI.Web.CDE.UI.SnippetControls
+{
+    class DynamicListFactorySnippet
+    {
+    }
+}
