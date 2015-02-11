8738c608d3880bf5f53979ce48e2074b8b9acb8d
Dion D'aquino <daquinohd@users.noreply.github.com>
2015-02-11 15:22:37 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/devonrex-it2@9420 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/SnippetControls/DynamicListDescImgDate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ComponentModel;
+using NCI.Web.CDE.Modules;
+using NCI.DataManager;
+using NCI.Web.UI.WebControls;
+
+namespace NCI.Web.CDE.UI.SnippetControls
+{
+    [DefaultProperty("Text")]
+    [ToolboxData("<{0}:DynamicListDescImgDate runat=server></{0}:DynamicListDescImgDate>")]
+    public class DynamicListDescImgDate : BaseSearchSnippet
+    {
+        override protected SearchList SearchList
+        {
+            get
+            {
+                if (base.SearchList == null)
+                {
+                    base.SearchList = ModuleObjectFactory<DynamicList>.GetModuleObject(SnippetInfo.Data);
+
+                    //This is completely dirty and really a hack, but it gets this done.  This should be
+                    //fixed in a future release. --BryanP 2/10/2015
+                    base.SearchList.ResultsTemplate = @" 
+                        #foreach($resultItem in $DynamicSearch.Results)
+	                        desc, image # $resultItem.RecNumber<br />
+                        #end
+                    ";
+                }
+                return base.SearchList;
+            }
+        }
+    }
+}
