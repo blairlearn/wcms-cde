9b7662b4073c257a8a3e30d3bbfb584bc915fb1e
Dion D'aquino <daquinohd@users.noreply.github.com>
2015-03-02 17:48:47 +0000

    Created helper class for the dynamic list strings - string literal is getting too bulky to keep in five separate classes.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/devonrex-it3@9780 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/SnippetControls/DynamicListHelper.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ComponentModel;
+using NCI.Web.CDE.Modules;
+using NCI.DataManager;
+using NCI.Web.UI.WebControls;
+
+namespace NCI.Web.CDE.UI.SnippetControls
+{
+    public class DynamicListHelper : BaseSearchSnippet
+    {
+        public string languageStrings()
+        {
+            String pageLanguage = @"
+			#set($videoContent = ""Video"")##
+			#set($carouselContent = ""Video Playlist"")##
+			#set($infographicContent = ""Infographic"")##
+			#set($fileContent = ""File"")##
+			#set($postedString = ""Posted"")##
+			#set($updatedString = ""Updated"")##
+			#set($reviewedString = ""Reviewed"")##
+            #set($newsString = ""All NCI news"")##
+			";
+            if (PageAssemblyContext.Current.PageAssemblyInstruction.GetField("Language") == "es")
+            {
+                pageLanguage = @"
+				#set($videoContent = ""Video"")##
+				#set($carouselContent = ""Lista de reproducci&oacute;n"")##
+				#set($infographicContent = ""Infograf&iacute;a"")##
+    			#set($fileContent = ""Fila"")##
+				#set($postedString = ""Publicaci&oacute;n"")##
+				#set($updatedString = ""Actualizaci&oacute;n"")##
+				#set($reviewedString = ""Revisi&oacute;n"")##
+                #set($newsString = ""Todas las noticias del NCI"")##
+				";
+            }
+            return pageLanguage;
+        }
+
+        //This is completely dirty and really a hack, but it gets this done.  This should be
+        //fixed in a future release. --BryanP 2/10/2015
+        public string openList()
+        {
+            string open = @"
+            <ul class=""list no-bullets"">##
+            #foreach($resultItem in $DynamicSearch.Results)
+                <li class=""general-list-item general"">##";
+            return open;
+        }
+
+        public string imageString()
+        {
+            string image = @"
+                ##
+                ## Display image
+                ##
+                <div class=""list-item-image image container"">##
+                    #if($resultItem.ThumbnailURL)## 
+                        <img src=""$resultItem.ThumbnailURL"" class=""item-image image"" align=""left"">
+                    #else##
+                        &nbsp;##
+                    #end##
+                </div>##";
+            return image;
+        }
+
+        public string titleString()
+        {
+            string title = @"
+                    <div class=""title-and-desc title desc container"">##
+                        ##
+                        ## Display title
+                        ##
+                        <a href=""$resultItem.Href"" onclick=""NCIAnalytics.SearchResults(this,$resultItem.RecNumber);"" class=""title"">
+                            #if($resultItem.ContentType == ""rx:nciFile"")##
+                                $fileContent  ##
+                            #end##
+                            $resultItem.LongTitle
+                            #if($resultItem.ContentType == ""rx:gloVideo"")##
+                                ($videoContent)##
+                            #elseif($resultItem.ContentType == ""rx:gloVideoCarousel"")##
+                                ($carouselContent)##
+                            #elseif($resultItem.ContentType == ""rx:cgvInfographic"")##
+                                ($infographicContent)##
+                            #end##
+                        </a>##
+                        <p class=""description date"">";
+            return title;
+        }
+
+        public string dateString()
+        {
+            string dates = @"
+                        ##
+                        ## Display dates
+                        ##
+
+                            <span class=""date"">
+								#if ($resultItem.DateDisplayMode == 1)##
+									($postedString: $resultItem.PostedDate) ##	
+								#elseif ($resultItem.DateDisplayMode == 2)##
+									($updatedString: $resultItem.UpdatedDate) ##	
+								#elseif ($resultItem.DateDisplayMode == 3)##
+									($postedString: $resultItem.PostedDate, $updatedString: $resultItem.UpdatedDate) ##
+								#elseif ($resultItem.DateDisplayMode == 4)##
+									($reviewedString: $resultItem.ReviewedDate) ##	
+								#elseif ($resultItem.DateDisplayMode == 5)##
+									($postedString: $resultItem.PostedDate, $reviewedString: $resultItem.ReviewedDate) ##
+								#elseif ($resultItem.DateDisplayMode == 6)##
+									($updatedString: $resultItem.UpdatedDate, $reviewedString: $resultItem.ReviewedDate) ##
+								#elseif ($resultItem.DateDisplayMode == 7)##
+									($postedString: $resultItem.PostedDate, $updatedString: $resultItem.UpdatedDate, $reviewedString: $resultItem.ReviewedDate) ##
+								#end
+							</span><br/>##";
+            return dates;
+        }
+
+        public string descString()
+        {
+            string desc = @"
+                            ##
+                            ## Display description
+                            ##
+                            <span>##
+                                #if($resultItem.LongDescription)##
+                                    $resultItem.LongDescription ##
+                                #else##
+                                    &nbsp;##
+                                #end##
+                            </span>##";
+            return desc;
+        }
+
+        public string closeList()
+        {
+            string close = @"
+                        </p>
+                    </div>
+                </li>
+            #end
+            </ul>
+            ";
+            return close;
+        }
+
+        public string closeNews()
+        {
+            string close = @"
+                        </p>
+                    </div>
+                </li>
+            #end
+            <li><a class=""arrow-link news-footer"" href=""#"">$newsString</a></li>
+            </ul>
+            ";
+            return close;
+        }
+    }
+}
--- CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/SnippetControls/DynamicListNoDescNoImgDate.cs
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/SnippetControls/DynamicListNoDescNoImgDate.cs
@@ -22,79 +22,15 @@ namespace NCI.Web.CDE.UI.SnippetControls
             {
                 if (base.SearchList == null)
                 {
+                    DynamicListHelper helper = new DynamicListHelper();
                     base.SearchList = ModuleObjectFactory<DynamicList>.GetModuleObject(SnippetInfo.Data);
-
-                    //This is completely dirty and really a hack, but it gets this done.  This should be
-                    //fixed in a future release. --BryanP 2/10/2015
-
-                    //Set content type and date labels according to locale of page, not the search results
-                    String pageLanguage = @"
-					#set($videoContent = ""Video"")##
-					#set($carouselContent = ""Video Playlist"")##
-					#set($infographicContent = ""Infographic"")##
-					#set($postedString = ""Posted"")##
-					#set($updatedString = ""Updated"")##
-					#set($reviewedString = ""Reviewed"")##
-					";
-                    if (PageAssemblyContext.Current.PageAssemblyInstruction.GetField("Language") == "es")
-                    {
-                        pageLanguage = @"
-						#set($videoContent = ""Video"")##
-						#set($carouselContent = ""Lista de reproducci&oacute;n"")##
-						#set($infographicContent = ""Infograf&iacute;a"")##
-						#set($postedString = ""Publicaci&oacute;n"")##
-						#set($updatedString = ""Actualizaci&oacute;n"")##
-						#set($reviewedString = ""Revisi&oacute;n"")##
-						";
-                    }
-
-                    String listTitle = "<h3>" + base.SearchList.SearchTitle + "</h3>";
-                    base.SearchList.ResultsTemplate = base.SearchList.ResultsTemplate = pageLanguage + listTitle +
-                    @"
-                    <ul class=""list no-bullets"">##
-                    #foreach($resultItem in $DynamicSearch.Results)
-                        <li class=""general-list-item general"">##
-                            <div class=""title-and-desc title desc container"">##
-                                ##
-                                ## Display title
-                                ##
-                                <a href=""$resultItem.Href"" onclick=""NCIAnalytics.SearchResults(this,$resultItem.RecNumber);"" class=""title"">
-                                    $resultItem.LongTitle
-                                    #if($resultItem.ContentType == ""rx:gloVideo"")##
-                                        ($videoContent)##
-                                    #elseif($resultItem.ContentType == ""rx:gloVideoCarousel"")##
-                                        ($carouselContent)##
-                                    #elseif($resultItem.ContentType == ""rx:cgvInfographic"")##
-                                        ($infographicContent)##
-                                    #end##
-                                </a>##
-	                            ##
-	                            ## Display dates
-    	                        ##
-                                <p class=""description"">
-                                    <span class=""date"">
-										#if ($resultItem.DateDisplayMode == 1)##
-											($postedString: $resultItem.PostedDate) ##	
-										#elseif ($resultItem.DateDisplayMode == 2)##
-											($updatedString: $resultItem.UpdatedDate) ##	
-										#elseif ($resultItem.DateDisplayMode == 3)##
-											($postedString: $resultItem.PostedDate, $updatedString: $resultItem.UpdatedDate) ##
-										#elseif ($resultItem.DateDisplayMode == 4)##
-											($reviewedString: $resultItem.ReviewedDate) ##	
-										#elseif ($resultItem.DateDisplayMode == 5)##
-											($postedString: $resultItem.PostedDate, $reviewedString: $resultItem.ReviewedDate) ##
-										#elseif ($resultItem.DateDisplayMode == 6)##
-											($updatedString: $resultItem.UpdatedDate, $reviewedString: $resultItem.ReviewedDate) ##
-										#elseif ($resultItem.DateDisplayMode == 7)##
-											($postedString: $resultItem.PostedDate, $updatedString: $resultItem.UpdatedDate, $reviewedString: $resultItem.ReviewedDate) ##
-										#end
-									</span>##
-                                </p>
-                            </div>
-                        </li>
-                    #end
-                    </ul>
-                    ";
+                    base.SearchList.ResultsTemplate = base.SearchList.ResultsTemplate =
+                    helper.languageStrings() +
+                    "<h3>" + base.SearchList.SearchTitle + "</h3>" +
+                    helper.openList() +
+                    helper.titleString() +
+                    helper.dateString() +
+                    helper.closeList();
                 }
                 return base.SearchList;
             }
