bd204d176bb8cb0037f385ddd81b589d30467311
Blair Learn <blairlearn@users.noreply.github.com>
2015-03-13 20:48:29 +0000

    Restore erroneously deleted configuration classes.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/hotfix-6.7.2@10204 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/HttpModules/SimpleRedirector/Configuration/SimpleRedirectorConfigurationSection.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+
+namespace NCI.Web.CDE.SimpleRedirector.Configuration
+{
+    public class SimpleRedirectorConfigurationSection : ConfigurationSection
+    {
+        public static SimpleRedirectorConfigurationSection Get()
+        {
+            SimpleRedirectorConfigurationSection config = (SimpleRedirectorConfigurationSection)ConfigurationManager.GetSection("nci/web/simpleRedirection");
+            return config;
+        }
+
+        [ConfigurationProperty("dataSource")]
+        public DataSourceConfigurationElement DataSource
+        {
+            get { return (DataSourceConfigurationElement)base["dataSource"]; }
+        }
+    }
+}
