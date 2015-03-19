9dd2c054280806004924c914808f4f51b55fd3c4
Blair Learn <blairlearn@users.noreply.github.com>
2015-03-19 19:59:14 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/trunk@10440 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/HttpModules/SimpleRedirector/Log.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+using NCI.Logging;
+
+namespace NCI.Web.CDE.SimpleRedirector
+{
+    // Wrapper for the NCI Logging classes to provide a simple interface,
+    // similar to log4net.
+    internal class Log
+    {
+        private String classname;
+
+        public Log(Type classType)
+        {
+            classname = classType.FullName;
+        }
+
+        public void trace(String message)
+        {
+            Logger.LogError(classname, message, NCIErrorLevel.Info);
+        }
+
+        public void trace(String message, Exception ex)
+        {
+            Logger.LogError(classname, message, NCIErrorLevel.Info, ex);
+        }
+
+        public void debug(String message)
+        {
+            Logger.LogError(classname, message, NCIErrorLevel.Debug);
+        }
+
+        public void debug(String message, Exception ex)
+        {
+            Logger.LogError(classname, message, NCIErrorLevel.Debug, ex);
+        }
+
+        public void warning(string message)
+        {
+            Logger.LogError(classname, message, NCIErrorLevel.Warning);
+        }
+
+        public void warning(string message, Exception ex)
+        {
+            Logger.LogError(classname, message, NCIErrorLevel.Warning, ex);
+        }
+
+        public void error(string message)
+        {
+            Logger.LogError(classname, message, NCIErrorLevel.Error);
+        }
+
+        public void error(string message, Exception ex)
+        {
+            Logger.LogError(classname, message, NCIErrorLevel.Error, ex);
+        }
+
+        public void fatal(string message)
+        {
+            Logger.LogError(classname, message, NCIErrorLevel.Critical);
+        }
+
+        public void fatal(string message, Exception ex)
+        {
+            Logger.LogError(classname, message, NCIErrorLevel.Critical, ex);
+        }
+    }
+}
---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/HttpModules/SimpleRedirector/SimpleRedirect.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Web;
+
+using NCI.Web.CDE.SimpleRedirector;
+using NCI.Web.CDE.SimpleRedirector.Configuration;
+
+namespace NCI.Web.CDE
+{
+    /// <summary>
+    /// Simple tool for redirecting URLs based on a comma-delimited list of old and new URLs.
+    /// </summary>
+    public class SimpleRedirect : IHttpModule
+    {
+        static Log log = new Log(typeof(SimpleRedirect));
+
+        #region IHttpModule Members
+
+        public void Dispose()
+        {
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += new EventHandler(OnBeginRequest);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// The guts of the HTTP module.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnBeginRequest(object sender, EventArgs e)
+        {
+            HttpContext context = ((HttpApplication)sender).Context;
+
+            // Get absolute path of the request URL as pretty URL
+            String url = context.Server.UrlDecode(context.Request.Url.AbsolutePath);
+
+            // Can we ignore this file?
+            if (Utility.IgnoreWebResource(url))
+                return;
+
+            // If the PageAssemblyInstruction is not null, then a page is being served
+            // and we can ignore this request.
+            if (PageAssemblyContext.Current.PageAssemblyInstruction != null)
+                return;
+
+            String redirectUrl = GetRedirectUrl(url, context);
+
+            if(!String.IsNullOrEmpty(redirectUrl))
+                DoPermanentRedirect(context.Response, redirectUrl);
+        }
+
+        /// <summary>
+        /// Private helper to find the redirection URL mapped to a given input URL.
+        /// </summary>
+        /// <param name="url">The URL to examine for a replacement</param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private String GetRedirectUrl(String url, HttpContext context)
+        {
+            String redirect = null;
+
+            SimpleRedirectorConfigurationSection config = SimpleRedirectorConfigurationSection.Get();
+
+            String datafile = HttpContext.Current.Server.MapPath(config.DataSource.DataFile);
+            RedirectionMap urlMap = RedirectionMap.GetMap(datafile, context);
+
+            if (urlMap.Contains(url))
+            {
+                redirect = urlMap[url];
+                log.debug(String.Format("Url '{0}' found; redirects to '{1}'.", url, redirect));
+            }
+            else
+            {
+                log.debug(String.Format("No match found for url '{0}.", url));
+            }
+            return redirect;
+        }
+
+        /// <summary>
+        /// Clears the Response text, issues an HTTP redirect using status 301, and ends
+        /// the current request.
+        /// </summary>
+        /// <param name="Response">The current response object.</param>
+        /// <param name="url">The redirection's target URL.</param>
+        /// <remarks>Response.Redirect() issues its redirect with a 301 (temporarily moved) status code.
+        /// We want these redirects to be permanent so search engines will link to the new
+        /// location. Unfortunately, HttpResponse.RedirectPermanent() isn't implemented until
+        /// at version 4.0 of the .NET Framework.</remarks>
+        /// <exception cref="ThreadAbortException">Called when the redirect takes place and the current
+        /// request is ended.</exception>
+        private void DoPermanentRedirect(HttpResponse Response, String url)
+        {
+            Response.Clear();
+            Response.Status = "301 Moved Permanently";
+            Response.AddHeader("Location", url);
+            Response.End();
+        }
+    }
+}
