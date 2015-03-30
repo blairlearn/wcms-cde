631d7e5e133c31982f9d087a79af90152fd7ab3e
Dion D'aquino <daquinohd@users.noreply.github.com>
2015-03-30 20:12:48 +0000

    Added async attribute to Page Options addthis
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/devonrex-it3@10938 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/PageOptions/PageOptionsControl.cs
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/PageOptions/PageOptionsControl.cs
@@ -347,6 +347,7 @@ namespace NCI.Web.CDE.UI.WebControls
         public override void RenderEndTag(HtmlTextWriter writer)
         {
             base.RenderEndTag(writer);
+            writer.AddAttribute("async", "async");
             writer.AddAttribute(HtmlTextWriterAttribute.Src, "http://s7.addthis.com/js/250/addthis_widget.js#pubid=xa-4ed7cc9f006efaac");
             writer.AddAttribute(HtmlTextWriterAttribute.Type, "text/javascript");
             writer.RenderBeginTag(HtmlTextWriterTag.Script);
