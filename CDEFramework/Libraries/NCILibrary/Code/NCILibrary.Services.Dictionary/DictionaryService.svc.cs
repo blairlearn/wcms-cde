04339f823d0bc51981ccda01de6b84d9fef1ef7b
Blair Learn <blairlearn@users.noreply.github.com>
2015-07-13 16:16:26 +0000

    Remove IDictionaryService interface, mark service contract directly in the DictionaryService class.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/feline-it1@12509 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Services.Dictionary/DictionaryService.svc.cs
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Services.Dictionary/DictionaryService.svc.cs
@@ -14,13 +14,28 @@ namespace NCI.Services.Dictionary
 {
 
     /// <summary>
-    ///    
+    /// Implements the public methods for the dictionary service and defines the rules for
+    /// mapping the parts of the dictionary URLs into method parameters.
     /// </summary>
     /// <remarks>
     /// Changing the class name "DictionaryService" here requires that you also update the reference
-    /// to "DictionaryService" in Web.config and the associated .svc file.
+    /// to "DictionaryService" in the Web.config and the associated .svc file.
+    /// 
+    /// NOTE: A possibly important implementation detail: These methods take enums as parameters.
+    /// 
+    /// The plus side is, unlike strings, WCF will guarantee that the parameter values are valid,
+    /// thus simplifying the validation code.
+    /// 
+    /// There are some recommendations against using enums in web services,
+    /// http://www.25hoursaday.com/weblog/2005/08/31/WhyYouShouldAvoidUsingEnumeratedTypesInXMLWebServices.aspx
+    /// but these revolve around using them as outputs (A calling routine might not handle a newly added
+    /// enum value correctly).
+    /// 
+    /// These parameters are input-only.     * 
+    /// 
     /// </remarks>
-    public class DictionaryService : IDictionaryService
+    [ServiceContract]
+    public class DictionaryService
     {
         const String API_VERSION = "v1";
 
@@ -73,6 +88,25 @@ namespace NCI.Services.Dictionary
 
         #endregion
 
+        /// <summary>
+        /// Retrieves a single dictionary term based on its specific term ID.
+        /// </summary>
+        /// <param name="termId">The ID of the term to be retrieved</param>
+        /// <param name="dictionary">The dictionary to retreive the term from.
+        ///     Valid values are
+        ///        term - Dictionary of Cancer Terms
+        ///        drug - Drug Dictionary
+        ///        genetic - Dictionary of Genetics Terms
+        /// </param>
+        /// <param name="language">The term's desired language.
+        ///     Supported values are:
+        ///         en - English
+        ///         es - Spanish
+        /// </param>
+        /// <returns></returns>
+        [WebGet(ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "v1/GetTerm?termID={termId}&language={language}&dictionary={dictionary}")]
+        [OperationContract]
         public TermReturn GetTerm(String termId, DictionaryType dictionary, Language language)
         {
             InputValidator validator = new InputValidator(dictionary, language);
@@ -88,6 +122,34 @@ namespace NCI.Services.Dictionary
                 return validator.GetInvalidResult();
         }
 
+
+        /// <summary>
+        /// Performs a search for terms with names matching searchText.
+        /// </summary>
+        /// <param name="searchText">text to search for.</param>
+        /// <param name="searchType">The type of search to perform.
+        ///     Valid values are:
+        ///         Begins - Search for terms beginning with searchText.
+        ///         Contains - Search for terms containing searchText.
+        ///         Magic - Search for terms beginning with searchText, followed by those containing searchText.
+        /// </param>
+        /// <param name="offset">Offset into the list of matches for the first result to return.</param>
+        /// <param name="maxResults">The maximum number of results to return. Must be at least 10.</param>
+        /// <param name="dictionary">The dictionary to retreive the term from.
+        ///     Valid values are
+        ///        term - Dictionary of Cancer Terms
+        ///        drug - Drug Dictionary
+        ///        genetic - Dictionary of Genetics Terms
+        /// </param>
+        /// <param name="language">The term's desired language.
+        ///     Supported values are:
+        ///         en - English
+        ///         es - Spanish
+        /// </param>
+        /// <returns></returns>
+        [WebGet(ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "v1/search?searchText={searchText}&searchType={searchType}&offset={offset}&maxResults={maxResults}&language={language}&dictionary={dictionary}")]
+        [OperationContract]
         public SearchReturn Search(String searchText, SearchType searchType, int offset, int maxResults, DictionaryType dictionary, Language language)
         {
             DictionaryManager mgr = new DictionaryManager();
@@ -96,6 +158,27 @@ namespace NCI.Services.Dictionary
             return ret;
         }
 
+
+        /// <summary>
+        /// Performs a search for terms with names matching searchText.  This alternate version is
+        /// invoked by a POST request.
+        /// </summary>
+        /// <param name="paramBlock"></param>
+        /// <param name="dictionary">The dictionary to retreive the term from.
+        ///     Valid values are
+        ///        term - Dictionary of Cancer Terms
+        ///        drug - Drug Dictionary
+        ///        genetic - Dictionary of Genetics Terms
+        /// </param>
+        /// <param name="language">The term's desired language.
+        ///     Supported values are:
+        ///         en - English
+        ///         es - Spanish
+        /// </param>
+        /// <returns></returns>
+        [WebInvoke(ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "v1/SearchPost")]
+        [OperationContract]
         public SearchReturn SearchPost(SearchInputs paramBlock)
         {
             // Convert paramBlock to individual values and re-use the GET version of Search.
