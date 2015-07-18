ccbda1ac6583d1233d4c3e7c3e69a15e0d174501
Dion D'aquino <daquinohd@users.noreply.github.com>
2015-07-18 17:19:29 +0000

    //tracker.nci.nih.gov/browse/OCEPROJECT-259 - Moved WA methods into WebAnalyticsInfo.cs
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/feline-it1@12588 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/PageAssembly/WebAnalyticsInfo.cs
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/PageAssembly/WebAnalyticsInfo.cs
@@ -1,14 +1,19 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Collections.Generic;
 using System.Xml.Serialization;
 using System.Xml.Schema;
 using System.Configuration;
 using NCI.Logging;
+using NCI.Web.CDE.WebAnalytics;
+
 namespace NCI.Web.CDE
 {
     /// <summary>
     /// Defines custom web analytics values set on folders or content types.
     /// </summary>
-    public class WebAnalyticsInfo
+    public class WebAnalyticsInfo : BasePageAssemblyInstruction
     {
         /// <summary>
         /// Gets comma-separated list of analytics channel namess
@@ -86,5 +91,313 @@ namespace NCI.Web.CDE
             RemoveParentProps = false;
             RemoveParentEvars = false;
         }
-    }
+
+
+
+        /// <summary>
+        /// Load the analytics that have been set on the navon. If there is no value,
+        /// recurse through parents until a value is found or until root is reached.
+        /// </summary>
+        protected WebAnalyticsInfo LoadCustomAnalytics(SectionDetail section)
+        {
+            WebAnalyticsInfo wai = section.WebAnalyticsInfo;
+            if (wai == null)
+            {
+                if (section.ParentPath != null)
+                {
+                    wai = section.Parent.WebAnalyticsInfo;
+                    LoadCustomAnalytics(section.Parent);
+                }
+                else return null;
+            }
+            return wai;
+        }
+
+        /// <summary>
+        /// Create list of WebAnalyticsInfo objects for current item and all ancestors
+        /// </summary>
+        protected List<WebAnalyticsInfo> WaiAll = new List<WebAnalyticsInfo> { };
+        protected List<WebAnalyticsInfo> LoadAllCustomAnalytics(SectionDetail section)
+        {
+            WaiAll.Add(section.WebAnalyticsInfo);
+            if (section.ParentPath != null)
+            {
+                LoadAllCustomAnalytics(section.Parent);
+            }
+            else
+            {
+                return null;
+            }
+            return WaiAll;
+        }
+
+        /// <summary>
+        /// Load the report suite(s) that have been set on this navon. If there is no value,
+        /// recurse through parents until a value is found. Suites set on a loweer folder 
+        /// overwrite parents' suites.
+        /// </summary>
+        protected string LoadSuite(SectionDetail section)
+        {
+            try
+            {
+                WebAnalyticsInfo wai = LoadCustomAnalytics(section);
+                string suite = wai.WAReportSuites;
+
+                if (String.IsNullOrEmpty(suite))
+                {
+                    wai = LoadCustomAnalytics(section.Parent);
+                    suite = LoadSuite(section.Parent);
+                }
+                return suite;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("CDE:SinglePageAssemblyInstruction.cs:LoadSuite()",
+                      "Exception encountered while retrieving web analytics suites.",
+                      NCIErrorLevel.Error, ex);
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Load the content group that has been set on this navon.
+        /// </summary>
+        protected string LoadContentGroup(SectionDetail section)
+        {
+            try
+            {
+                WebAnalyticsInfo wai = LoadCustomAnalytics(section);
+                string group = wai.WAContentGroups;
+
+                if (String.IsNullOrEmpty(group))
+                {
+                    group = "";
+                }
+                return group;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("CDE:SinglePageAssemblyInstruction.cs:LoadContentGroup()",
+                      "Exception encountered while retrieving web analytics content group.",
+                      NCIErrorLevel.Error, ex);
+                return "";
+            }
+        }
+
+
+        protected List<string> _events = new List<string> { };
+        protected List<string> LoadEvents(SectionDetail section)
+        {
+            try
+            {
+                string key = "";
+                bool removeParents = false;
+
+                WaiAll.Clear();
+                List<WebAnalyticsInfo> waInfos = LoadAllCustomAnalytics(section);
+                foreach (WebAnalyticsInfo waInfo in waInfos)
+                {
+                    if (removeParents == false)
+                    {
+                        WebAnalyticsCustomVariableOrEvent[] waEvents = waInfo.WAEvents;
+                        foreach (WebAnalyticsCustomVariableOrEvent waEvent in waEvents)
+                        {
+                            key = waEvent.Key;
+                            _events.Add(key);
+                        }
+                        removeParents = waInfo.RemoveParentEvents;
+                    }
+                }
+                return _events;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("CDE:SinglePageAssemblyInstruction.cs:LoadProps()",
+                      "Exception encountered while retrieving web analytics custom props",
+                      NCIErrorLevel.Error, ex);
+                return null;
+            }
+        }
+
+
+        protected Dictionary<string, string> _props = new Dictionary<string, string> { };
+        protected Dictionary<string, string> LoadProps(SectionDetail section)
+        {
+            try
+            {
+                string key = "";
+                string value = "";
+                bool removeParents = false;
+
+                WaiAll.Clear();
+                List<WebAnalyticsInfo> waInfos = LoadAllCustomAnalytics(section);
+                foreach (WebAnalyticsInfo waInfo in waInfos)
+                {
+                    if (removeParents == false)
+                    {
+                        WebAnalyticsCustomVariableOrEvent[] waProps = waInfo.WAProps;
+                        foreach (WebAnalyticsCustomVariableOrEvent waProp in waProps)
+                        {
+                            key = waProp.Key;
+                            value = waProp.Value;
+                            if (!_props.ContainsKey(key))
+                            {
+                                _props.Add(key, value);
+                            }
+                        }
+                        removeParents = waInfo.RemoveParentProps;
+                    }
+                }
+                return _props;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("CDE:SinglePageAssemblyInstruction.cs:LoadProps()",
+                      "Exception encountered while retrieving web analytics custom props",
+                      NCIErrorLevel.Error, ex);
+                return null;
+            }
+        }
+
+        protected Dictionary<string, string> _evars = new Dictionary<string, string> { };
+        protected Dictionary<string, string> LoadEvars(SectionDetail section)
+        {
+            try
+            {
+                string key = "";
+                string value = "";
+                bool removeParents = false;
+
+                WaiAll.Clear();
+                List<WebAnalyticsInfo> waInfos = LoadAllCustomAnalytics(section);
+                foreach (WebAnalyticsInfo waInfo in waInfos)
+                {
+                    if (removeParents == false)
+                    {
+                        WebAnalyticsCustomVariableOrEvent[] waEvars = waInfo.WAEvars;
+                        foreach (WebAnalyticsCustomVariableOrEvent waEvar in waEvars)
+                        {
+                            key = waEvar.Key;
+                            value = waEvar.Value;
+                            if (!_evars.ContainsKey(key))
+                            {
+                                _evars.Add(key, value);
+                            }
+                        }
+                        removeParents = waInfo.RemoveParentEvars;
+                    }
+                }
+                return _evars;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("CDE:SinglePageAssemblyInstruction.cs:LoadEvars()",
+                      "Exception encountered while retrieving web analytics custom evars",
+                      NCIErrorLevel.Error, ex);
+                return null;
+            }
+        }
+
+        public string GetCustomEvents(string cEvent)
+        {
+            string customEvent = "";
+            foreach (WebAnalyticsOptions.Events ev in Enum.GetValues(typeof(WebAnalyticsOptions.Events)))
+            {
+                if (cEvent == ev.ToString())
+                    customEvent = cEvent;
+            }
+            return customEvent;
+        }
+
+
+        public string GetCustomProps(KeyValuePair<string, string> cProp)
+        {
+            string customProp = "";
+            foreach (WebAnalyticsOptions.Props prop in Enum.GetValues(typeof(WebAnalyticsOptions.Props)))
+            {
+                if (cProp.Key == prop.ToString())
+                    customProp = cProp.Key;
+            }
+            return customProp;
+        }
+
+
+        public string GetCustomEvars(KeyValuePair<string, string> cEvar)
+        {
+            string customEvar = "";
+            foreach (WebAnalyticsOptions.eVars evar in Enum.GetValues(typeof(WebAnalyticsOptions.eVars)))
+            {
+                if (cEvar.Key == evar.ToString())
+                    customEvar = cEvar.Key;
+            }
+            return customEvar;
+        }
+
+        public void RegisterCustomWebAnalytics(SectionDetail detail)
+        {
+
+            // Get the section details for the content item, then load any custom analytics
+            // values from it or its parents
+            WebAnalyticsInfo wai = LoadCustomAnalytics(detail);
+            string suite = LoadSuite(detail);
+            string group = LoadContentGroup(detail);
+            List<String> eventsList = LoadEvents(detail);
+            Dictionary<string, string> props = LoadProps(detail);
+            Dictionary<string, string> evars = LoadEvars(detail);
+
+
+            try
+            {
+                foreach (string evn in eventsList)
+                {
+
+                    if (!String.IsNullOrEmpty(evn))
+                    {
+                        SetWebAnalytics(evn, waField =>
+                        {
+                            waField.Value = "";
+                        });
+                    }
+                }
+
+
+                // Register custom props entered on navon
+                foreach (KeyValuePair<string, string> pr in props)
+                {
+                    String propKey = GetCustomProps(pr);
+                    String propValue = pr.Value;
+
+                    if (!String.IsNullOrEmpty(propKey))
+                    {
+                        SetWebAnalytics(propKey, waField =>
+                        {
+                            waField.Value = propValue;
+                        });
+                    }
+                }
+
+                // Register custom evars entered on navon
+                foreach (KeyValuePair<string, string> evr in evars)
+                {
+                    String evarKey = GetCustomEvars(evr);
+                    String evarValue = evr.Value;
+
+                    if (!String.IsNullOrEmpty(evarKey))
+                    {
+                        SetWebAnalytics(evarKey, waField =>
+                        {
+                            waField.Value = evarValue;
+                        });
+                    }
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                Logger.LogError("SinglePageAssemblyInstruction.cs:RegisterWebAnalyticsFieldFilters()",
+                    "WebAnalyticsInfo is missing from SectionDetails XML", NCIErrorLevel.Error, ex);
+                return;
+            }
+        } 
+
+    } 
 }
