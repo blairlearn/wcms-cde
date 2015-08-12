2690e5d9575633dba16793483aa295ea1e3ebd94
Blair Learn <blairlearn@users.noreply.github.com>
2015-08-12 19:15:23 +0000

    Documentation!
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/feline-it2@12889 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Services.Dictionary/BusinessObjects/TermReturn.cs
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Services.Dictionary/BusinessObjects/TermReturn.cs
@@ -11,9 +11,22 @@ namespace NCI.Services.Dictionary.BusinessObjects
     [DataContract()]
     public class TermReturn
     {
+        /// <summary>
+        /// Metadata about the term being returned.
+        /// </summary>
         [DataMember(Name = "meta")]
         public TermReturnMeta Meta { get; set; }
 
+        /// <summary>
+        /// String containg the JSON structure for a single term definition,
+        /// as rendred by GateKeeper.
+        /// </summary>
+        /// <remarks>
+        /// When the contents of Term are retrieved via a web service call, the default serialization
+        /// renders value as a single string with leading and trailing quotation marks, causing it to be treated
+        /// as an individual JSON values rather than a data structure.  Changing this behavior will likely
+        /// require custom serialization and formatting.
+        /// </remarks>
         [DataMember(Name = "term")]
         public String Term { get; set; }
     }
