fd2dc2e641ba335c16c80db45d85ffc97c5238bc
ruckerad <webcommauto@users.noreply.github.com>
2015-08-24 18:35:43 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/feline-it2@13024 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.Dictionary/BusinessObjects/DictionaryExpansion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace NCI.Web.Dictionary.BusinessObjects
+{
+    [DataContract()]
+    public class DictionaryExpansion
+    {
+        /// <summary>
+        ///  The Term's ID
+        /// </summary>
+        [DataMember(Name = "id")]
+        public int ID { get; set; }
+
+        /// <summary>
+        /// The Term name (or alias) that matched the expansion
+        /// </summary>
+        [DataMember(Name = "matched")]
+        public String MatchedTerm { get; set; }
+
+        /// <summary>
+        /// Dictionary Terms
+        /// </summary>
+        [DataMember(Name = "term")]
+        public DictionaryTerm Term { get; set; }
+    }
+}
---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.Dictionary/BusinessObjects/ExpandReturn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace NCI.Web.Dictionary.BusinessObjects
+{
+    /// <summary>
+    /// Outermost data structure for returns from Expand().
+    /// </summary>
+    [DataContract()]
+    public class ExpandReturn
+    {
+        public ExpandReturn()
+        {
+            // Force collection to be non-null.
+            Result = new DictionaryExpansion[] { };
+        }
+
+        /// <summary>
+        /// Metadata about the expansion search results.
+        /// </summary>
+        [DataMember(Name = "meta")]
+        public ExpandReturnMeta Meta { get; set; }
+
+        /// <summary>
+        /// Array of DictionaryExpansion objects containg details of the individual terms which met the search criteria.
+        /// </summary>
+        [DataMember(Name = "result")]
+        public DictionaryExpansion[] Result { get; set; }
+    }
+}
