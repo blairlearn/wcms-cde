d3ac56cec6bfd9aa3a722c6824b9e7a5d2055125
Blair Learn <blairlearn@users.noreply.github.com>
2015-08-25 23:20:23 +0000

    Start work on a cheesy HTTP handler to make up for WCF not giving us an easy way to remove the quotation marks from around the string containing the term details.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/feline-it2@13050 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Services.Dictionary/DictionaryHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Web;
+
+namespace NCI.Services.Dictionary
+{
+    /// <summary>
+    /// Cheesy HTTP handler to make up for WCF not giving us an easy way to remove the quotation marks from
+    /// around the string containing the term details.
+    /// 
+    /// To configure this handler, go to the web.config and in the system.webServer / handlers section, add the new line:
+    /// 
+    ///     <add name="DictionaryServiceHandler" verb="GET" path="Dictionary.svc" type ="NCI.Services.Dictionary.DictionaryHandler, NCILibrary.Services.Dictionary"/>
+    /// 
+    /// In the Risk Assessment Tools, in the same section, be sure to add a matching
+    /// 
+    ///     <remove name="DictionaryServiceHandler" />
+    /// 
+    /// </summary>
+    public class DictionaryHandler : IHttpHandler
+    {
+        /// <summary>
+        /// </summary>
+        #region IHttpHandler Members
+
+        public bool IsReusable
+        {
+            // Return false in case your Managed Handler cannot be reused for another request.
+            // Usually this would be false in case you have some state information preserved per request.
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            // Get the Function by parsing context.Request.PathInfo
+            // Get the paramters from context.Request.QueryString
+            context.Response.Write("Blah, blah, blah.  Whatever.");
+        }
+
+        #endregion
+    }
+}
