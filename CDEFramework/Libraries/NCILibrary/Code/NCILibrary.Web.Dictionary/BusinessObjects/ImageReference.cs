095e3c18ec1411c330aabdb78182b18ddd2a2b0a
ruckerad <webcommauto@users.noreply.github.com>
2015-08-31 19:28:19 +0000

    readded datacontract because deserialization had different names
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/feline-it2@13136 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.Dictionary/BusinessObjects/ImageReference.cs
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.Dictionary/BusinessObjects/ImageReference.cs
@@ -6,12 +6,17 @@ using System.Web;
 
 namespace NCI.Web.Dictionary.BusinessObjects
 {
+    //This particular class needs Data Contract since the data from the database has different names than these
+    [DataContract()]
     public class ImageReference
     {
+        [DataMember(Name = "ref")]
         public string Filename { get; set; }
 
+        [DataMember(Name = "alt")]
         public string AltText { get; set; }
 
+        [DataMember(Name = "caption")]
         public string Caption { get; set; }
     }
 }
