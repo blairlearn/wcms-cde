8c7f7cb13fb00197d4e8f0ecbd47879b92ff55d6
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2015-09-14 19:37:32 +0000

    - Added in functionality to exclude items marked as DoNotIndex from appearing in the Sitemap for OCEPROJECT-3515 - Refactored the pretty URL fetching code. - Removed hardcoded link to www.cancer.gov so that sitemaps can be used on other DOC sites
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/feline-it3@13355 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/PageAssembly/InstructionSitemapUrlStore.cs
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE/PageAssembly/InstructionSitemapUrlStore.cs
@@ -13,6 +13,60 @@ namespace NCI.Web.CDE.PageAssembly
 {
     public class InstructionSitemapUrlStore : SitemapUrlStoreBase
     {
+        /// <summary>
+        /// Selects out the DoNotIndex property
+        /// </summary>
+        /// <param name="nav">The XPathNavigator representing the Instruction</param>
+        /// <param name="manager">The namespace manager for the Instruction</param>
+        /// <param name="instructionType">The type of the Instruction (SinglePageAssemblyInstruction or GenericFileInstruction)</param>
+        /// <returns>The value of the DoNotIndex node, or false if it does not exist</returns>
+        private bool DoNotIndex(XPathNavigator nav, XmlNamespaceManager manager, string instructionType)
+        {
+            string nodePath = String.Format("//cde:{0}/SearchMetadata/DoNotIndex", instructionType);
+
+            try
+            {
+                //Try and select it as a boolean returning the actual value as a boolean
+                return nav.SelectSingleNode(nodePath, manager).ValueAsBoolean;
+            }
+            catch { } //If it could not convert or be found, then continue.                        
+
+            return false; //Default is DoNotIndex = false;  This is in case the node is not found or not boolean text
+        }
+
+        /// <summary>
+        /// Gets the URL for this item
+        /// </summary>
+        /// <param name="nav">The XPathNavigator representing the Instruction</param>
+        /// <param name="manager">The namespace manager for the Instruction</param>
+        /// <param name="instructionType">The type of the Instruction (SinglePageAssemblyInstruction or GenericFileInstruction)</param>
+        /// <returns>The value of the PrettyUrl node with the base URL, or null if it does not exist</returns>
+        private string GetURL(XPathNavigator nav, XmlNamespaceManager manager, string instructionType)
+        {
+            string nodePath = String.Format("//cde:{0}/PrettyUrl", instructionType);
+            string url = null;
+
+            try
+            {
+                //Get out URL from node
+                url = nav.SelectSingleNode(nodePath, manager).Value;
+
+                if (url != null && url.Trim() != "")
+                {
+                    // If the URL is good, remove outer text and concatenate with base URL
+                    url = ContentDeliveryEngineConfig.CanonicalHostName.CanonicalUrlHostName.CanonicalHostName + url;
+                }
+                else
+                {
+                    // Otherwise, set the URL back to null
+                    url = null;
+                }
+            }
+            catch { }
+
+            return url;
+        }
+
         public override SitemapUrlSet GetSitemapUrls()
         {
             List<SitemapUrl> sitemapUrls = new List<SitemapUrl>();
@@ -32,21 +86,15 @@ namespace NCI.Web.CDE.PageAssembly
                 // Add CDE namespace to parse through document
                 XmlNamespaceManager manager = new XmlNamespaceManager(nav.NameTable);
                 manager.AddNamespace("cde", "http://www.example.org/CDESchema");
+                
+                //If this item is marked as DoNotIndex, then skip it.
+                if (DoNotIndex(nav, manager, "SinglePageAssemblyInstruction"))
+                    continue;
 
                 // Get pretty url from PrettyUrl node
-                if (nav.SelectSingleNode("//cde:SinglePageAssemblyInstruction/PrettyUrl", manager) != null)
-                {
-                    path = nav.SelectSingleNode("//cde:SinglePageAssemblyInstruction/PrettyUrl", manager).Value;
-                }
-                else
-                {
+                path = GetURL(nav, manager, "SinglePageAssemblyInstruction");
+                if (path == null)
                     continue;
-                }
-
-                // Remove outer text and concatenate with base URL
-                path.Replace("<![CDATA[", "");
-                path.Replace("]]>", "");
-                path = "http://www.cancer.gov" + path;
 
                 // Get content type and set priority accordingly
                 contentType = nav.SelectSingleNode("//cde:SinglePageAssemblyInstruction/ContentItemInfo/ContentItemType", manager).Value;
@@ -76,18 +124,14 @@ namespace NCI.Web.CDE.PageAssembly
                 XmlNamespaceManager manager = new XmlNamespaceManager(nav.NameTable);
                 manager.AddNamespace("cde", "http://www.example.org/CDESchema");
 
-                // Get pretty url from PrettyUrl node
-                if (nav.SelectSingleNode("//cde:GenericFileInstruction/PrettyUrl", manager) != null)
-                {
-                    path = nav.SelectSingleNode("//cde:GenericFileInstruction/PrettyUrl", manager).Value;
-                }
-                else
-                {
+                //If this item is marked as DoNotIndex, then skip it.
+                if (DoNotIndex(nav, manager, "GenericFileInstruction"))
                     continue;
-                }
 
-                // Remove outer text and concatenate with base URL
-                path = "http://www.cancer.gov" + path;
+                // Get pretty url from PrettyUrl node
+                path = GetURL(nav, manager, "GenericFileInstruction");
+                if (path == null)
+                    continue;
 
                 sitemapUrls.Add(new SitemapUrl(path, sitemapChangeFreq.always, 0.5));
             }
