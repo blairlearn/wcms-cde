ac710292908c9ac5e289fca2d72393286d754099
Dan Young <youngdr@users.noreply.github.com>
2016-05-11 01:07:06 +0000

    Removed clear link.
    https://tracker.nci.nih.gov/browse/WCMSCDE-67
    
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/Chronos@15604 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDESites/CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/ClinicalTrialsSearchTemplate.ascx.designer.cs
+++ CDESites/CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/ClinicalTrialsSearchTemplate.ascx.designer.cs
@@ -488,14 +488,5 @@ namespace CancerGov.Web.SnippetTemplates {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button submit;
-        
-        /// <summary>
-        /// clear control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor clear;
     }
 }
