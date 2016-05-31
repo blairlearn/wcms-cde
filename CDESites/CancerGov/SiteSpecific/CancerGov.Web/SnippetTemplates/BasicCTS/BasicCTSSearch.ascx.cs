9679dce0fcc52140494ea118570ec22967d7ee87
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2016-05-31 00:59:03 +0000

    Made controls SnippetControls.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/Chronos_CTS@15741 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDESites/CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/BasicCTS/BasicCTSSearch.ascx.cs
+++ CDESites/CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/BasicCTS/BasicCTSSearch.ascx.cs
@@ -5,9 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using NCI.Web.CDE.UI;
+
 namespace CancerGov.Web.SnippetTemplates.BasicCTS
 {
-    public partial class BasicCTSSearch : System.Web.UI.UserControl
+    public partial class BasicCTSSearch : SnippetControl
     {
         protected void Page_Load(object sender, EventArgs e)
         {
--- CDESites/CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/BasicCTS/BasicCTSView.ascx.cs
+++ CDESites/CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/BasicCTS/BasicCTSView.ascx.cs
@@ -1,17 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using NCI.Web.CDE.UI;
+
+
 namespace CancerGov.Web.SnippetTemplates.BasicCTS
 {
-    public partial class BasicViewTrial : System.Web.UI.UserControl
+    public partial class BasicViewTrial : SnippetControl
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Get ID
+            string nctid = Request.Params["id"];
+            if (String.IsNullOrWhiteSpace(nctid)) {
+                this.Controls.Add(new LiteralControl("NeedID"));
+                return;
+            }
+                       
+            if (!Regex.IsMatch(nctid, "^NCT[0-9]+$")) {
+                this.Controls.Add(new LiteralControl("Invalid ID"));
+                return;
+            }
 
+
+            // Get Trial by ID
+            // Show Trial
         }
+
     }
 }
