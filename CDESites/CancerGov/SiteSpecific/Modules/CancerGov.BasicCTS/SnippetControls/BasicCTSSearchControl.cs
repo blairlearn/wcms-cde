c3bfdc8d509ba972508e0b34ce41ae4bba81fa78
Dan Young <youngdr@users.noreply.github.com>
2016-06-01 15:39:48 +0000

    Created initial stub for basic search form Velocity template.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/Chronos_CTS@15749 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTS/SnippetControls/BasicCTSSearchControl.cs
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTS/SnippetControls/BasicCTSSearchControl.cs
@@ -7,17 +7,21 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using NCI.Web.CDE.Modules;
 using NCI.Web.CDE.UI;
 
 namespace CancerGov.ClinicalTrials.Basic.SnippetControls
 {
     public class BasicCTSSearchControl : SnippetControl
     {
+        private string _templatePath = "~/VelocityTemplates/BasicCTSSearch.vm";
+
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
 
+            LiteralControl ltl = new LiteralControl(VelocityTemplate.MergeTemplateWithResultsByFilepath(_templatePath, String.Empty));
+            Controls.Add(ltl);
         }
     }
 }
