cd343534735822c72c41e127e443f104a8bea995
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2016-06-02 19:21:22 +0000

    Adding Classes For ZIP Code Lookups.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/Chronos_CTS@15773 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTS/ZipLookup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CancerGov.ClinicalTrials.Basic
+{
+    /// <summary>
+    /// Represents a ZIP Code Lookup from ElasticSearch
+    /// </summary>
+    public class ZipLookup
+    {
+        /// <summary>
+        /// Gets and Sets the Zip Code
+        /// </summary>
+        public string PostalCode_ZIP { get; set; }
+
+        /// <summary>
+        /// Gets and Sets the GeoCode for that Zip Code
+        /// </summary>
+        public GeoLocation GeoCode { get; set; }
+    }
+}
