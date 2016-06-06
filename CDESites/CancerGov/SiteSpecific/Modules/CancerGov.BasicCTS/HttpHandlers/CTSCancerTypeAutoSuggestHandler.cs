184cc0e8979a2456b72e54450652bf0a1c01df9b
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2016-06-06 03:18:10 +0000

    Updated layout of details. Added HttpHandler for autosuggest. (Although might be better as an ApiController, but introducing a new tech may be risky)
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/Chronos_CTS@15824 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTS/HttpHandlers/CTSCancerTypeAutoSuggestHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CancerGov.ClinicalTrials.Basic.HttpHandlers
+{
+    public class CTSCancerTypeAutoSuggestHandler : IHttpHandler
+    {
+        #region IHttpHandler Members
+
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("");    
+        }
+
+        #endregion
+    }
+}
