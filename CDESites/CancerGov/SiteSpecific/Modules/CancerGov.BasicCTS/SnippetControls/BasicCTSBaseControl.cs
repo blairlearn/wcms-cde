0442ab7826da3758bdf3d88e3acc6f96c454fdb7
Dan Young <youngdr@users.noreply.github.com>
2016-06-21 14:57:39 +0000

    Initial move of search parameter functions to CTS base.
    https://tracker.nci.nih.gov/browse/OCEPROJECT-4180
    
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/Chronos_CTS@16238 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTS/SnippetControls/BasicCTSBaseControl.cs
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTS/SnippetControls/BasicCTSBaseControl.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using NCI.Logging;
 using NCI.Web.CDE.Modules;
 using NCI.Web.CDE.UI;
@@ -7,8 +8,194 @@ namespace CancerGov.ClinicalTrials.Basic.SnippetControls
 {
     public class BasicCTSBaseControl : SnippetControl
     {
+        /// <summary>
+        /// Enumeration representing a bitmap for the fields that are set.
+        /// </summary>
+        [Flags]
+        protected enum SetFields
+        {
+            None = 0,
+            Age = 1,
+            Gender = Age << 1,
+            ZipCode = Gender << 1,
+            ZipProximity = ZipCode << 1,
+            Phrase = ZipProximity << 1,
+            CancerType = Phrase << 1
+        }
+
+        /// <summary>
+        /// basic CTS query parameters
+        /// </summary>
+        protected const string PAGENUM_PARAM = "pn";
+        protected const string ITEMSPP_PARAM = "ni";
+        protected const string PRASE_PARAM = "q";
+        protected const string ZIP_PARAM = "z";
+        protected const string ZIPPROX_PARAM = "zp";
+        protected const string AGE_PARAM = "a";
+        protected const string GENDER_PARAM = "g";
+        protected const string CANCERTYPE_PARAM = "t";
+
+
         private BasicCTSPageInfo _basicCTSPageInfo = null;
         public bool invalidSearchParam = false;
+
+        protected SetFields _setFields = SetFields.None;
+        protected BasicCTSManager _basicCTSManager = null;
+        protected string cancerTypeIDAndHash = null;
+
+        protected BaseCTSSearchParam SetSearchParams()
+        {
+            //Parse Parameters
+            int pageNum = this.ParmAsInt(PAGENUM_PARAM, 1);
+            int itemsPerPage = this.ParmAsInt(ITEMSPP_PARAM, BasicCTSPageInfo.DefaultItemsPerPage);
+            string phrase = this.ParmAsStr(PRASE_PARAM, string.Empty);
+            string zip = this.ParmAsStr(ZIP_PARAM, string.Empty);
+            int zipProximity = this.ParmAsInt(ZIPPROX_PARAM, BasicCTSPageInfo.DefaultZipProximity); //In miles
+            int age = this.ParmAsInt(AGE_PARAM, 0);
+            int gender = this.ParmAsInt(GENDER_PARAM, 0); //0 = decline, 1 = female, 2 = male, 
+            string cancerType = this.ParmAsStr(CANCERTYPE_PARAM, string.Empty);
+            string cancerTypeDisplayName = null;
+
+            BaseCTSSearchParam searchParams = null;
+
+            #region Set Cancer Type or Phrase
+
+            if (cancerType != string.Empty)
+            {
+                //cancerTypeIDAndHash = cancerType;
+                string[] ctarr = cancerType.Split(new Char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (ctarr.Length >= 1)
+                {
+                    if (ctarr.Length > 1)
+                        cancerTypeDisplayName = _basicCTSManager.GetCancerTypeDisplayName(ctarr[0], ctarr[1]);
+                    else if (ctarr.Length == 1)
+                        cancerTypeDisplayName = _basicCTSManager.GetCancerTypeDisplayName(ctarr[0], null);
+
+                    if (cancerTypeDisplayName != null)
+                    {
+                        cancerTypeIDAndHash = cancerType;
+
+                        //Test id to match ^CDR\d+$
+                        searchParams = new CancerTypeSearchParam()
+                        {
+                            //get cancer type.
+                            CancerTypeID = ctarr[0],
+
+                            CancerTypeDisplayName = cancerTypeDisplayName,
+
+                            //Add in the label which is go to ElasticSearch, fetch ctarr[1] (the hash) and get the text
+                            ESTemplateFile = BasicCTSPageInfo.ESTemplateCancerType
+                        };
+
+                        _setFields |= SetFields.CancerType;
+                    }
+                    else
+                    {
+                        invalidSearchParam = true;
+                        searchParams = new CancerTypeSearchParam()
+                        {
+                            ESTemplateFile = BasicCTSPageInfo.ESTemplateCancerType
+                        };
+                    }
+
+
+                }
+
+            }
+            else
+            {
+                searchParams = new PhraseSearchParam()
+                {
+                    Phrase = phrase,
+                    ESTemplateFile = BasicCTSPageInfo.ESTemplateFullText
+                };
+
+                if (!string.IsNullOrWhiteSpace(phrase))
+                {
+                    _setFields |= SetFields.Phrase;
+                }
+
+            }
+
+            #endregion
+
+            // Fill in common parameters
+
+            #region Set Zip Code + GeoLocation
+            if (!string.IsNullOrWhiteSpace(zip))
+            {
+                string pattern = @"^[0-9]{5}$";
+
+                if (Regex.IsMatch(zip, pattern))
+                {
+                    searchParams.ZipLookup = _basicCTSManager.GetZipLookupForZip(zip);
+                    if (searchParams.ZipLookup != null)
+                    {
+                        _setFields |= SetFields.ZipCode;
+                        if (zipProximity != BasicCTSPageInfo.DefaultZipProximity)
+                            _setFields |= SetFields.ZipProximity;
+                    }
+                    else
+                    {
+                        invalidSearchParam = true;
+                    }
+                }
+                else
+                {
+                    invalidSearchParam = true;
+                }
+            }
+
+            #endregion
+
+            #region Set Page and Items Per Page
+            if (pageNum < 1)
+                searchParams.Page = 1;
+            else
+                searchParams.Page = pageNum;
+
+            searchParams.ItemsPerPage = itemsPerPage;
+            #endregion
+
+            #region Set Age
+
+            //Handle Age
+            if (age > 0)
+            {
+                if (age > 120)
+                {
+                    invalidSearchParam = true;
+                }
+                else
+                {
+                    searchParams.Age = age;
+                    _setFields |= SetFields.Age;
+                }
+            }
+
+            #endregion
+
+            #region Set Gender
+
+            //Handle Gender if specified
+            switch (gender)
+            {
+                case 1:
+                    searchParams.Gender = BaseCTSSearchParam.GENDER_FEMALE;
+                    _setFields |= SetFields.Gender;
+                    break;
+                case 2:
+                    searchParams.Gender = BaseCTSSearchParam.GENDER_MALE;
+                    _setFields |= SetFields.Gender;
+                    break;
+            }
+
+            #endregion
+
+            return searchParams;
+        }
+
 
         /// <summary>
         /// Gets a query parameter as a string or uses a default
@@ -51,16 +238,6 @@ namespace CancerGov.ClinicalTrials.Basic.SnippetControls
                 }
             }
         }
-
-        protected const string PAGENUM_PARAM = "pn";
-        protected const string ITEMSPP_PARAM = "ni";
-        protected const string PRASE_PARAM = "q";
-        protected const string ZIP_PARAM = "z";
-        protected const string ZIPPROX_PARAM = "zp";
-        protected const string AGE_PARAM = "a";
-        protected const string GENDER_PARAM = "g";
-        protected const string CANCERTYPE_PARAM = "t";
-
 
         protected BasicCTSPageInfo BasicCTSPageInfo
         {
