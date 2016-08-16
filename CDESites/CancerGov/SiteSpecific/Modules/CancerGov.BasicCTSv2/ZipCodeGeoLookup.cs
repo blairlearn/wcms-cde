7504d9c6c414167c44c9adfe248f29b901433a77
Dion D'aquino <daquinohd@users.noreply.github.com>
2016-08-16 14:34:10 +0000

    //tracker.nci.nih.gov/browse/OCECTS-162 - Added ReloadDictionary() and OnChange() methods - Added comments
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/CTS-Phase2@16704 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTSv2/ZipCodeGeoLookup.cs
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTSv2/ZipCodeGeoLookup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -16,10 +17,19 @@ namespace CancerGov.ClinicalTrials.Basic.v2
      */
     public static class ZipCodeGeoLookup
     {
+        /// <summary>
+        /// ZipCodeDictionary field that will be used for Loader/Reloader
+        /// </summary>
         private static ZipCodeDictionary zipCodeDict;
 
-        // Constructor 
-		// TODO: add Reloader to constructor once it's built out
+        /// <summary>
+        /// Used by WatchTemplateDirectory() to watch for changes to zip_code.json
+        /// </summary>
+        private static FileSystemWatcher zipCodeFileWatcher;
+
+        /// <summary>
+        /// Static constructor
+        /// </summary>
         static ZipCodeGeoLookup()
         {
             zipCodeDict = ZipCodeGeoLoader.LoadDictionary();
@@ -29,6 +39,9 @@ namespace CancerGov.ClinicalTrials.Basic.v2
         public static ZipCodeGeoEntry GetZipCodeGeoEntry(string zipcode)
         {
             ZipCodeDictionary zips = zipCodeDict;
+
+
+            zips = ReloadDictionary(zips);
             if(zips.ContainsKey(zipcode))
             {
                 return zips[zipcode];
@@ -38,5 +51,47 @@ namespace CancerGov.ClinicalTrials.Basic.v2
                 return null;
             }
         }
+
+        static ZipCodeDictionary ReloadDictionary(ZipCodeDictionary zips)
+        {
+            String zipFilePath = ConfigurationSettings.AppSettings["ZipCodesJsonMap"].ToString();
+            zipCodeFileWatcher = new FileSystemWatcher((Path.GetDirectoryName(zipFilePath)));
+            zipCodeFileWatcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.CreationTime | NotifyFilters.Size | NotifyFilters.LastAccess | NotifyFilters.Attributes;
+            zipCodeFileWatcher.Filter = "*.json";
+            zipCodeFileWatcher.EnableRaisingEvents = true;
+            zipCodeFileWatcher.Changed += new FileSystemEventHandler(OnChange);
+            //zipCodeFileWatcher.Created += new FileSystemEventHandler(OnChange);
+            //zipCodeFileWatcher.Deleted += new FileSystemEventHandler(OnChange);
+            //zipCodeFileWatcher.Renamed += new RenamedEventHandler(OnChange);
+
+                            
+
+            try
+            {
+                using (StreamReader r = new StreamReader(zipFilePath))
+                {
+                    string json = r.ReadToEnd();
+                    ZipCodeDictionary zipCodes = JsonConvert.DeserializeObject<ZipCodeDictionary>(json);
+                    return zipCodes;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+                    
+
+        /// <summary>
+        /// Event handler for json files in the Configuration\files directory being modified, created, or deleted.
+        /// </summary>
+        /// <param name="src">event source (not used)</param>
+        /// <param name="e">event arguments (not used)</param>
+        private static void OnChange(object src, FileSystemEventArgs e)
+        {
+            ZipCodeGeoLoader.LoadDictionary();
+        }
+
     }
 }
