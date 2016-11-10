a65182f8c35d5631252ea89e4fc5552d75577e8b
Sarina Padilla <sarinapadilla@users.noreply.github.com>
2016-11-10 20:00:42 +0000

    OCEPROJECT-4308: Created snippet control for new BlogAnalytics app module. This app module is placed on the navon for any blog series, and attaches analytics for blog post page views. BasePageAssemblyInstruction has a new GetWebAnalytics method that allows for getting the value of an analytics prop/evar/event.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/Pheme@17573 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDESites/CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/BlogAnalytics.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="BlogAnalytics.ascx.cs" Inherits="CancerGov.Web.SnippetTemplates.BlogAnalytics" %>
---
+++ CDESites/CancerGov/SiteSpecific/CancerGov.Web/SnippetTemplates/BlogAnalytics.ascx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NCI.Web.CDE;
+using NCI.Web.CDE.UI;
+
+namespace CancerGov.Web.SnippetTemplates
+{
+    public partial class BlogAnalytics : SnippetControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BasePageAssemblyInstruction basePage = PageAssemblyContext.Current.PageAssemblyInstruction as BasePageAssemblyInstruction;
+            string blogName = "";
+
+            if (basePage.ContentItemInfo.ContentItemType == "rx:cgvBlogPost")
+            {
+                blogName = basePage.GetWebAnalytics("prop44");
+                if (!String.IsNullOrWhiteSpace(blogName))
+                {
+                    basePage.SetWebAnalytics("evar48", ffD =>
+                    {
+                        ffD.Value = blogName + " Viewer";
+                    });
+                    basePage.SetWebAnalytics("event53", ffD =>
+                    {
+                        // only fires off event
+                    });
+                }
+            }
+        }
+    }
+}
