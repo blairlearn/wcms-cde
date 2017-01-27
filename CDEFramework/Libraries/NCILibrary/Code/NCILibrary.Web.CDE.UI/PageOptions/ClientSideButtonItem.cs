df0e737eb775aeead58c96e32b4c966d2a3a6435
Dan Young <youngdr@users.noreply.github.com>
2017-01-27 21:28:56 +0000

    WCMSCDE-160: Created ClientSideButtonItem class to represent page option items with no URL.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/Kraken@18176 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/PageOptions/ClientSideButtonItem.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+
+namespace NCI.Web.CDE.UI.WebControls
+{
+    public class ClientSideButtonItem : PageOptionsButtonItem
+    {
+        public new string Title
+        {
+            get
+            {
+                return (string)this.ViewState["Title"] ?? string.Empty;
+            }
+            set
+            {
+                if (!object.Equals(value, this.ViewState["Title"]))
+                {
+                    this.ViewState["Title"] = value;
+                    this.OnClientSideButtonItemChanged();
+                }
+            }
+        }
+
+        internal event EventHandler ClientSideButtonItemChanged;
+
+        protected internal void OnClientSideButtonItemChanged()
+        {
+            if (this.ClientSideButtonItemChanged != null)
+            {
+                this.ClientSideButtonItemChanged(this, EventArgs.Empty);
+            }
+        }
+
+        protected internal ClientSideButtonItem CloneClientSideButtonItem()
+        {
+            ClientSideButtonItem newClientSideButtonItem = this.CreateClientSideButtonItem();
+            this.CopyProperties(newClientSideButtonItem);
+            return newClientSideButtonItem;
+        }
+
+        protected void CopyProperties(ClientSideButtonItem newClientSideButtonItem)
+        {
+            ((ClientSideButtonItem)newClientSideButtonItem).Title = this.Title;
+        }
+
+        protected ClientSideButtonItem CreateClientSideButtonItem()
+        {
+            return new ClientSideButtonItem();
+        }
+
+        public virtual string OnClick
+        {
+            get
+            {
+                return (string)this.ViewState["OnClick"] ?? string.Empty;
+            }
+            set
+            {
+                //If the text is the same, just ignore and don't set dirtyness
+                //or fire any events.
+                if (!object.Equals(value, this.ViewState["OnClick"]))
+                {
+                    this.ViewState["OnClick"] = value;
+                    this.OnClientSideButtonItemChanged();
+                }
+            }
+        }
+    }
+}
