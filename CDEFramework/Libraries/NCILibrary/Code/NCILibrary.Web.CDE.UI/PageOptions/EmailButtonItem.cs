db65fa85d4546181207b3137c85db701b5e1c758
Dan Young <youngdr@users.noreply.github.com>
2017-01-27 21:27:05 +0000

    WCMSCDE-160: Removed unneeded duplication of viewstate code.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/Kraken@18175 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/PageOptions/EmailButtonItem.cs
+++ CDEFramework/Libraries/NCILibrary/Code/NCILibrary.Web.CDE.UI/PageOptions/EmailButtonItem.cs
@@ -8,11 +8,7 @@ namespace NCI.Web.CDE.UI.WebControls
 {
     public class EmailButtonItem : PageOptionsButtonItem
     {
-
-        private bool _trackViewState = false;
-
-        private string _title;
-        public string Title
+        public new string Title
         {
             get
             {
@@ -25,15 +21,6 @@ namespace NCI.Web.CDE.UI.WebControls
                     this.ViewState["Title"] = value;
                     this.OnEmailButtonItemChanged();
                 }
-            }
-        }
-
-        private StateBag _statebag = new StateBag();
-        protected StateBag ViewState
-        {
-            get
-            {
-                return this._statebag;
             }
         }
 
@@ -64,35 +51,6 @@ namespace NCI.Web.CDE.UI.WebControls
             return new EmailButtonItem();
         }
 
-        protected void LoadViewState(object savedState)
-        {
-            if (savedState != null)
-            {
-                object[] objArray = (object[])savedState;
-
-                if (objArray[0] != null)
-                    ((IStateManager)this.ViewState).LoadViewState(objArray[0]);
-            }
-        }
-
-        protected object SaveViewState()
-        {
-            object obj1 = ((IStateManager)this.ViewState).SaveViewState();
-
-            if (obj1 == null)
-                return null;
-
-            return new object[] { obj1 };
-
-        }
-
-        protected void TrackViewState()
-        {
-            this._trackViewState = true;
-            ((IStateManager)this.ViewState).TrackViewState();
-            //If there are any sub IStateManager items, start tracking view state for them as well.
-        }
-
         public virtual string OnClick
         {
             get
@@ -110,28 +68,5 @@ namespace NCI.Web.CDE.UI.WebControls
                 }
             }
         }
-
-        internal void SetDirty()
-        {
-            this._statebag.SetDirty(true);
-
-            //If there are any sub IStateManager items, set them dirty as well.
-        }
-        #region IStateManager Members
-
-        /// <summary>
-        /// When implemented by a class, gets a value indicating whether a server control is tracking its view state changes.
-        /// </summary>
-        /// <value></value>
-        /// <returns>true if a server control is tracking its view state changes; otherwise, false.
-        /// </returns>
-        public bool IsTrackingViewState
-        {
-            get { return this._trackViewState; }
-        }
-
-        #endregion
-
-
     }
 }
