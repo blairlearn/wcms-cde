5493305af9bd60124a24550ba58ba7b8c9800a62
Ben Ferry <ferrybl-nih@users.noreply.github.com>
2017-03-21 20:11:14 +0000

    [OCECTS-351] - Create search terms class for use in query.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/CTSv4@18621 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTSv2/SearchTerm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CancerGov.ClinicalTrials.Basic.v2
+{
+    public class SearchTerms
+    {
+        public String Age { get; set; }
+        public String ZipCode { get; set; }
+        public String CancerType { get; set; }
+    }
+}
