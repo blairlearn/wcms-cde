7cf7ebe79d21a477ac2741060a30c802cdd81a7c
Sarina Padilla <sarinapadilla@users.noreply.github.com>
2017-05-09 21:38:56 +0000

    OCECTS-298: Added config deserialization to BaseTrialListingControl for use in dynamic listing pages.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/CTSv4.2@19243 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTSv2/SnippetControls/BaseTrialListingControl.cs
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTSv2/SnippetControls/BaseTrialListingControl.cs
@@ -2,6 +2,7 @@
 using System.Configuration;
 using System.Text.RegularExpressions;
 using System.Web;
+using System.IO;
 using CancerGov.ClinicalTrials.Basic.v2.Configuration;
 using Common.Logging;
 using NCI.Web;
@@ -9,6 +10,10 @@ using NCI.Web.CDE;
 using NCI.Web.CDE.Modules;
 using NCI.Web.CDE.UI;
 using CancerGov.ClinicalTrials.Basic.v2.SnippetControls.Configs;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
 
 namespace CancerGov.ClinicalTrials.Basic.v2.SnippetControls
 {
@@ -17,6 +22,8 @@ namespace CancerGov.ClinicalTrials.Basic.v2.SnippetControls
     /// </summary>
     public abstract class BaseTrialListingControl : SnippetControl
     {
+        static ILog log = LogManager.GetLogger(typeof(BaseTrialListingControl));
+
         /// <summary>
         /// Gets the configuration type of the derrieved class
         /// </summary>
@@ -30,6 +37,30 @@ namespace CancerGov.ClinicalTrials.Basic.v2.SnippetControls
             base.OnPreRender(e);
 
             Type configType = this.GetConfigType();
+
+            // Read the basic CTS page information JSON
+            string sidata = this.SnippetInfo.Data;
+
+            try
+            {
+                if (string.IsNullOrWhiteSpace(sidata))
+                {
+                    throw new Exception("TrialListingConfig not present in JSON, associate an Application Module item with this page in Percussion");
+                }
+                sidata = sidata.Trim();
+
+                // Get our TrialListingPageInfo object this is JSON data that includes template and URL paths and result count parameters.
+                // It also includes a nested, JSON-formatted string "RequestFilters", which represents the JSON passed in with the API body request - this is
+                // deserialized in TrialListingPageControl.
+                // TODO: handle all deserialization in once place, if possible. This will avoid having to go through the process twice
+                this.Config = (BaseTrialListingConfig)JsonConvert.DeserializeObject(sidata, configType);
+
+            }
+            catch (Exception ex)
+            {
+                log.Error("Could not load the TrialListingPageInfo; check the config info of the Application Module item in Percussion", ex);
+                throw ex;
+            }
             //this.Config = Deserialize JSON in this.Data
         }
     }
