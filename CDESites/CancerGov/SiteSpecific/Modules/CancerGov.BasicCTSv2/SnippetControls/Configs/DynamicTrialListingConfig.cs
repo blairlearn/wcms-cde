45bd572f6ea68770e9f9371be81ea6c045792191
Sarina Padilla <sarinapadilla@users.noreply.github.com>
2017-05-09 17:57:35 +0000

    OCECTS-298: Creating new DynamicTrialListingConfig/Patterns for dynamic trial listing pages
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/CTSv4.2@19225 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTSv2/SnippetControls/Configs/DynamicTrialListingConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CancerGov.ClinicalTrials.Basic.v2.SnippetControls.Configs
+{
+    public class DynamicTrialListingConfig : BaseTrialListingConfig
+    {
+        /// <summary>
+        /// A dictionary for dynamic trial listing pattern names and patterns.
+        /// </summary>
+        public Dictionary<string, DynamicTrialListingConfigPattern> DynamicListingPatterns;
+    }
+}
