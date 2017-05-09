a798642cdc3040853f79e1afd8102d3e273951e6
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2017-05-09 20:27:22 +0000

    [OCECTS-298] Added new base class for trial listings.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/CTSv4.2@19242 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTSv2/SnippetControls/BaseTrialListingControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using System.Web;
+using CancerGov.ClinicalTrials.Basic.v2.Configuration;
+using Common.Logging;
+using NCI.Web;
+using NCI.Web.CDE;
+using NCI.Web.CDE.Modules;
+using NCI.Web.CDE.UI;
+using CancerGov.ClinicalTrials.Basic.v2.SnippetControls.Configs;
+
+namespace CancerGov.ClinicalTrials.Basic.v2.SnippetControls
+{
+    /// <summary>
+    /// Serves as the base class for the different types of trial listing pages.
+    /// </summary>
+    public abstract class BaseTrialListingControl : SnippetControl
+    {
+        /// <summary>
+        /// Gets the configuration type of the derrieved class
+        /// </summary>
+        /// <returns></returns>
+        protected abstract Type GetConfigType();
+
+        protected BaseTrialListingConfig Config { get; private set; }
+
+        protected sealed override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+
+            Type configType = this.GetConfigType();
+            //this.Config = Deserialize JSON in this.Data
+        }
+    }
+}
