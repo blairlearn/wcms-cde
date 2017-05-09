4878be2a57c6588d7f759dc612d4f3673126e0b3
Sarina Padilla <sarinapadilla@users.noreply.github.com>
2017-05-09 14:52:55 +0000

    OCECTS-298: Adding DynamicTrialListingPageControl and DynamicTrialListingPageDiseaseControl for dynamic listing pages for disease type
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/CTSv4.2@19222 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTSv2/SnippetControls/DynamicTrialListingPageControl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using System.Web;
+using CancerGov.ClinicalTrials.Basic.v2.Configuration;
+using Common.Logging;
+using NCI.Web;
+using NCI.Web.CDE.Modules;
+using NCI.Web.CDE.UI;
+
+namespace CancerGov.ClinicalTrials.Basic.v2.SnippetControls
+{
+    public abstract class DynamicTrialListingPageControl : SnippetControl
+    {
+
+    }
+}
