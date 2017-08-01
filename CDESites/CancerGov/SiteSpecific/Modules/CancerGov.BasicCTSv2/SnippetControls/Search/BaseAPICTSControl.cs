e41ed928831d20ff23fa93949ff7895c311d4454
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2017-08-01 15:46:00 +0000

    Added additional control base class and implemented template loading methods.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/pilfering-penguin@19884 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTSv2/SnippetControls/Search/BaseAPICTSControl.cs
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTSv2/SnippetControls/Search/BaseAPICTSControl.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.Text.RegularExpressions;
 using System.Web;
 using CancerGov.ClinicalTrials.Basic.v2.Configuration;
@@ -7,17 +8,78 @@ using NCI.Web;
 using NCI.Web.CDE.Modules;
 using NCI.Web.CDE.UI;
 using CancerGov.ClinicalTrialsAPI;
+using System.Web.UI;
+using System.Dynamic;
+using System.Collections.Generic;
 
 namespace CancerGov.ClinicalTrials.Basic.v2.SnippetControls.Search
 {
     /// <summary>
     /// This is the base class for all API-Based Clinical Trial Search controls.
+    /// This will also serve as the base for the Search controls as they do not need the API.
     /// <remarks>This replaces BasicCTSBaseControl</remarks>
     /// </summary>
-    public class BaseAPICTSControl: SnippetControl
+    public abstract class BaseAPICTSControl: SnippetControl
     {
         static ILog log = LogManager.GetLogger(typeof(BaseAPICTSControl));
-    
 
+        protected BasicCTSPageInfo _config = null;
+
+        /// <summary>
+        /// Create a new instance of a APICTS Control
+        /// </summary>
+        public BaseAPICTSControl()
+        {
+
+            //////////////////////////////
+            // Load the configuration from the SnippetInfo data
+            string spidata = this.SnippetInfo.Data;
+            try
+            {
+                if (string.IsNullOrEmpty(spidata))
+                    throw new Exception("BasicCTSPageInfo not present in xml, associate an application module item  with this page in percussion");
+
+                spidata = spidata.Trim();
+                if (string.IsNullOrEmpty(spidata))
+                    throw new Exception("BasicCTSPageInfo not present in xml, associate an application module item  with this page in percussion");
+
+                _config = ModuleObjectFactory<BasicCTSPageInfo>.GetModuleObject(spidata);
+            }
+            catch (Exception ex)
+            {
+                log.Error("could not load the BasicCTSPageInfo, check the config info of the application module in percussion", ex);
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Gets the path to the template.
+        /// </summary>
+        /// <returns></returns>
+        protected abstract string GetTemplatePath();
+
+        /// <summary>
+        /// Gets the data that should be bound to the Velocity Control
+        /// </summary>
+        protected abstract object GetDataForTemplate();
+
+        /// <summary>
+        /// Overrides the OnPreRender event and 
+        /// </summary>
+        /// <param name="e"></param>
+        protected override sealed void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+
+            string templatePath = this.GetTemplatePath();
+            object templateData = this.GetDataForTemplate();
+
+            LiteralControl ltl = new LiteralControl(VelocityTemplate.MergeTemplateWithResultsByFilepath(
+                templatePath, 
+                templateData
+                )
+            );
+            Controls.Add(ltl);
+        }
     }
 }
---
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTSv2/SnippetControls/Search/BaseMgrAPICTSControl.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using System.Web;
+using CancerGov.ClinicalTrials.Basic.v2.Configuration;
+using Common.Logging;
+using NCI.Web;
+using NCI.Web.CDE.Modules;
+using NCI.Web.CDE.UI;
+using CancerGov.ClinicalTrialsAPI;
+
+namespace CancerGov.ClinicalTrials.Basic.v2.SnippetControls.Search
+{
+    /// <summary>
+    /// This is the base control for anything that needs to interact with the CTAPI (e.g. Results and Details view)
+    /// </summary>
+    public abstract class BaseMgrAPICTSControl : BaseAPICTSControl
+    {
+        static ILog log = LogManager.GetLogger(typeof(BaseMgrAPICTSControl));
+
+        //An instance of the BasicCTSManager for interacting with the CTSAPI
+        protected BasicCTSManager _basicCTSManager = null;
+        protected CTSSearchParams _searchParams = null;
+
+        public BaseMgrAPICTSControl()
+        {
+            //////////////////////////////
+            // Create an instance of a BasicCTSManager.
+            string apiURL = BasicClinicalTrialSearchAPISection.GetAPIUrl();
+            if (string.IsNullOrEmpty(apiURL))
+            {
+                string err = String.Format("Could not load APIURL for {0}", this.GetType().ToString());
+                log.Error(err);
+                throw new Exception(err);
+            }
+            _basicCTSManager = new BasicCTSManager(new ClinicalTrialsAPIClient(apiURL));
+
+            /////////////////////////////
+            // Parse the Query to get the search params.
+            try
+            {
+                CTSSearchParamFactory factory = new CTSSearchParamFactory(DynamicTrialListingMapping.Instance);
+                _searchParams = factory.Create(this.Request.Url.Query);
+            }
+            catch (Exception ex)
+            {
+                log.Error("could not parse the CTS search parameters", ex);
+                throw ex;
+            }
+
+        }
+    }
+}
