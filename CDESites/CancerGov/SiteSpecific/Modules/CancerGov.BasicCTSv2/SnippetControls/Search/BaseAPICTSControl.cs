218229a8f470f2376a27ddbcff176725c5bccaa2
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2017-08-01 14:04:03 +0000

    Added new base class for CTS controls.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/pilfering-penguin@19883 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTSv2/SnippetControls/Search/BaseAPICTSControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using System.Text.RegularExpressions;
+using System.Web;
+using CancerGov.ClinicalTrials.Basic.v2.Configuration;
+using Common.Logging;
+using NCI.Web;
+using NCI.Web.CDE.Modules;
+using NCI.Web.CDE.UI;
+using CancerGov.ClinicalTrialsAPI;
+
+namespace CancerGov.ClinicalTrials.Basic.v2.SnippetControls.Search
+{
+    /// <summary>
+    /// This is the base class for all API-Based Clinical Trial Search controls.
+    /// <remarks>This replaces BasicCTSBaseControl</remarks>
+    /// </summary>
+    public class BaseAPICTSControl: SnippetControl
+    {
+        static ILog log = LogManager.GetLogger(typeof(BaseAPICTSControl));
+    
+
+    }
+}
