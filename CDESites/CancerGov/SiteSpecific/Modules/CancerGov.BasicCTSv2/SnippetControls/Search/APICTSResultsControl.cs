0fe1484be6b4088e524b62dd2480c7aa43406b82
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2017-08-03 16:51:27 +0000

    Wired up pager functions on results page.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/pilfering-penguin@19906 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTSv2/SnippetControls/Search/APICTSResultsControl.cs
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTSv2/SnippetControls/Search/APICTSResultsControl.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Web;
-using CancerGov.ClinicalTrials.Basic.v2.Configuration;
+
 using Common.Logging;
+
 using NCI.Web;
 using NCI.Web.CDE.Modules;
 using NCI.Web.CDE.UI;
+
+using CancerGov.ClinicalTrials.Basic.v2.Configuration;
 using CancerGov.ClinicalTrialsAPI;
 
 namespace CancerGov.ClinicalTrials.Basic.v2.SnippetControls
@@ -62,11 +67,11 @@ namespace CancerGov.ClinicalTrials.Basic.v2.SnippetControls
                 Control = this,
                 Parameters = SearchParams,
                 PageInfo = new {
-                    CurrentPage = this.PageNum.ToString(),
-                    MaxPage = maxPage.ToString(),
-                    ItemsPerPage = this.ItemsPerPage.ToString(),
-                    StartItemNumber = startItemNumber.ToString(),
-                    LastItemNumber = lastItemNumber.ToString()
+                    CurrentPage = this.PageNum,
+                    MaxPage = maxPage,
+                    ItemsPerPage = this.ItemsPerPage,
+                    StartItemNumber = startItemNumber,
+                    LastItemNumber = lastItemNumber
                 },
                 TrialTools = new TrialVelocityTools()
             };
@@ -93,9 +98,212 @@ namespace CancerGov.ClinicalTrials.Basic.v2.SnippetControls
 
             //Add the NCI URL param.
             url.QueryParameters.Add("id", nciID);
+            url.QueryParameters.Add("pn", this.PageNum.ToString());
+            url.QueryParameters.Add("ni", this.ItemsPerPage.ToString());
 
             return url.ToString();
         }
+
+        /// <summary>
+        /// Gets URL for a Single Page of Results
+        /// </summary>
+        /// <param name="pageNum"></param>
+        /// <returns></returns>
+        public string GetPageUrl(int pageNum)
+        {
+            //Create a new url for the current details page.
+            NciUrl url = new NciUrl();
+            url.SetUrl(this.Config.ResultsPagePrettyUrl);
+
+            //Convert the current search parameters into a NciUrl
+            NciUrl paramsUrl = CTSSearchParamFactory.ConvertParamsToUrl(this.SearchParams);
+
+            //Copy the params 
+            url.QueryParameters = paramsUrl.QueryParameters;
+
+            url.QueryParameters.Add("pn", pageNum.ToString());
+            url.QueryParameters.Add("ni", this.ItemsPerPage.ToString());
+
+            return url.ToString();
+        }
+
+        /// <summary>
+        /// Gets the Urls and Labels for all the pages of results from Curr Page - numLeft to Curr Page + numRight
+        /// </summary>
+        /// <param name="numLeft">The number of pages to display left of the selected page</param>
+        /// <param name="numRight">The number of pages to display to the right of the selected page</param>
+        /// <param name="maxPage">The maximum number of pages that could be in this result set</param>
+        /// <param name="totalResults">The total number of results </param>
+        /// <returns></returns>
+        public IEnumerable<object> GetPagerItems(int numLeft, int numRight, int maxPage, long totalResults)
+        {
+            int startPage = (this.PageNum - numLeft) >= 1 ? this.PageNum - numLeft : 1; // Current page minus left limit (numLeft)
+            int endPage = (this.PageNum + numRight) <= maxPage ? this.PageNum + numRight : maxPage; // Current page plus right limit (numRight)
+
+            // The maximum number of elements that can be drawn in this pager. his would be:
+            // ("< Previous" + 1 + 2/ellipsis + numLeft + current + numRight + next-to-last/ellipsis + "Next >") 
+            int itemsTotalMax = 7 + numLeft + numRight;
+
+            // If the pageNumber parameter is set above the highest available page number, set the start page to the endPage value.
+            if (this.PageNum > endPage)
+            {
+                startPage = (endPage - numLeft) >= 1 ? endPage - numLeft : 1;
+            }
+
+            // If maxPage == 1, then only one page of results is found. Therefore, return null for the pager items.
+            // Otherwise, set up the pager accordingly.
+            if (maxPage > 1)
+            {
+                // Create a list of pager item objects:
+                //  Text (string) = link text
+                //  PageUrl (string) = href value for item 
+                //  IsLink (bool) - whether or not this item will be used as a link
+                List<object> items = new List<object>();
+
+                // Draw text and links for first & previous pages
+                if (this.PageNum != 1)
+                {
+                    // Draw link to previous page
+                    string PrevUrl = GetPageUrl(this.PageNum - 1);
+                    items.Add(
+                    new
+                    {
+                        Text = "&lt; Previous",
+                        PageUrl = PrevUrl
+                    });
+
+                    // add previous url to filter
+                    this.PageInstruction.AddUrlFilter("RelPrev", (name, url) =>
+                    {
+                        url.SetUrl(PrevUrl);
+                    });
+
+                    // Draw first page links and text
+                    if (this.PageNum > (numLeft + 1))
+                    {
+                        // Draw link to first page
+                        items.Add(
+                        new
+                        {
+                            Text = "1",
+                            PageUrl = GetPageUrl(1)
+                        });
+
+                        // Draw elipses to delimit first page. 
+                        // If the ellipses only represent a single digit, draw that instead (in this case it will always be 2).
+                        if (this.PageNum > (numLeft + 2))
+                        {
+                            if (this.PageNum == (numLeft + 3))
+                            {
+                                items.Add(
+                                new
+                                {
+                                    Text = "2",
+                                    PageUrl = GetPageUrl(2)
+                                });
+                            }
+                            else
+                            {
+                                items.Add(
+                                new
+                                {
+                                    Text = "...",
+                                    IsLink = false
+                                });
+                            }
+                        }
+                    }
+                }
+
+                // Draw links before and after current
+                for (int i = startPage; i <= endPage; i++)
+                {
+                    items.Add(
+                        new
+                        {
+                            Text = i.ToString(),
+                            PageUrl = GetPageUrl(i)
+                        }
+                    );
+                }
+
+                // Draw last page links and text
+                if (this.PageNum < endPage)
+                {
+                    // Draw iink to last page
+                    if (this.PageNum < (maxPage - numRight))
+                    {
+                        // Draw elipses to delimit last page. 
+                        // If the ellipses only represent a single digit, draw that instead (in this case it will always maxmimum page minus one).
+                        if (this.PageNum < (maxPage - numRight - 1))
+                        {
+                            if (this.PageNum == (maxPage - numRight - 2))
+                            {
+                                items.Add(
+                                new
+                                {
+                                    Text = (maxPage - 1).ToString(),
+                                    PageUrl = GetPageUrl(maxPage - 1)
+                                });
+                            }
+                            else
+                            {
+                                items.Add(
+                                new
+                                {
+                                    Text = "...",
+                                    IsLink = false
+                                });
+                            }
+                        }
+
+                        // Draw link to last page
+                        items.Add(
+                        new
+                        {
+                            Text = maxPage.ToString(),
+                            PageUrl = GetPageUrl(maxPage)
+                        });
+                    }
+
+                    // Draw link to next page
+                    string NextUrl = GetPageUrl(this.PageNum + 1);
+                    items.Add(
+                    new
+                    {
+                        Text = "Next &gt;",
+                        PageUrl = NextUrl
+                    });
+
+                    this.PageInstruction.AddUrlFilter("RelNext", (name, url) =>
+                    {
+                        url.SetUrl(NextUrl);
+                    });
+
+                }
+
+                // Remove any duplicate links that may have slipped though. This only occurs in cases where the URL query param 
+                // is greater than the last available page.
+                // Doing this after the fact to prevent mucking up the code above. This is an edge case and should be handled outside of
+                // the general logic. 
+                if (this.PageNum > maxPage)
+                {
+                    items = items.Distinct().ToList();
+                    // Remove ellipsis if it shows betweeen two consecutive numbers
+                    if (items.Count - 2 == maxPage)
+                    {
+                        items.RemoveAt(2);
+                    }
+                }
+
+                return items;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
 
         #endregion
 
