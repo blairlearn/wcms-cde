a70ddf21ddb4eca65b1b133a4e271edbb561252e
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2017-08-03 17:01:02 +0000

    Added new code-behind for details page.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/pilfering-penguin@19907 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTSv2/SnippetControls/Search/APICTSDetailsControl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+using Common.Logging;
+
+using NCI.Web;
+using NCI.Web.CDE.Modules;
+using NCI.Web.CDE.UI;
+
+using CancerGov.ClinicalTrials.Basic.v2.Configuration;
+using CancerGov.ClinicalTrialsAPI;
+
+namespace CancerGov.ClinicalTrials.Basic.v2.SnippetControls.Search
+{
+    public class APICTSDetailsControl : BaseMgrAPICTSControl
+    {
+        /// <summary>
+        /// Gets the path to the template
+        /// </summary>
+        /// <returns></returns>
+        protected override string GetTemplatePath()
+        {
+            return this.Config.DetailedViewPageTemplatePath;
+        }
+
+        /// <summary>
+        /// Goes and fetches the data from the API & Returns the results to base class to be bound to the template.
+        /// </summary>
+        /// <returns></returns>
+        protected override object GetDataForTemplate()
+        {
+            return new
+            {
+
+            };
+        }
+    }
+}
