2a8613e2b7e0b82b6bc0ede969da38de012c2529
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2017-08-06 04:32:46 +0000

    Commited initial logic for filtering sites when drawing clinical trial.  Wanted to release locks on the project files.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/pilfering-penguin@19919 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.APICTS.Test/TrialSiteLocationFilterExtension.AtNIHTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CancerGov.ClinicalTrials.Basic.v2.SnippetControls;
+using CancerGov.ClinicalTrialsAPI;
+
+using Xunit;
+using Moq;
+
+namespace CancerGov.ClinicalTrials.Basic.v2.Test
+{
+    public partial class TrialSiteLocationFilterExtension_Test
+    {
+
+        public static IEnumerable<object[]> AtNIHFilteringData 
+        {
+            get
+            {
+                // Array of tests
+                return new[]
+                {
+                    //Empty Set
+                    new object[] {
+                        new ClinicalTrial() {
+                            Sites = new List<ClinicalTrial.StudySite>()
+                        },
+                        new ClinicalTrial.StudySite[] { }
+                    },
+                    //
+                    new object[] { 
+                        new ClinicalTrial() {
+                            Sites = new List<ClinicalTrial.StudySite>()
+                        }, 
+                        new ClinicalTrial.StudySite[] {
+
+                        }
+                    },
+                };
+            }
+        }
+
+        [Theory, MemberData("AtNIHFilteringData")]
+        public void FilterByAtNIH(ClinicalTrial trial, IEnumerable<ClinicalTrial.StudySite> expectedSites)
+        {
+
+            CTSSearchParams searchParams = new CTSSearchParams() { Location = LocationType.AtNIH };
+            IEnumerable<ClinicalTrial.StudySite> actual = trial.FilterSitesByLocation(searchParams);
+
+        }
+
+        private static IEnumerable<ClinicalTrial.StudySite> GetNIHSites() 
+        {
+            List<ClinicalTrial.StudySite> rtnSites = new List<ClinicalTrial.StudySite>();
+
+            return rtnSites;
+        }
+        
+    }
+}
---
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.APICTS.Test/TrialSiteLocationFilterExtension.CountryCityStateTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CancerGov.ClinicalTrials.Basic.v2.SnippetControls;
+using CancerGov.ClinicalTrialsAPI;
+
+using Xunit;
+using Moq;
+
+namespace CancerGov.ClinicalTrials.Basic.v2.Test
+{
+    public partial class TrialSiteLocationFilterExtension_Test
+    {
+
+    }
+}
