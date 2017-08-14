dfa431a0f7883b4df6b402aa2c1593be6227b74b
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2017-08-14 19:29:49 +0000

    Added test cases.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/pilfering-penguin@19990 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDESites/CancerGov/SiteSpecific/Modules/CancerGov.APICTS.Test/SearchParams/CountryCityStateLocationSearchParams.Test.cs
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.APICTS.Test/SearchParams/CountryCityStateLocationSearchParams.Test.cs
@@ -4,10 +4,95 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Xunit;
+
 namespace CancerGov.ClinicalTrials.Basic.v2.Test.SearchParams
 {
     public class CountryCityStateLocationSearchParams_Test
     {
-        //TODO: Test all combos, and ensure _usedFields is set correctly
+        [Fact]
+        public void SetCityHasCityGetCity()
+        {
+            CountryCityStateLocationSearchParams locParams = new CountryCityStateLocationSearchParams();
+            locParams.City = "Baltimore";
+
+            Assert.True(locParams.IsFieldSet("City"));
+            Assert.False(locParams.IsFieldSet("State"));
+            Assert.False(locParams.IsFieldSet("Country"));
+            Assert.Equal("Baltimore", locParams.GetFieldAsString("City"));
+        }
+
+        [Fact]
+        public void SetCountryHasCountryGetCountry()
+        {
+            CountryCityStateLocationSearchParams locParams = new CountryCityStateLocationSearchParams();
+            locParams.Country = "United States";
+
+            Assert.True(locParams.IsFieldSet("Country"));
+            Assert.False(locParams.IsFieldSet("City"));
+            Assert.False(locParams.IsFieldSet("State"));
+            Assert.Equal("United States", locParams.GetFieldAsString("Country"));
+        }
+
+        [Fact]
+        public void SetStateHasStateGetState()
+        {
+            CountryCityStateLocationSearchParams locParams = new CountryCityStateLocationSearchParams();
+            locParams.State = new LabelledSearchParam[] 
+            {
+                new LabelledSearchParam() {
+                    Key = "MD",
+                    Label = "Maryland"
+                }
+            };
+
+            Assert.True(locParams.IsFieldSet("State"));
+            Assert.False(locParams.IsFieldSet("City"));
+            Assert.False(locParams.IsFieldSet("Country"));
+            Assert.Equal("Maryland", locParams.GetFieldAsString("State"));
+        }
+
+        [Fact]
+        public void SetStateHasStateGetStateX2()
+        {
+            CountryCityStateLocationSearchParams locParams = new CountryCityStateLocationSearchParams();
+            locParams.State = new LabelledSearchParam[] 
+            {
+                new LabelledSearchParam() {
+                    Key = "MD",
+                    Label = "Maryland"
+                },
+                new LabelledSearchParam() {
+                    Key = "DE",
+                    Label = "Delaware"
+                }
+            };
+
+            Assert.True(locParams.IsFieldSet("State"));
+            Assert.False(locParams.IsFieldSet("City"));
+            Assert.False(locParams.IsFieldSet("Country"));
+            Assert.Equal("Delaware, Maryland", locParams.GetFieldAsString("State"));
+        }
+
+        [Fact]
+        public void SetAllHasAll()
+        {
+            CountryCityStateLocationSearchParams locParams = new CountryCityStateLocationSearchParams();
+            locParams.Country = "United States";
+            locParams.City = "Baltimore";
+            locParams.State = new LabelledSearchParam[] 
+            {
+                new LabelledSearchParam() {
+                    Key = "MD",
+                    Label = "Maryland"
+                }
+            };
+
+            Assert.True(locParams.IsFieldSet("State"));
+            Assert.True(locParams.IsFieldSet("City"));
+            Assert.True(locParams.IsFieldSet("Country"));
+        }
+
+
     }
 }
