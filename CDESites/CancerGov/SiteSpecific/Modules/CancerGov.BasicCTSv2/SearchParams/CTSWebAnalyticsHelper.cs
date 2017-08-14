0a858c620009f62663ec31cd26885a3c8ecdf3bd
Dion D'aquino <daquinohd@users.noreply.github.com>
2017-08-14 15:37:24 +0000

    //tracker.nci.nih.gov/browse/OCECTS-524 - added CTSWebAnalyticsHelper class
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/pilfering-penguin@19983 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.BasicCTSv2/SearchParams/CTSWebAnalyticsHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NCI.Web;
+
+namespace CancerGov.ClinicalTrials.Basic.v2
+{
+    /// <summary>
+    /// Helper class to create page-load analytics values for CTS pages.
+    /// </summary>
+    class CTSWebAnalyticsHelper
+    {
+
+        /**TODO:
+         * Create unit tests
+         * Clean up static.cancer.gov files
+         */
+        /// See serialize methods from param factory as example:
+        ///
+        //private static void SerializeTrialTypes(NciUrl url, CTSSearchParams searchParams)
+        //{
+        //    if (searchParams.IsFieldSet(FormFields.TrialTypes))
+        //    {
+        //        url.QueryParameters.Add(
+        //            "tt",
+        //            string.Join(",", searchParams.TrialTypes.Select(tp => tp.Key))
+        //        );
+        //    }
+        //}
+    }
+}
