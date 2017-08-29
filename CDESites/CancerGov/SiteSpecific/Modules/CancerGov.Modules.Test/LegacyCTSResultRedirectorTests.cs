d0fef06cd8735622770ea45c4cf9116fa46effad
Dion D'aquino <daquinohd@users.noreply.github.com>
2017-08-29 00:13:41 +0000

    //tracker.nci.nih.gov/browse/OCECTS-707 - fixed unit tests
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/pilfering-penguin@20066 e9cc8610-1bbf-714c-a5f3-26df0ede829a

--- CDESites/CancerGov/SiteSpecific/Modules/CancerGov.Modules.Test/LegacyCTSResultRedirectorTests.cs
+++ CDESites/CancerGov/SiteSpecific/Modules/CancerGov.Modules.Test/LegacyCTSResultRedirectorTests.cs
@@ -4,24 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Web;
 
+using CancerGov.HttpModules;
 using Xunit;
 
 namespace CancerGov.Modules.Test
 {
     public class LegacyCTSResultRedirectorTests
     {
-
-        // TODO: fix references. For some reason, I'm not able to reach LegacyCTSResultsRedirector from here. 
-        // Can't create a new LegacyCTSResultsRedirector object to test the methods there. Adding
-        // references doesn't help. Copying and pasting the existing methods for now, but will need to figure this 
-        // out later since this isn's really testing anything. 
+        // Create new redirector object for testing
+        LegacyCTSResultRedirector fakeRedirector = new LegacyCTSResultRedirector();
 
         // Test one-item array
         [Fact]
         private void TestSplit1()
         {
-            String[] vals = SplitArrayOnPipes("C4872");
+            String[] vals = fakeRedirector.SplitArrayOnPipes("C4872");
             Assert.Equal(vals[0], "C4872");
             Assert.Equal(vals.Length, 1);
         }
@@ -30,7 +29,7 @@ namespace CancerGov.Modules.Test
         [Fact]
         private void TestSplit2()
         {
-            String[] vals = SplitArrayOnPipes("C4872|breast_carcinoma");
+            String[] vals = fakeRedirector.SplitArrayOnPipes("C4872|breast_carcinoma");
             Assert.Equal(vals[0], "C4872");
             Assert.Equal(vals[1], "breast_carcinoma");
             Assert.Equal(vals.Length, 2);
@@ -40,7 +39,7 @@ namespace CancerGov.Modules.Test
         [Fact]
         private void TestSplit3()
         {
-            String[] vals = SplitArrayOnPipes("C89636|C4006|stage_iv_uterine_corpus_cancer");
+            String[] vals = fakeRedirector.SplitArrayOnPipes("C89636|C4006|stage_iv_uterine_corpus_cancer");
             Assert.Equal(vals[0], "C89636");
             Assert.Equal(vals[1], "C4006");
             Assert.Equal(vals[2], "stage_iv_uterine_corpus_cancer");
@@ -54,7 +53,7 @@ namespace CancerGov.Modules.Test
         [InlineData("C89636%7CC4006")]
         private void TestIsNotLegacyData(string val)
         {
-            Assert.False(IsLegacyValue(val));
+            Assert.False(fakeRedirector.IsLegacyValue(val));
         }
 
         // Test legacy (non C-code) values
@@ -64,40 +63,7 @@ namespace CancerGov.Modules.Test
         [InlineData("i")]
         private void TestIsLegacyData(string val)
         {
-            Assert.True(IsLegacyValue(val));
-        }
-
-        /// <summary>
-        /// Returns an array of values from a pipe-delimited string.
-        /// </summary>
-        /// <param name="val"></param>
-        /// <returns></returns>
-        public String[] SplitArrayOnPipes(string val)
-        {
-            List<string> vals = new List<string>();
-            string[] valArray = vals.ToArray();
-            char[] delimiterChars = { '|' };
-
-            // Check that the parameter has a value. 
-            if (!string.IsNullOrWhiteSpace(val))
-            {
-                // Create an array of pipe-separated values.
-                valArray = val.Split(delimiterChars);
-            }
-            return valArray;
-        }
-
-        /// <summary>
-        /// Checks for a non-C-Code pattern in a given string.
-        /// </summary>
-        /// <param name="str"></param>
-        /// <returns></returns>
-        public bool IsLegacyValue(string str)
-        {
-            // Regex pattern: any string that contains an underscore or a letter other than "c".
-            string pattern = @"[ab-zAB-Z_]$";
-            Regex rgx = new Regex(pattern);
-            return rgx.IsMatch(str);
+            Assert.True(fakeRedirector.IsLegacyValue(val));
         }
 
     }
