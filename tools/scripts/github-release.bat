e4d3dae4ced0791fb3cf2ea62229c5ebf3bb6fa5
Blair Learn <blairlearn@users.noreply.github.com>
2017-09-08 20:12:47 +0000

    Tool for uploading releases to GitHub.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/git-build@20092 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ tools/scripts/github-release.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell -ExecutionPolicy RemoteSigned ./github-release.ps1
---
+++ tools/scripts/github-release.ps1
@@ -0,0 +1,50 @@
+
+<#
+#   GitHub-Release
+#
+#   Create a release on GitHub.
+#   Based on https://gist.github.com/JanJoris/ee4c7f9b4289016b2216
+#>
+function GitHub-Release($versionNumber, $commitId, $preRelease, $releaseNotes, $artifactOutputDirectory, $artifact, $gitHubUsername, $gitHubRepository, $gitHubApiKey)
+{
+    $draft = $TRUE
+    
+    $releaseData = @{
+       tag_name = [string]::Format("v{0}", $versionNumber);
+       target_commitish = $commitId;
+       name = [string]::Format("v{0}", $versionNumber);
+       body = $releaseNotes;
+       draft = $draft;
+       prerelease = $preRelease;
+    }
+
+    $auth = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($gitHubApiKey + ":x-oauth-basic"));
+
+    $releaseParams = @{
+       Uri = "https://api.github.com/repos/$gitHubUsername/$gitHubRepository/releases";
+       Method = 'POST';
+       Headers = @{
+         Authorization = $auth;
+       }
+       ContentType = 'application/json';
+       Body = (ConvertTo-Json $releaseData -Compress)
+    }
+
+    $result = Invoke-RestMethod @releaseParams 
+    $uploadUri = $result | Select -ExpandProperty upload_url
+    Write-Host $uploadUri
+    $uploadUri = $uploadUri -creplace '\{\?name,label\}'  #, "?name=$artifact"
+    $uploadUri = $uploadUri + "?name=$artifact"
+    $uploadFile = Join-Path -path $artifactOutputDirectory -childpath $artifact
+
+    $uploadParams = @{
+      Uri = $uploadUri;
+      Method = 'POST';
+      Headers = @{
+        Authorization = $auth;
+      }
+      ContentType = 'application/zip';
+      InFile = $uploadFile
+    }
+    $result = Invoke-RestMethod @uploadParams
+}
