e6d5bba01527cfe1a2cc401a9fb84e4588dfdf3f
Blair Learn <blairlearn@users.noreply.github.com>
2017-09-12 18:48:56 +0000

    Add create-zip script
    Powershell script to create ZIP files.
    
    git-svn-id: https://ncisvn.nci.nih.gov/svn/oce_wcmteam/ContentDeliveryEngine/branches/git-build@20098 e9cc8610-1bbf-714c-a5f3-26df0ede829a

---
+++ tools/scripts/create-zip.ps1
@@ -0,0 +1,26 @@
+Param(
+    [Parameter(mandatory=$True, ValueFromPipeline=$False, Position=0)]
+    [string]$sourcePath,
+
+    [Parameter(mandatory=$True, ValueFromPipeline=$False, Position=0)]
+    [string]$destinationPath
+)
+
+<#
+    .SYNOPSIS
+    Creates a ZIP file.
+
+    .DESCRIPTION
+    Compresses the content of sourcePath, creating a new zip at the location
+    specified by destinationPath.
+
+    .PARAMETER sourcePath
+    Location of the files to be compressed.  This may be a directory, or an individual filespec (wildcards are supported).
+
+
+    .PARAMETER destinationPath
+    The name and path of the ZIP file to be created.
+#>
+
+
+Compress-Archive -Path $sourcePath -DestinationPath $destinationPath
