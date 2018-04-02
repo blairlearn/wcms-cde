5a1a8921d110bfae8d5fd65c6e9672888cfd9333
Blair Learn <blairlearn@users.noreply.github.com>
2018-04-02 15:19:40 -0400

    Move deploy steps into job definition
    The deploy job doesn't use the main code repository, so we want
    everything to be as self-contained as possible.

--- tools/jenkins/deploy-steps.bat
+++
@@ -1,4 +0,0 @@
-mkdir _dist
-powershell -executionpolicy unrestricted github-tools\download-release.ps1 -gitHubUsername nciocpl -gitHubRepository wcms-cde -releaseName %release_name% -saveToPath _dist\distribution.zip
-powershell -executionpolicy unrestricted zip-tools\expand-zip -source _dist\distribution.zip -destinationPath _dist
-_dist\cdeDeploy.bat
