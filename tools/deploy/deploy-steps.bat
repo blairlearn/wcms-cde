368851e7a8023eacf61772c9998a38adf6f3aa7f
Blair Learn <blairlearn@users.noreply.github.com>
2018-04-03 17:53:21 -0400

    Extract release commit hash

--- tools/deploy/deploy-steps.bat
+++ tools/deploy/deploy-steps.bat
@@ -1,4 +1,14 @@
-mkdir _dist
+if "%GIT_URL%"=="" (
+    echo Required environment variable 'GIT_URL' not set.
+    exit /b 1
+)
+
+rmdir /s/q _dist && mkdir _dist
 powershell -executionpolicy unrestricted tools\build\build-tools\github-tools\download-release.ps1 -gitHubUsername nciocpl -gitHubRepository wcms-cde -releaseName %release_name% -saveToPath _dist\distribution.zip
 powershell -executionpolicy unrestricted tools\build\build-tools\zip-tools\expand-zip -source _dist\distribution.zip -destinationPath _dist
 rem _dist\cdeDeploy.bat
+
+:: Get the hash for the release.
+for /f %%a in ('git ls-remote -t %GIT_URL% %release_name%') do set RELEASE_HASH=%%a
+
+echo '%RELEASE_HASH%'
